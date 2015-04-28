e61fcc99efb511c345193d86f25c2d349b4c763f
Mike Kasprzak <mike@sykhronics.com>
2015-04-28 13:41:44 -0400

    tweak to the add ports script. Maybe this time.

--- scripts/add-ports.sh
+++ scripts/add-ports.sh
@@ -2,6 +2,10 @@
 # This script adds the extra ports for other Ludum Dare services.
 # i.e. 8080 for api.ludumdare.com, 8081 for static.ludumdare.com, etc.
 
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd $SCRIPTPATH
+
 echo "Adding additional server ports to Apache"
 cat apache-ports-conf.txt>>/etc/apache2/ports.conf
 cat apache-default-conf.txt>>/etc/apache2/sites-available/000-default.conf
