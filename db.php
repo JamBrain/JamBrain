2e16f051dc422e31f473c074e61d128c69680fee
Mike Kasprzak <mike@sykhronics.com>
2015-04-27 21:09:32 -0400

    db line errer

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ include "config.php";
 if ( !isset($cfg_dbo) ) {
 	$msg = "No database configuration found.";
 	error_log( $msg );
-	echo "<strong>ERROR:</strong> " . $msg );
+	echo "<strong>ERROR:</strong> " . $msg;
 }
 
 ?>
