bcb0a395b68818393af1a304e37d34f1bca1d766
Mike Kasprzak <mike@sykhronics.com>
2015-04-29 14:14:44 -0400

    Added some function code. Lets see if this works.

--- db.php
+++ db.php
@@ -1,11 +1,50 @@
 <?php
 
+// Load the configuration //
 include "config.php";
 
-if ( !isset($cfg_db) ) {
-	$msg = "No database configuration found.";
-	error_log( $msg );
-	echo "<strong>ERROR:</strong> " . $msg;
+// **** GLOBAL DATABASE VARIABLE **** //
+$db = null;
+
+// Logging function specific to database operations //
+function db_log( $msg ) {
+	error_log( "LD-CMW DB ERROR: " . $msg );
+	echo "<strong>LD-CMW DB ERROR:</strong> " . $msg;
 }
+
+// Check database config //
+if ( !isset($cfg_db_host) ) {
+	db_log( "No database host name set." );
+}
+if ( !isset($cfg_db_name) ) {
+	db_log( "No database name set." );
+}
+if ( !isset($cfg_db_login) ) {
+	db_log( "No database login set." );
+}
+if ( !isset($cfg_db_password) ) {
+	db_log( "No database password set." );
+}
+
+// Connect to the Database //
+function db_init() {
+	if ( !$db ) {
+		$db = new mysqli($cfg_db_host,$cfg_db_name,$cfg_db_login,$cfg_db_password);
+		
+		// http://php.net/manual/en/mysqli.quickstart.connections.php
+		if ($db->connect_errno) {
+    		db_log( "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error );
+    	}
+	}
+	else {
+		db_log( "Database connection already initialized" );
+	}
+}
+// Are we connected/ready? //
+function db_isReady() {
+	return ( isset($db) && ($db != null) );
+}
+
+
 
 ?>
