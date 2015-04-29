fe1e750f47f98992311078ba8331a224bf63f0e0
Mike Kasprzak <mike@sykhronics.com>
2015-04-28 20:28:48 -0400

    apc test

--- public/index.php
+++ public/index.php
@@ -1,8 +1,20 @@
 So. It has come to this...<br />
-
+<br />
 <?php
 
 include "../db.php";
 include "../lib.php";
+
+
+$berg = apcu_fetch( "Hamburg" );
+
+if ( $berg === false ) {
+	$berg = 1;
+}
+else {
+	$berg++;
+}
+
+apcu_store( "Hamburg", $berg );
 
 ?>
