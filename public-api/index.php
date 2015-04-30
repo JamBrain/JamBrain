e54c81b9f6a0ddbc5c7f463bf5700a7406d3e4fa
Mike Kasprzak <mike@sykhronics.com>
2015-04-30 15:13:02 -0400

    api test

--- public-api/index.php
+++ public-api/index.php
@@ -1,1 +1,16 @@
-API stub
+<?php
+// CMW API //
+
+$out = array(
+		'bort' => 17,
+		'court' => 'westby'
+		'zome' => array(
+			'themby' => true,
+			'chebble' => 'scorn'
+			)
+		);
+	
+
+print_r( json_encode($out,true) );
+
+?>
