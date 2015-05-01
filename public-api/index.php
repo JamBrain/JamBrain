7950bfbd3800642cde17a971a45295e7dd6fd272
Mike Kasprzak <mike@sykhronics.com>
2015-05-01 18:05:36 -0400

    testing rewrite

--- public-api/index.php
+++ public-api/index.php
@@ -2,15 +2,14 @@
 // Default API //
 $url = getenv('REDIRECT_URL');
 $query = getenv('REDIRECT_QUERY_STRING');
-if ( !$query ) {
-	$query = "";	// getenv returns false on error //
-}
+
 
 $out = array(
 	'response' => 'ok'
 );
-$out['url'] = $url;
-$out['query'] = $query;
+if ( $url )		$out['url'] = $url;
+if ( $query )	$out['query'] = $query;
+
 
 // By default, PHP will make '/' slashes in to '\/'. These flags fix that //
 $out_format = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
@@ -18,6 +17,7 @@ $out_format = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
 if ( isset($_GET['pretty']) ) {
 	$out_format |= JSON_PRETTY_PRINT;
 } 
+
 
 header('Content-Type: application/json');
 echo str_replace('</', '<\/', json_encode($out,$out_format));
