056affb8725520774e7c99ab65464b74f9d5c2e6
Mike Kasprzak <mike@sykhronics.com>
2015-05-01 13:34:10 -0400

    testing pattern matching

---
+++ public-api/game.php
@@ -0,0 +1,19 @@
+<?php
+// CMW API //
+
+$out = array(
+		'game' => 1
+		);
+
+$out['url'] = $_GET['u'];
+$out['args'] = $_GET;
+
+$out_format = 0;
+if ( isset($_GET['pretty']) ) {
+	$out_format |= JSON_PRETTY_PRINT;
+} 
+
+header('Content-Type: application/json');
+echo json_encode($out,$out_format);
+
+?>
