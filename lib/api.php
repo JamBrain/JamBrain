12c5eff3bfed7b199a21333b9c48808d1f9ce971
Mike Kasprzak <mike@sykhronics.com>
2015-05-01 20:38:10 -0400

    testing rewrite

---
+++ lib/api.php
@@ -0,0 +1,48 @@
+<?php
+
+// http://stackoverflow.com/questions/3128062/is-this-safe-for-providing-jsonp
+function api_isValidJSONPCallback($subject) {
+     $identifier_syntax
+       = '/^[$_\p{L}][$_\p{L}\p{Mn}\p{Mc}\p{Nd}\p{Pc}\x{200C}\x{200D}]*+$/u';
+
+     $reserved_words = array('break', 'do', 'instanceof', 'typeof', 'case',
+       'else', 'new', 'var', 'catch', 'finally', 'return', 'void', 'continue', 
+       'for', 'switch', 'while', 'debugger', 'function', 'this', 'with', 
+       'default', 'if', 'throw', 'delete', 'in', 'try', 'class', 'enum', 
+       'extends', 'super', 'const', 'export', 'import', 'implements', 'let', 
+       'private', 'public', 'yield', 'interface', 'package', 'protected', 
+       'static', 'null', 'true', 'false');
+
+     return preg_match($identifier_syntax, $subject)
+         && ! in_array(mb_strtolower($subject, 'UTF-8'), $reserved_words);
+}
+
+function api_emitJSON( $out ) {
+	// By default, PHP will make '/' slashes in to '\/'. These flags fix that //
+	$out_format = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
+	
+	// If 'pretty' mode (i.e. readable) //
+	if ( isset($_GET['pretty']) ) {
+		$out_format |= JSON_PRETTY_PRINT;
+	}
+	
+	$prefix = "";
+	$suffix = "";
+	if ( isset($_GET['callback']) ) {
+		$callback = $_GET['callback'];
+		if ( api_isValidJSONPCallback($callback) ) {
+			$prefix = $callback . "(";
+			$suffix = ");";
+		}
+		else {
+			http_response_code(400);
+			exit(1);
+		}
+	}
+	
+	// Output the Page //
+	header('Content-Type: application/json');
+	echo $prefix . str_replace('</', '<\/', json_encode($out,$out_format)) . $suffix;
+}
+
+?>
--- public-api/index.php
+++ public-api/index.php
@@ -1,4 +1,6 @@
 <?php
+require "../lib/api.php";
+
 // Default API (for catching errors). Never fails. //
 $out = array(
 	'status' => 'OK'
@@ -11,58 +13,13 @@ if ( $url ) {
 	$out['debug'] = array();
 	$out['debug']['url'] = $url;
 }
-
 $query = getenv('REDIRECT_QUERY_STRING');
 if ( $query ) {
 	$out['debug']['query'] = $query;
 }
 
 
-
-// http://stackoverflow.com/questions/3128062/is-this-safe-for-providing-jsonp
-function api_isValidCallback($subject) {
-     $identifier_syntax
-       = '/^[$_\p{L}][$_\p{L}\p{Mn}\p{Mc}\p{Nd}\p{Pc}\x{200C}\x{200D}]*+$/u';
-
-     $reserved_words = array('break', 'do', 'instanceof', 'typeof', 'case',
-       'else', 'new', 'var', 'catch', 'finally', 'return', 'void', 'continue', 
-       'for', 'switch', 'while', 'debugger', 'function', 'this', 'with', 
-       'default', 'if', 'throw', 'delete', 'in', 'try', 'class', 'enum', 
-       'extends', 'super', 'const', 'export', 'import', 'implements', 'let', 
-       'private', 'public', 'yield', 'interface', 'package', 'protected', 
-       'static', 'null', 'true', 'false');
-
-     return preg_match($identifier_syntax, $subject)
-         && ! in_array(mb_strtolower($subject, 'UTF-8'), $reserved_words);
-}
-
-function api_emitJSON( $out ) {
-	// By default, PHP will make '/' slashes in to '\/'. These flags fix that //
-	$out_format = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
-	// If 'pretty' mode (i.e. readable) //
-	if ( isset($_GET['pretty']) ) {
-		$out_format |= JSON_PRETTY_PRINT;
-	}
-	
-	$prefix = "";
-	$suffix = "";
-	if ( isset($_GET['callback']) ) {
-		$callback = $_GET['callback'];
-		if ( api_isValidCallback($callback) ) {
-			$prefix = $callback . "(";
-			$suffix = ");";
-		}
-		else {
-			http_response_code(400);
-			exit(1);
-		}
-	}
-	
-	// Output the Page //
-	header('Content-Type: application/json');
-	echo $prefix . str_replace('</', '<\/', json_encode($out,$out_format)) . $suffix;
-}
-
+// Output the Page //
 api_emitJSON( $out );
 
 ?>
