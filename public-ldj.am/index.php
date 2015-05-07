bcaf43b554a879ea2ba7788530704aa3c71456c8
Mike Kasprzak <mike@sykhronics.com>
2015-05-07 11:45:13 -0400

    Added notes to ldj.am, how it will work

--- public-ldj.am/index.php
+++ public-ldj.am/index.php
@@ -1,1 +1,24 @@
-ldj.am stub
+<?php
+require_once __DIR__ . "/../db.php";
+
+
+// Parse the URL //
+
+
+// Validate/Convert to an ID //
+
+
+// Check if cached (APCu), and emit stored redirect URL if it is (END) //
+
+
+// Do database lookup for Item ID, cache (APCu) and emit redirect URL (END) //
+
+//apcu_store(); // Cache value for 5 minutes //
+
+
+// If no ID is present //
+echo "<strong>ldj.am</strong> - Ludum Dare URL shortener.";
+
+// If ID is bad //
+
+?>
