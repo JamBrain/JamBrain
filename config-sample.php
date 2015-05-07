c57d828473210cac2c01350ae9bb7b56c0247434
Mike Kasprzak <mike@sykhronics.com>
2015-05-07 11:23:21 -0400

    Updated the config sample

---
+++ config-sample.php
@@ -0,0 +1,17 @@
+<?php
+
+// Database Configuration //
+$cfg_db_host = "localhost";
+$cfg_db_name = "database";
+$cfg_db_login = "login";
+$cfg_db_password = "password";
+
+
+// Disable Debug Mode //
+//$cfg_no_debug = true;
+
+
+// By default we use MySQL. Enable this flag to use PDO. //
+//$cfg_uses_pdo = true;
+
+?>
