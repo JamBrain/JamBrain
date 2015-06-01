5edb192d131df782abdcfa47da2f1c9e8100c1b9
Mike Kasprzak <mike@sykhronics.com>
2015-06-01 18:53:02 -0400

    Added actions to login.php, and stubs for additional actions

--- public/a/login.php
+++ public/a/login.php
@@ -9,72 +9,129 @@ $response = api_newResponse();
 user_start();
 $response['uid'] = user_getId();
 
-// If already logged in, dispose of the session.
-if ( $response['uid'] !== 0 ) {
-	session_unset();	// Remove Session Variables //
-	session_destroy();	// Destroy the Session //
-	$response['uid'] = 0;
+// Retrieve Action and Arguments
+$arg = api_parseActionURL();
+$action = array_shift($arg);
+$arg_count = count($arg);
+
+// Default action
+if ( empty($action) ) {
+	$action = 'login';
 }
 
 // Generic Error Function for everything (so to not offer any hints if abusing)
 function my_loginError() {
 	api_emitErrorAndExit(400,"BAD LOGIN");
 }
+function my_logout() {
+	session_unset();	// Remove Session Variables //
+	session_destroy();	// Destroy the Session //	
+}
 
-// Check the APCU cache if access attempts for this IP address is > 5, deny access.
 
-// On failure, increase the access attempt (APCU). Timeout in 5 minutes. Log attempt.
+// Confirm we have a legal number of arguments
+if ( $arg_count > 1 ) {
+	my_loginError();
+}
 
 
-// Get login and password from $_POST //
-if ( isset($_REQUEST['l']) && isset($_REQUEST['p']) ) {
-//if ( isset($_POST['l']) && isset($_POST['p']) ) {
-//	$login = $_POST['l'];
-//	$password = $_POST['p'];
-	$login = trim($_REQUEST['l']);
-	$password = trim($_REQUEST['p']);
+// On 'login' Action, attempt to log-in given POST data.
+if ( $action === 'login' ) {
+	// Bail if not an HTTP POST
+//	if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) {
+//		my_loginError();
+//	}
+	
+	// If already logged in, dispose of the active session.
+	if ( $response['uid'] !== 0 ) {
+		my_logout();		// Destroy Session
+		user_start();		// New Session!
+		$response['uid'] = user_getId();
+	}
+		
+	// Check the APCU cache if access attempts for this IP address is > 5, deny access.
+	
+	// On failure, increase the access attempt (APCU). Timeout in 5 minutes. Log attempt.
+	
+	
+	// Get login and password from $_POST //
+	if ( isset($_REQUEST['l']) && isset($_REQUEST['p']) ) {
+	//if ( isset($_POST['l']) && isset($_POST['p']) ) {
+	//	$login = $_POST['l'];
+	//	$password = $_POST['p'];
+		$login = trim($_REQUEST['l']);
+		$password = trim($_REQUEST['p']);
+	}
+	else {
+		my_loginError();
+	}
+	
+	// Sanitize the data
+	$mail = sanitize_email($login);
+	if ( !$mail ) {
+		$login = sanitize_slug($login);
+		if ( !$login ) {
+			my_loginError();
+		}
+	}
+	
+	if ( $mail )
+		$response['mail'] = $mail;
+	else
+		$response['login'] = $login;
+	$response['pw'] = $password;
+	
+	if ( $mail ) {
+		// Search user_table for a matching e-mail.
+		
+		// if none found, fail
+	}
+	else {
+		// Search for a user-type node matching the sanitized login.
+		
+		// Search user_table for the matching UID
+	
+		// if none found, fail
+	}
+	
+	// if found, verify password against the stored hash.
+	
+	//if ( user_verifyPassword($password,$hash) ) {
+		// Success! //
+	//	user_setId( $that_user_id );
+	//}
+	//else {
+		// Password Failed //
+	//}
+	
+	
+	// ** Successfully Logged in ** //
+	// Retrieve my info //
+	
+	// Retrieve my list of Favourites, and a list of most recent posts I've loved. //
+}
+// On 'logout' action, Destroy the Session
+else if ( $action === 'logout' ) {
+	my_logout();
+}
+else if ( $action === 'register' ) {
+	// Add a user (if legal), send a verification e-mail.
+}
+else if ( $action === 'verify' ) {	
+	// Verify a previously added user given a verification URL.
+}
+else if ( $action === 'purge-retry-cache' ) {	
+	// *** Admin Only *** //
+	// Clear the Login retries cache of a specific user.
+}
+else if ( $action === 'verify-user' ) {	
+	// *** Admin Only *** //
+	// Explicitly verify a user.
 }
 else {
 	my_loginError();
 }
 
-// Sanitize the data
-$mail = sanitize_email($login);
-if ( !$mail ) {
-	$login = sanitize_slug($login);
-	if ( !$login ) {
-		api_emitErrorAndExit(400,"BAD LOGIN");
-	}
-}
-
-if ( $mail ) {
-	$response['mail'] = $mail;
-}
-else {
-	$response['login'] = $login;
-}
-$response['pw'] = $password;
-
-
-// Search user_table for a matching e-mail //
-
-// if none found, fail
-
-// if found, verify password against the stored hash.
-
-//if ( user_verifyPassword($password,$hash) ) {
-	// Password Success //
-//	user_setId( $that_user_id );
-//}
-//else {
-	// Password Failed //
-//}
-
-
-// ** Successfully Logged in ** //
-// Retrieve my info //
-
-// Retrieve my list of Favourites, and a list of most recent posts I've loved. //
-
+// Done. Output the response.
 api_emitJSON($response);
 ?>
