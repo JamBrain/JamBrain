a96978f807922e7406c34fcf637579912548b375
Mike Kasprzak <mike@sykhronics.com>
2015-06-08 01:16:30 -0400

    Added PHPDoc comments to template. Going to try generating a doc for it.

--- core/template.php
+++ core/template.php
@@ -1,53 +1,67 @@
 <?php
 
-$cmw_theme = "default";
+/**
+ * **INTERNAL**: Name of the current theme.
+*/
+$_cmw_theme = "default";
 
-function template_Include( $file, $include_once ) {
+/**
+ * **INTERNAL**: Includes template files (.php) and sets up the environment.
+ *
+ * @param {string} - Fully qualified path to a file.
+ * @param {bool} - Should we use include_once? (default: false)
+*/
+function _template_Include( $path_to_file, $include_once = false ) {
 	// Make certain global variables implicitly available to the template.
 	//global $db;
 	
 	// Include the template file.
 	if ( $include_once ) {
-		include_once $file;
+		include_once $path_to_file;
 	}
 	else {
-		include $file;
+		include $path_to_file;
 	}
 }
 
-function template_Load( $name, $include_once ) {
-	return template_Include( __DIR__."/..".CMW_STATIC_DIR.CMW_THEME_BASE."/".$name, $include_once );
+/**
+ * Instance a template.
+ *
+ * @param {string} - File to load.
+ * @param {string} - Theme to use (default: "default")
+ * @param {bool} - Should we use include_once? (default: false)
+*/
+function template_Get( $file, $theme = null, $include_once = false ) {
+	_template_Include( __DIR__."/..".CMW_STATIC_DIR.CMW_THEME_BASE."/".template_GetTheme($theme).$file, $include_once );
 }
+
 
 function template_SetTheme( $theme = null ) {
-	global $cmw_theme;
+	global $_cmw_theme;
 	if ( is_null($theme) ) {
-		$cmw_theme = "default";
+		$_cmw_theme = "default";
 	}
-	$cmw_theme = $theme;
+	$_cmw_theme = $theme;
 }
 
 function template_GetTheme( $theme = null ) {
-	global $cmw_theme;
+	global $_cmw_theme;
 	if ( is_null($theme) ) {
-		if ( !is_null($cmw_theme) ) {
-			return $cmw_theme;
+		if ( !is_null($_cmw_theme) ) {
+			return $_cmw_theme;
 		}
 		return "default";
 	}
 	return $theme;
 }
 
-function template_Get( $file, $theme = null, $include_once = false ) {
-	template_Load( template_GetTheme($theme).$file, $include_once );
-}
 
 function template_GetHeader( $theme = null, $include_once = true ) {
-	template_Load( template_GetTheme($theme)."/header.php", $include_once );
+	template_Get( "/header.php", $theme, $include_once );
 }
 
 function template_GetFooter( $theme = null, $include_once = true ) {
-	template_Load( template_GetTheme($theme)."/footer.php", $include_once );	
+	template_Get( "/footer.php", $theme, $include_once );	
 }
 
 ?>
