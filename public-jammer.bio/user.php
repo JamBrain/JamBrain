b92686a42aaaa72057acb55856d14f64d10e10c5
Mike Kasprzak <mike@sykhronics.com>
2015-07-05 14:41:22 -0400

    Cleaned up the Jammer footer CSS

--- public-jammer.bio/user.php
+++ public-jammer.bio/user.php
@@ -47,16 +47,18 @@ db_Connect();
 	padding:8px;
 	font-variant:small-caps;
 }
-/*
-.mike {
-	background:
-		url(<?php STATIC_URL(); ?>/logo/mike/Chicken16W.png),
-		#<?php echo $theme_color; ?>;
-	background-blend-mode: screen;
-}*/
+.footer img {
+	vertical-align:middle;
+}
+/*.footer .jammer { }*/
+.footer .mike {
+	margin-bottom:4px;
+	mix-blend-mode:screen;
+}
+/*.footer .ludumdare { }*/
 </style>
 <div class="footer">
-	<a href="//jammer.bio"><img src="<?php STATIC_URL(); ?>/logo/jammer/JammerLogo28W.png" height="28" alt="Jammer" title="Jammer" style="vertical-align:middle;" /></a> by <a href="http://twitter.com/mikekasprzak" target="_blank"><img class="mike" src="<?php STATIC_URL(); ?>/logo/mike/Chicken16W.png" src3="<?php STATIC_URL(); ?>/_.png" src2="http://www.gravatar.com/avatar/8266e6e52fe185d057db55021ac7bb86?s=16" width="16" height="16" alt="Mike Kasprzak" title="Mike Kasprzak" style="vertical-align:middle;margin-bottom:4px;mix-blend-mode:screen;"></a> &nbsp;|&nbsp; powered by &nbsp;<a href="http://ludumdare.com" target="_blank"><img src="<?php STATIC_URL(); ?>/logo/ludumdare/2009/LudumDareLogo20W.png" height="20" alt="Ludum Dare" title="Ludum Dare" style="vertical-align:middle;" /></a>
+	<a href="//jammer.bio"><img class="jammer" src="<?php STATIC_URL(); ?>/logo/jammer/JammerLogo28W.png" height="28" alt="Jammer" title="Jammer" /></a> by <a href="http://twitter.com/mikekasprzak" target="_blank"><img class="mike" src="<?php STATIC_URL(); ?>/logo/mike/Chicken16W.png" width="16" height="16" alt="Mike Kasprzak" title="Mike Kasprzak"></a> &nbsp;|&nbsp; powered by &nbsp;<a href="http://ludumdare.com" target="_blank"><img class="ludumdare" src="<?php STATIC_URL(); ?>/logo/ludumdare/2009/LudumDareLogo20W.png" height="20" alt="Ludum Dare" title="Ludum Dare" /></a>
 </div>
 
 </body>
