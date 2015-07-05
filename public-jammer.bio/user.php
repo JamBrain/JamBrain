a5225a2955e6d116e143038ae7c46d67b1b04488
Mike Kasprzak <mike@sykhronics.com>
2015-07-05 13:22:20 -0400

    Logo refinements for the Jammer footer, and a B+W Chicken

--- public-jammer.bio/user.php
+++ public-jammer.bio/user.php
@@ -29,7 +29,7 @@ db_Connect();
 
 <br />
 <br />
-<div style="text-align:center;background:#000;color:#FFF;padding:8px;font-variant:small-caps"><img src="<?php STATIC_URL(); ?>/logo/jammer/JammerLogo28W.png" height="28" alt="Jammer" title="Jammer" style="vertical-align:middle;" /> by <a href="http://twitter.com/mikekasprzak" target="_blank"><img src="http://www.gravatar.com/avatar/8266e6e52fe185d057db55021ac7bb86?s=16" width="16" height="16" style="vertical-align:middle;"></a> | powered by <a href="http://ludumdare.com" target="_blank"><img src="<?php STATIC_URL(); ?>/logo/ludumdare/2009/LudumDareLogo20W.png" height="20" alt="Ludum Dare" title="Ludum Dare" style="vertical-align:middle;" /></a></div>
+<div style="text-align:center;background:#000;color:#FFF;padding:8px;font-variant:small-caps"><a href="//jammer.bio"><img src="<?php STATIC_URL(); ?>/logo/jammer/JammerLogo28W.png" height="28" alt="Jammer" title="Jammer" style="vertical-align:middle;" /></a> by <a href="http://twitter.com/mikekasprzak" target="_blank"><img src="<?php STATIC_URL(); ?>/logo/mike/Chicken16W.png" src2="http://www.gravatar.com/avatar/8266e6e52fe185d057db55021ac7bb86?s=16" width="16" height="16" alt="Mike Kasprzak" title="Mike Kasprzak" style="vertical-align:middle;"></a> &nbsp;|&nbsp; powered by &nbsp;<a href="http://ludumdare.com" target="_blank"><img src="<?php STATIC_URL(); ?>/logo/ludumdare/2009/LudumDareLogo20W.png" height="20" alt="Ludum Dare" title="Ludum Dare" style="vertical-align:middle;" /></a></div>
 
 </body>
 <?php template_GetFooter(); ?>
