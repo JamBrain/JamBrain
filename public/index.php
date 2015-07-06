5fc8f39c61e13246fefb8033d5120b3c40c0aea4
Mike Kasprzak <mike@sykhronics.com>
2015-07-06 01:31:03 -0400

    further tweaks to ludum dare index

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ require_once __DIR__ . "/../core/internal/emoji.php";
 require_once __DIR__ . "/../core/post.php";
 
 user_StartSession();
+//template_SetTheme("embed");
 
 // Modes //
 const M_DEFAULT = 1;	// Default State //
@@ -21,27 +22,44 @@ $args_count = count($args);
 // Sanitize Input
 foreach( $args as $arg ) {
 	$arg = sanitize_Slug($arg);
-}
-
-$args_merged = implode('/',$args);
-
-
-$paths = [ CMW_NODE_ROOT ];
-foreach( $args as $key => $slug ) {
-	$id = node_GetNodeIdByParentIdAndSlug($paths[$key],$slug);
-	if ( !empty($id) ) {
-		$paths[] = $id;
+	if ( empty($arg) ) {
+		$mode = M_ERROR;
+		break;
 	}
 }
-$paths_count = count($paths);
-$this_id = &$paths[$paths_count-1];
 
-$nodes = node_GetNodesByParentId( $this_id );
-$nodes_count = count($nodes);
-
-//print_r($nodes);
-
-//template_SetTheme("embed");
+if ( $mode > 0 ) {
+	$args_merged = implode('/',$args);
+	
+	
+	$paths = [ CMW_NODE_ROOT ];
+	foreach( $args as $key => $slug ) {
+		$id = node_GetNodeIdByParentIdAndSlug($paths[$key],$slug);
+		if ( !empty($id) ) {
+			$paths[] = $id;
+		}
+		else {
+			$mode = M_ERROR;
+			break;			
+		}
+	}
+	if ( $mode > 0 ) {
+		$paths_count = count($paths);
+		$this_node_id = &$paths[$paths_count-1];
+		
+		$this_node = node_GetNodeById( $this_node_id );
+		$author_node = [];
+		if ( !empty($this_node['author']) && ($this_node['author'] > 0) ) {
+			$author_node = node_GetNodeById( $this_node['author'] );
+		}
+		if ( !empty($this_node['parent']) && ($this_node['parent'] > 0) ) {
+			$parent_node = node_GetNodeById( $this_node['parent'] );
+		}
+		
+		$nodes = node_GetNodesByParentId( $this_node_id );
+		$nodes_count = count($nodes);
+	}
+}
 
 ?>
 <?php template_GetHeader(); ?>
@@ -51,6 +69,9 @@ $nodes_count = count($nodes);
 	</div>
 	
 	<style>
+		body {
+			background: #EEE;
+		}
 		#nav .row {
 			margin-left:16px;
 			padding:2px;
@@ -72,11 +93,31 @@ $nodes_count = count($nodes);
 			position:absolute;
 			left:320px;
 		}
+		
+		#content {
+			margin:16px;
+			padding:8px;
+			border:2px solid #000;
+			background:#FFF;
+		}
+		
+		#content h1, #content h3 {
+			margin:0;
+		}
+		#content .body {
+			margin-top:12px;
+		}
 	</style>
+
+	<?php if ( $mode === M_DEFAULT ) { ?>	
+	<div id="content">
+		<div class="title"><h1><?php echo $this_node['name']; ?></h1><?php if ( $author_node ) { echo "<h3>by <a href='/user/".$author_node['slug']."'>" . $author_node['name'] . "</a></h3>"; } ?></div>
+	<?php if ( !empty($this_node['body']) ) { ?><div class="body"><?php echo $this_node['body']; ?></div><?php } ?>
+	</div>
 	
 	<div id='nav' style="padding:16px;">
 		<?php
-			echo "/" . $args_merged . " [".$this_id."]<br />\n";
+			echo "/" . $args_merged . " [".$this_node_id."]<br />\n";
 			if ( $args_count >= 1 ) {
 				echo "<div class='row'>\n";
 				echo "<div class='slug'><a href='../'>../</a></div><br />\n";
@@ -88,13 +129,21 @@ $nodes_count = count($nodes);
 				echo "</div>\n";	
 			}
 		?>
-	</div>	
+	</div>
+
+	<?php } else if ( $mode === M_ERROR ) { ?>	
+		<div id="content">
+			<div class="title"><h1>Nope</h1></div>
+		</div>
+
+	<?php } /* $mode */ ?>	
+
 	<script>
 		emojione.ascii = true;
 	
 		// Process Emoji on all the following sections //
 		var Section = [
-			'nav','message'
+			'nav','message','content'
 		];
 		for(var Key in Section) {
 			var el = document.getElementById(Section[Key]);
