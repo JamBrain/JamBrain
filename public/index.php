5fb14612ba63df2edcd14a90bf5cd8209d2b97c5
Mike Kasprzak <mike@sykhronics.com>
2015-07-07 03:45:41 -0400

    some Ludum Dare bar experimentation

--- public/index.php
+++ public/index.php
@@ -64,6 +64,185 @@ if ( $mode > 0 ) {
 ?>
 <?php template_GetHeader(); ?>
 <body>
+	<script>
+		// Cookie Functions //
+		function getCookie(name) {
+			var value = "; " + document.cookie;
+			var parts = value.split("; " + name + "=");
+			if (parts.length == 2) 
+				return parts.pop().split(";").shift();
+			return parts;
+		}
+		function setCookie(name,value) {
+			document.cookie = name + '=' + value;
+		}
+		function removeCookie( name ) {
+			document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+		}
+	</script>
+	
+	<style>
+		/* Top Navigation Bar */
+		#ldbar-outer {			
+			width:100%;
+			height:48px;
+
+			/*position:fixed;*/
+			left:0;
+			top:0;
+			z-index:1000;
+			
+			/* shadow */
+			border-bottom:1px solid #000;/*rgba(96,96,96,0.2);*/
+		}
+		#ldbar-inner {
+			color:#fff;
+			background:/*#765*/ rgba(96,96,96,1.0);
+			
+			line-height:47px;
+			
+			position:relative;
+			width:100%;
+		}
+		/* Padding in the main document */
+		#ldbar-pad {			
+			width:100%;
+			height:49px; /* plus shadow */
+		}
+
+		.noselect {
+		    -webkit-touch-callout: none;
+		    -webkit-user-select: none;
+		    -khtml-user-select: none;
+		    -moz-user-select: none;
+		    -ms-user-select: none;
+		    user-select: none;
+		}
+		#ldbar img {
+			/*vertical-align:middle;*/
+		}
+		
+		#ldbar-toggle {
+			font-family:monospace;
+			margin-left:32px;
+			cursor:pointer;
+		}
+		
+		#ldbar-notifications {
+			font-size:20px;
+			margin-left:24px;
+			font-weight:bold;
+			border-radius:9px;
+			cursor:pointer;
+			display:inline-block !important;
+			width:32px;
+			height:32px;
+			line-height:32px;
+			text-align:center;
+			font-family:monospace;
+			position:relative;
+			text-shadow:0 2px rgba(0,0,0,0.5);
+			
+			background:rgba(255,32,48,0.5);
+			border:3px solid #000;
+			top:0px;
+
+   			-webkit-transition:all 0.125s;
+    		transition:all 0.125s;
+		}
+		#ldbar-notifications:hover {
+			background:rgba(255,32,48,0.8);
+			border-top:1px solid #000;
+			border-bottom:5px solid #000;
+			top:-2px;
+		}
+
+		#ldbar-logo {
+			position:relative;
+			cursor:pointer;
+			
+			top:9px;
+			opacity:0.7;
+ 
+   			-webkit-transition:all 0.125s;
+    		transition:all 0.125s;
+		}
+		#ldbar-logo:hover {
+			top:6px;
+			opacity:1.0;
+		}
+
+
+		#ldbar .leftnav {
+			display:inline;
+			padding-left:64px;
+			
+		}
+		#ldbar .rightnav {
+			float:right;
+			display:inline;
+			/*padding-right:64px;*/
+		}
+		
+		
+		#ldbar .cell {
+			display:inline;
+		}
+		
+		#ldbar .size32 {
+			position:relative;
+			width:32px;
+			height:32px;
+			top:8px;
+		}
+
+	</style>
+	
+	<div id='ldbar' class='noselect'>
+		<div id='ldbar-outer' style='position:fixed;'>
+			<div id='ldbar-inner'>
+				<div class='leftnav'>
+					<div id='ldbar-logo' class='cell'><img src="<?php STATIC_URL(); ?>/logo/ludumdare/2009/LudumDareLogo80W.png" height="30" /></div>
+				</div>
+				<div class='rightnav'>
+					<div class='cell size32'><img src="<?php STATIC_URL(); ?>/logo/mike/chicken64W.png" width="32" height="32" style="mix-blend-mode:screen" /></div>
+					<div id='ldbar-notifications' class='cell' >5</div>
+					<div id='ldbar-toggle' class='cell' onclick='toggleLDBar();' title='Toggle Sticky Bar'>[x]</div>
+				</div>
+			</div>
+		</div>
+		<div id='ldbar-pad'></div>
+	</div>
+	
+	<script>
+		function updateLDBar() {
+			if ( getCookie('ldbar') === '1' ) {
+				document.getElementById('ldbar-outer').style.position = '';
+				document.getElementById('ldbar-pad').style.display = 'none';
+				document.getElementById('ldbar-toggle').innerHTML = '[ ]';
+			}
+			else {
+				document.getElementById('ldbar-outer').style.position = 'fixed';
+				document.getElementById('ldbar-pad').style.display = '';
+				document.getElementById('ldbar-toggle').innerHTML = '[x]';
+			}
+		}
+		function toggleLDBar() {
+			var val = !(getCookie('ldbar') === '1');
+			if ( val ) {
+				setCookie('ldbar',1);
+			}
+			else {
+				removeCookie('ldbar');
+			}
+			updateLDBar();
+		}
+		updateLDBar();
+		
+	</script>
+	
+	<!-- *************** -->
+	
 	<div id='message' style="padding:16px;">
 		:D :ca: :football: :se: :house: :grin:
 	</div>
