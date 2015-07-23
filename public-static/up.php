0a35c73d64102e328d74ce45f20c532f8270686d
Mike Kasprzak <mike@sykhronics.com>
2015-07-23 16:49:57 -0400

    okay, now being less specific with includes

--- public-static/up.php
+++ public-static/up.php
@@ -5,8 +5,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
-require_once __DIR__."/../core/internal/json.php";
-require_once __DIR__."/../core/node.php";
+require_once __DIR__."/../api.php";
 
 $response = json_NewResponse();
 
