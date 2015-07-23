af0fc5a9c0c7b56735d96dfd6457bce2e282de88
Mike Kasprzak <mike@sykhronics.com>
2015-07-23 16:37:56 -0400

    Added stub uploader script, and a redis test

--- public-static/custom/emojitwo/README.md
+++ public-static/custom/emojitwo/README.md
@@ -2,5 +2,4 @@
 
 EmojiTwo.js is a fork of EmojiOne by Rick Moby. http://emojione.com/
 
-It strips away redundant and less useful data from the original `emojione.js`, and makes it a little less flexible. It's about half the size.
-
+It strips away redundant and less useful data from the original `emojione.js`, and makes it less flexible. As a result, it's about half the size.
---
+++ public-static/up.php
@@ -0,0 +1,48 @@
+<?php
+/* Image Uploader */
+
+require_once __DIR__."/../api.php";
+require_once __DIR__."/../core/node.php";
+
+$response = json_NewResponse();
+
+user_StartEnd();
+
+// Retrieve Action and Arguments
+$arg = core_ParseActionURL();
+$node_id = intval(array_shift($arg));
+$arg_count = count($arg);
+
+$response['node'] = $node_id;
+
+$redis = new Redis();
+$redis->connect('127.0.0.1');
+
+$val = $redis->incr('key');
+$response['value'] = $val;
+
+$redis->close();
+
+/*
+// From: http://php.net/manual/en/features.file-upload.multiple.php
+function reArrayFiles(&$file_post) {
+    $file_ary = array();
+    $file_count = count($file_post['name']);
+    $file_keys = array_keys($file_post);
+
+    for ($i=0; $i<$file_count; $i++) {
+        foreach ($file_keys as $key) {
+            $file_ary[$i][$key] = $file_post[$key][$i];
+        }
+    }
+
+    return $file_ary;
+}
+$_FILES = reArrayFiles($_FILES);
+
+$response['files'] = $_FILES;
+
+*/
+
+json_Emit($response);
+?>
