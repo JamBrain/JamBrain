8be372c373b16fabc2cfbe70338fce80fdb06dca
Mike Kasprzak <mike@sykhronics.com>
2015-07-23 16:51:38 -0400

    disabling errors (warnings), and done. Redis test on shocker should be good

--- public-static/up.php
+++ public-static/up.php
@@ -1,9 +1,9 @@
 <?php
 /* Image Uploader */
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
+//ini_set('display_errors',1);
+//ini_set('display_startup_errors',1);
+//error_reporting(-1);
 
 require_once __DIR__."/../api.php";
 
