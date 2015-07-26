b2b76a50701d6b0ec2d2b614cf8c5f889f699f48
Mike Kasprzak <mike@sykhronics.com>
2015-07-26 14:59:53 -0400

    changed times for describing a Ludum Dare event in to relative times. OMFG! I LOVE THIS!

--- core/schedule.php
+++ core/schedule.php
@@ -9,26 +9,25 @@ require_once __DIR__ . "/../db.php";
 
 // NOTE: None of these functions sanitize. Make sure you sanitize your input! //
 
-function schedule_Add( $start, $end, $name, $extra = [], $type = "", $parent = 0, $priority = 0.0 ) {
+function schedule_Add( $start, $end, $name, $extra = null, $type = "", $subtype = "", $parent = 0, $priority = 0.0 ) {
 	db_Connect();
 	
 	// Turn EXTRA in to a JSON string //
-	if ($extra === null) {
-		$json = "{}";
+	if ( empty($extra) ) {
+		$json = "";
 	}
 	else {
 		$json = json_encode($extra);
 		if ( empty($json) )
-			return null;
+			$json = "";
 	}
-	
-	$json = db_EscapeString($json);
 
 	db_Query(
 		"INSERT IGNORE `" . CMW_TABLE_SCHEDULE . "` (".
 			"`parent`,".
 			"`priority`,".
 			"`type`,".
+			"`subtype`,".
 			"`start`,".
 			"`end`,".
 			"`name`,".
@@ -37,16 +36,17 @@ function schedule_Add( $start, $end, $name, $extra = [], $type = "", $parent = 0
 		"VALUES (" .
 			intval($parent) . "," .
 			floatval($priority) . "," .
-			"\"".$type . "\"," .
-			"\"".$start . "\"," .
-			"\"".$end . "\"," .
-			"\"".$name . "\"," .
-			"\"".$json . "\"" .
+			"\"".db_EscapeString($type)."\"," .
+			"\"".db_EscapeString($subtype)."\"," .
+			"\"".db_EscapeString($start)."\"," .
+			"\"".db_EscapeString($end)."\"," .
+			"\"".db_EscapeString($name)."\"," .
+			"\"".db_EscapeString($json)."\"" .
 		");");
 		
 	// TODO: do something on db_query error
 
-	return !empty(db_AffectedRows());
+	return db_GetId();
 }
 
 
