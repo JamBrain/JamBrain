acedbc0862d1260cfa687f2a5d150456948148c1
Mike Kasprzak <mike@sykhronics.com>
2015-11-07 22:40:47 -0500

    due to how opcache lookup function works, adjusted how extra info is generated

--- public-static/themes/default/template/footer.html.php
+++ public-static/themes/default/template/footer.html.php
@@ -2,16 +2,8 @@
 	if (!isset($GLOBALS['HTML_NO_FOOTER'])) {
 		echo (isset($GLOBALS['HTML_SHOW_FOOTER'])) ? '<!-- ' : '<div class="footer">';
 		
-		$FOOTER_DATA_POINT=0; 
-		
-		if (function_exists('opcache_is_script_cached')) {
-			echo ' ' . (opcache_is_script_cached(__FILE__) ? "Executed" : "Compiled and Executed");
-		}
-		else {
-			echo "Generated";
-		}
-		
-		echo " in <strong>".php_GetExecutionTime()."</strong>";
+		$FOOTER_DATA_POINT=0; 		
+		echo "Generated in <strong>".php_GetExecutionTime()."</strong>";
 		
 		if (function_exists('db_GetQueryCount')) { 
 			if ($FOOTER_DATA_POINT++ > 0) { echo ','; } 
@@ -20,6 +12,17 @@
 		if (function_exists('cache_GetReads')) {
 			if ($FOOTER_DATA_POINT++ > 0) { echo ','; } 
 			echo ' ' . cache_GetReads() . ' cache read(s), '. cache_GetWrites() .' cache write(s)';
+		}
+
+		if (function_exists('opcache_is_script_cached')) {
+			if (!opcache_is_script_cached(__FILE__)) {
+				if ($FOOTER_DATA_POINT++ > 0) { echo ','; }
+				echo "with opcache disabled");
+			}
+		}
+		else {
+			if ($FOOTER_DATA_POINT++ > 0) { echo ','; }
+			echo "without opcache";
 		}
 
 		echo (isset($GLOBALS['HTML_SHOW_FOOTER'])) ? ' -->' : '</div>'; 
