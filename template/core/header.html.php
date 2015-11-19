ce57a8e897b49301417fad5d446c89ee853df6fb
Mike Kasprzak <mike@sykhronics.com>
2015-11-19 17:41:30 -0500

    Added time conversion functions

--- template/core/header.html.php
+++ template/core/header.html.php
@@ -21,6 +21,7 @@
 		if ( defined('HTML_USE_CORE') ) {
 			echo "<script src='".CMW_STATIC_URL."/internal/src/lib.js".VERSION_STRING."'></script>\n";
 			echo "<script src='".CMW_STATIC_URL."/internal/src/locale.js".VERSION_STRING."'></script>\n";
+			echo "<script src='".CMW_STATIC_URL."/internal/src/time.js".VERSION_STRING."'></script>\n";
 			echo "<script src='".CMW_STATIC_URL."/internal/src/xhr.js".VERSION_STRING."'></script>\n";
 		}
 		if ( defined('HTML_USE_STARSHIP') ) {
