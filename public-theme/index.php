711087542dc1ad6fa150d300c1ebbfdcd66b882f
Mike Kasprzak <mike@sykhronics.com>
2015-11-29 21:30:55 -0500

    Added support for both local and live server

--- public-theme/index.php
+++ public-theme/index.php
@@ -152,8 +152,12 @@ function ShowHeadline() {
 
 function ShowLogin() {
 	$LOGIN_URL = LEGACY_LOGIN_URL;
-	if ( isset($_GET['beta']) )
-		$LOGIN_URL .= "?beta";
+	if ( isset($_GET['beta']) ) {
+		if ( strpos($LOGIN_URL,"?") === false )
+			$LOGIN_URL .= "?beta";
+		else
+			$LOGIN_URL .= "&beta";
+	}
 ?>
 	<div class="action" id="action-login">
 		<a class="no-style" href="<?=$LOGIN_URL?>"><button type="button" class="login-button">Login</button></a>
