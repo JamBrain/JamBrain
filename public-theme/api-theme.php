70cf7a365d649a1879a708711cd0d151c718559d
Mike Kasprzak <mike@sykhronics.com>
2015-12-11 20:01:21 -0500

    forgot the round

--- public-theme/api-theme.php
+++ public-theme/api-theme.php
@@ -8,7 +8,7 @@ require_once __DIR__."/../core/legacy_user.php";
 config_Load();
 
 $EVENT_NAME = "Ludum Dare 34";
-$EVENT_MODE = 4;
+$EVENT_MODE = 5;
 $EVENT_NODE = 100;//intval($CONFIG['event-active']);//isset($_POST['node']) ? intval($_POST['node']) : 0;
 $EVENT_DATE = new DateTime("2015-12-12T02:00:00Z");
 
