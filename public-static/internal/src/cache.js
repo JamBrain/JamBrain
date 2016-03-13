989da24c6a66434e717e66a68493a6e31aeceab4
Mike Kasprzak <mike@sykhronics.com>
2016-03-13 00:29:53 -0500

    Tested cache.js, now working

--- public-static/internal/src/cache.js
+++ public-static/internal/src/cache.js
@@ -12,7 +12,7 @@ var dataPrefix = "!!$";
 // TODO: Add checks for setItem failure (i.e. memory full).
 //   Then do a flush and try again, before failing.
 
-window.cache_Store( key, value, ttl ) {
+window.cache_Store = function( key, value, ttl ) {
 	// Store TTL first, then Data //
 	if ( ttl )
 		storage.setItem( ttlPrefix+key, Date.now()+ttl );
@@ -24,39 +24,42 @@ window.cache_Store( key, value, ttl ) {
 	return true;
 }
 
-window.cache_Fetch( key ) {
+window.cache_Exists = function( key ) {
 	var ttl = storage.getItem( ttlPrefix+key );
-	
+
 	// Does it exist? //
-	if ( !ttl ) {
-		return null;
+	if ( ttl == null ) {
+		return false;
 	}
 	// Has the TTL expired? //
 	else if ( Date.now() > ttl ) {
 		// Remove in reverse order, just in case //
 		storage.removeItem( dataPrefix+key );
 		storage.removeItem( ttlPrefix+key );
-		return null;
+		return false;
 	}
+	
+	return true;
+}
+
+window.cache_Fetch = function( key ) {
+	if ( !cache_Exists(key) )
+		return null;
 
 	// Return Data //
 	return storage.getItem( dataPrefix+key );
 }
 
-window.cache_Touch( key, new_ttl ) {
-	var ttl = storage.getItem( ttlPrefix+key );
+window.cache_Create = function( key, value, ttl ) {
+	if ( cache_Exists(key) )
+		return false;
+	
+	return cache_Store( key, value, ttl );
+}
 
-	// Does it exist? //
-	if ( !ttl ) {
+window.cache_Touch = function( key, new_ttl ) {
+	if ( !cache_Exists(key) )
 		return null;
-	}
-	// Has the TTL expired? //
-	else if ( Date.now() > ttl ) {
-		// Remove in reverse order, just in case //
-		storage.removeItem( dataPrefix+key );
-		storage.removeItem( ttlPrefix+key );
-		return null;
-	}
 
 	// Update the TTL //
 	if ( new_ttl )
@@ -68,24 +71,13 @@ window.cache_Touch( key, new_ttl ) {
 	return true;
 }
 
-window.cache_Exists( key ) {
-	return storage.getItem( ttlPrefix+key ) != null;
-}
-
-window.cache_Create( key, value, ttl ) {
-	if ( cache_Exists(key) )
-		return false;
-	
-	return cache_Store( key, value, ttl );
-}
-
 // Remove expired items. 
 // Optionally, specify the maximum number of items to remove.
 //
 // NOTE: Due to differences in browser implementations, this is not guaranteed
 //   to remove all expired items. But if there is one, it will remove it.
 //   Think of this as a single pass cache flush that *may* catch everything.
-window.cache_Flush( max_items ) {
+window.cache_Flush = function( max_items ) {
 	var itemsRemoved = 0;
 	// Reverse order, just in case keys are like an array, so removing
 	// elements off the end wont give us bad indexes
@@ -93,7 +85,7 @@ window.cache_Flush( max_items ) {
 		var key = storage.key(idx);
 		// For this, we only care about items with the ttlPrefix
 		if ( key.indexOf(ttlPrefix) === 0 ) {
-			if ( Date.now() > storage.getItem( ttlPrefix+key ) {
+			if ( Date.now() > storage.getItem(ttlPrefix+key) ) {
 				// Remove in reverse order, just in case
 				storage.removeItem( dataPrefix+key );
 				storage.removeItem( ttlPrefix+key );
---
+++ public/cache-js-test.php
@@ -0,0 +1,22 @@
+<?php 
+require_once __DIR__ . "/../web.php";
+
+// HTML Library Configuration //
+const HTML_USE_CORE = true;
+const HTML_USE_STARSHIP = true;
+const HTML_SHOW_FOOTER = true;
+
+?>
+<?php template_GetHeader(); ?>
+hey <br />
+<br />
+<span onclick="if ( cache_Store('moofer',10,5000) ) console.log(this.innerHTML);">[Store 10]</span>
+<span onclick="if ( cache_Store('moofer',20,5000) ) console.log(this.innerHTML);">[Store 20]</span>
+<span onclick="if ( cache_Create('moofer',30,5000) ) console.log(this.innerHTML);">[Create 30]</span>
+<span onclick="console.log(this.innerHTML,cache_Fetch('moofer'));">[Read]</span>
+<span onclick="console.log(this.innerHTML,cache_Exists('moofer'));">[Exists]</span>
+<span onclick="if ( cache_Touch('moofer',5000) ) console.log(this.innerHTML);">[Touch]</span>
+<span onclick="cache_Flush(); console.log(this.innerHTML);">[Flush]</span>
+<br />
+<br />
+<?php template_GetFooter(); ?>
