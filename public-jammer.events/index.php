32132c5c8217f83406a559fdb8a8cf0e82bfdc03
Mike Kasprzak <mike@sykhronics.com>
2016-03-14 19:34:34 -0400

    fixed the firefox cache bug. rough calender

--- public-jammer.events/index.php
+++ public-jammer.events/index.php
@@ -5,13 +5,31 @@ require_once __DIR__."/../web.php";
 template_GetPageHeader();
 ?>
 <style>
+	._hidden {
+		display:hidden;
+	}
+	._invisible {
+		visibility:hidden;
+	}
+	._nosel {
+		-webkit-touch-callout: none;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
+		
 	.day {
 		display:inline-block;
-		width:80px;
-		height:60px;
+		cursor:pointer;
 		
 		border-top:1px solid #888;
 		border-left:1px solid #888;
+		
+		padding:2px 4px;
+		
+		width:80px;
+		height:60px;
 	}
 	.day:hover {
 		background:#DDD;
@@ -26,23 +44,36 @@ template_GetPageHeader();
 	}
 	
 	.-saturday, .-sunday {
-		background:#EEE;		
+		background:#EEE;
+	}
+	
+	.-next {
+		background:#DDF;
+	}
+	.-next:hover {
+		background:#BBD;
 	}
 </style>
-<div class="month">
+<div class="month _nosel">
 	<?php
 	for ( $week = 0; $week < 5; $week++ ) {
 	?><div class="week"><?php
-		for ( $day = 0; $day < 7; $day++ ) {
-			if ( $day == 0 ) {
-				?><div class="day -sunday"><?= (($day+1)+($week*7)) ?></div><?php
+		for ( $weekday = 0; $weekday < 7; $weekday++ ) {
+			$day = (($weekday+1)+($week*7));
+			$div_class = "day";
+			
+			if ( $weekday == 0 ) {
+				$div_class .= " -sunday";
 			}
-			else if ( $day == 6 ) {
-				?><div class="day -saturday"><?= (($day+1)+($week*7)) ?></div><?php
+			else if ( $weekday == 6 ) {
+				$div_class .= " -saturday";
 			}
-			else {
-				?><div class="day"><?= (($day+1)+($week*7)) ?></div><?php
+			
+			if ($day > 31) {
+				$div_class .= " -next";
 			}
+			
+			?><div class="<?=$div_class?>"><?=$day?></div><?php
 		}
 	?></div><?php
 	}
