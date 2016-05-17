6af673bd6f8d06c1c2823da4b0abfb3b309d3bea
Mike Kasprzak <mike@sykhronics.com>
2016-05-17 16:14:54 -0400

    started but then reconsidered doing a general URL redirector for downloads. Instead, they should be tied to content ids

---
+++ public-com/v1/download.php
@@ -0,0 +1,17 @@
+<?php
+
+// Wikipedia: https://en.wikipedia.org/wiki/Uniform_Resource_Locator
+// scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]
+
+// download/scheme/[user[/password[[/--/port[/---/fragment]]]]/-/host[/]path[?query]
+
+//$scheme = $_SERVER['REQUEST_SCHEME']; // "http";
+
+// TODO: Punycode support for host ( https://en.wikipedia.org/wiki/Punycode )
+
+//parse_url
+
+
+// Actually no. Don't do the above.
+
+// This should be handled by Ids (of game), and Sub Ids (of which link). 
