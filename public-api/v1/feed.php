298fc3df66273fb4afad22e3cae938701bf75616
Mike Kasprzak <mike@sykhronics.com>
2016-06-18 13:43:17 -0400

    Added stub for the feed call

---
+++ public-api/v1/feed.php
@@ -0,0 +1,17 @@
+<?php
+
+// ** Feeds ** //
+// feed/my - my feed (who you follow intermixed with popular)
+// feed/pop - popular (love)
+// feed/all - all
+// feed/ID - all items under a node (numeric)
+
+// ** Filters ** //
+// ?type[]=GAME - item types only
+
+// NOTE: public "USER" feed should be delayed (i.e. query should only return values older than 1 hour)
+
+
+// Returns:
+// - ID
+// - TimeStamp
