87d50a99e179c217024b48491c30f448531e2462
Mike Kasprzak <mike@sykhronics.com>
2016-06-17 20:18:13 -0400

    Added dummy domains for proper cookie subdomain testing

--- public-api/v1/status.php
+++ public-api/v1/status.php
@@ -18,10 +18,8 @@ require_once __DIR__."/../../api.php";
 //	json_EmitError(401);
 //}
 
+// ** Begin ** //
 $response = json_NewResponse();
-
-$response['name'] = gethostname();
-$response['php_version'] = PHP_VERSION;
 
 $start_time = time();
 function time_offset() {
@@ -29,63 +27,138 @@ function time_offset() {
 	return $start_time - time();
 }
 
-if ( defined('CMW_USING_APCU') ) {
-	$response['apcu_version'] = phpversion('apcu');
+$need_admin = false;
+$show_specific = false;
+$show_db = false;
+$show_redis = false;
+$show_memcached = false;
+$is_admin = false;
+
+// Parse action URL
+$action = core_ParseActionURL();
+//$response['action'] = $action;
+
+foreach ( $action as $key => &$value ) {
+	if ( $action[$key] == "db" ) {
+		$show_db = true;
+		$need_admin = true;
+	}
+	else if ( $action[$key] == "redis" ) {
+		$show_redis = true;
+		$need_admin = true;
+	}
+	else if ( $action[$key] == "memcached" ) {
+		$show_memcached = true;
+		$need_admin = true;
+	}
+	else if ( $action[$key] == "all" ) {
+		$show_db = true;
+		$show_redis = true;
+		$show_memcached = true;	
+		$need_admin = true;
+	}
+
+	if ( $action[$key] == "admin" ) {
+		$is_admin = true;	// hack
+	}
+	
+	
+	if ( $action[$key] == "db" || $action[$key] == "redis" || $action[$key] == "memcached" ) {
+		$show_specific = true;
+	}
+}
+
+if ( $need_admin ) {
+	// TODO: Do authentication check
+//	if ( blah ) {
+//		$is_admin = true;
+//	}
+}
+
+// PHP //
+if ( !$show_specific ) {
+	$response['name'] = gethostname();
+	$response['php_version'] = PHP_VERSION;
+}
+
+// OpCache //
+//if ( !$show_specific ) {
+//}
+
+// APCu //
+if ( defined('CMW_USING_APCU') && !$show_specific ) {
+	$response['apcu_api_version'] = phpversion('apcu');
 
 	$response['apcu_uptime'] = time() - intval(apcu_cache_info(true)['start_time']);
 //	$response['apcu'] = apcu_cache_info(true);
 }
 
-if ( defined('CMW_USING_REDIS') ) {
-	$response['redis_version'] = phpversion('redis');
+// Database //
+if ( defined('CMW_USING_DB') && $show_db ) {
+	$response['db_api_version'] = phpversion('mysqli');
 
-	$redis = new Redis();
-	$redis->connect(CMW_REDIS_HOST);
-	$response['redis_uptime'] = time_offset() + intval($redis->info('default')['uptime_in_seconds']);
-	$redis->close();
+	if ( $is_admin ) {
+		require_once __DIR__."/../../db.php";
+		
+		$db_data = db_QueryFetchPair("show VARIABLES like \"%version%\"");
+		$response['db_version'] = $db_data['version'];
+	
+		$db_data = db_QueryFetchPair("show global status where Variable_Name = 'Uptime';");
+		$response['db_uptime'] = time_offset() + intval($db_data['Uptime']);
+	}
 }
 
-if ( defined('CMW_USING_DB') ) {
-	require_once __DIR__."/../../db.php";
+// Redis //
+if ( defined('CMW_USING_REDIS') && $show_redis ) {
+	$response['redis_api_version'] = phpversion('redis');
 
-	$db_data = db_QueryFetchPair("show VARIABLES like \"%version%\"");
-	$response['db_version'] = $db_data['version'];
+	if ( $is_admin ) {
+		// TODO: Get Redis Server Version //
 
-	$db_data = db_QueryFetchPair("show global status where Variable_Name = 'Uptime';");
-	$response['db_uptime'] = time_offset() + intval($db_data['Uptime']);
+		$redis = new Redis();
+		$redis->connect(CMW_REDIS_HOST);
+		$response['redis_uptime'] = time_offset() + intval($redis->info('default')['uptime_in_seconds']);
+		$redis->close();
+	}
 }
 
-if ( defined('CMW_USING_MEMCACHED') ) {
+// Memcached //
+if ( defined('CMW_USING_MEMCACHED') && $show_memcached ) {
 	$response['memcached_version'] = phpversion('memcached');
 
-	$m = new Memcached();
-	$m->addServer(CMW_MEMCACHED_HOST, CMW_MEMCACHED_PORT);
-	
-	$m_data = $m->getStats();
-	//$response['memcached'] = [];							// If multiple servers
-	
-	foreach ( $m_data as $key => $value ) {
-		//$response['memcached'][$key] = $value['uptime'];	// If multiple servers
-		$response['memcached_uptime'] = time_offset() + $value['uptime'];
+	if ( $is_admin ) {
+		// TODO: Get Memcached Server Version //
+		
+		$m = new Memcached();
+		$m->addServer(CMW_MEMCACHED_HOST, CMW_MEMCACHED_PORT);
+		
+		$m_data = $m->getStats();
+		//$response['memcached'] = [];							// If multiple servers
+		
+		foreach ( $m_data as $key => $value ) {
+			//$response['memcached'][$key] = $value['uptime'];	// If multiple servers
+			$response['memcached_uptime'] = time_offset() + $value['uptime'];
+		}
+		
+		$m->quit();
 	}
-	
-	$m->quit();
 }
 
-if ( defined('CMW_USING_IMAGEMAGICK') ) {
+// Command Line Tools //
+if ( defined('CMW_USING_IMAGEMAGICK') && !$show_specific && $is_admin ) {
 	// TODO: this	
 }
 
-if ( defined('CMW_USING_PHP_IMAGEMAGICK') ) {
+if ( defined('CMW_USING_PHP_IMAGEMAGICK') && !$show_specific && $is_admin ) {
 	// TODO: this
 	// NOTE: This is the PHP library ImageMagick, not the command line tool
 }
 
-if ( defined('CMW_USING_PNGQUANT') ) {
+if ( defined('CMW_USING_PNGQUANT') && !$show_specific && $is_admin ) {
 	// TODO: this	
 }
 
-if ( defined('CMW_USING_FFMPEG') ) {
+if ( defined('CMW_USING_FFMPEG') && !$show_specific && $is_admin ) {
 	// TODO: this	
 }
 
--- scripts/apache/add-ports.sh
+++ scripts/apache/add-ports.sh
@@ -8,7 +8,15 @@ cd $SCRIPTPATH
 
 echo "Adding additional server ports to Apache"
 cat apache-ports-conf.txt>>/etc/apache2/ports.conf
-cat apache-default-conf.txt>>/etc/apache2/sites-available/000-default.conf
+
+echo "Adding additional virtual hosts"
+cat custom-port.conf>/etc/apache2/sites-available/custom-port.conf
+cat jammer-work.conf>/etc/apache2/sites-available/jammer-work.conf
+cat ludumdare-org.conf>/etc/apache2/sites-available/ludumdare-org.conf
+
+ln -s ../sites-available/custom-port.conf /etc/apache2/sites-enabled/001-custom-port.conf
+ln -s ../sites-available/jammer-work.conf /etc/apache2/sites-enabled/010-jammer-work.conf
+ln -s ../sites-available/ludumdare-org.conf /etc/apache2/sites-enabled/001-ludumdare-org.conf
 
 echo "Restarting Apache..."
 apache2ctl restart
