cfd2ea10c8956c23c3d7c269d4c0946dbf9ffb77
Mike Kasprzak <mike@sykhronics.com>
2016-07-08 04:32:41 -0400

    now outputing the MIN file where it can be accessed

--- gulpfile.js
+++ gulpfile.js
@@ -27,19 +27,29 @@ var raw_js_in_files	= babelignore_files.map(function(el){return el+'**/*.js';})
 						.concat(babelignore_files.map(function(el){return '!'+el+'**/_*.js';}))
 						.concat(babelignore_files.map(function(el){return '!'+el+'**/*.min.js';}));
 
-//console.log(raw_js_in_files);
-
+var build_folder		= 'output';
+var release_folder		= 'public-static/output';
 
 // Ignore any min files, and the output file //
 var css_output 			= 'all.css';
 var css_min_output		= 'all.min.css';
 var css_min_gz_output	= 'all.min.css.gz';
-var css_out_files		= ['output/**/*.css','!output/**/_*.css','!output/**/*.min.css','!output/'+css_output];
+var css_out_files		= [
+	build_folder+'/**/*.css',
+	'!'+build_folder+'/**/_*.css',
+	'!'+build_folder+'/**/*.min.css',
+	'!'+build_folder+'/'+css_output
+];
 
 var js_output			= 'all.js';
 var js_min_output		= 'all.min.js';
 var js_min_gz_output	= 'all.min.js.gz';
-var js_out_files		= ['output/**/*.js','!output/**/_*.js','!output/**/*.min.js','!output/'+js_output];
+var js_out_files		= [
+	build_folder+'/**/*.js',
+	'!'+build_folder+'/**/_*.js',
+	'!'+build_folder+'/**/*.min.js',
+	'!'+build_folder+'/'+js_output
+];
 
 
 /* LESS files to CSS */
@@ -52,7 +62,7 @@ gulp.task('less', function() {
 	var autoprefix	= require('less-plugin-autoprefix');
 
 	return gulp.src( less_files )
-		.pipe( newer({dest:"output",ext:".css"}) )
+		.pipe( newer({dest:build_folder,ext:".css"}) )
 		.pipe( debug({title:'less:'}) )
 //		.pipe( sourcemaps.init() )
 			.pipe( less({
@@ -63,22 +73,22 @@ gulp.task('less', function() {
 				]
 			}) )
 //		.pipe( sourcemaps.write() )
-		.pipe( gulp.dest("output/") );
+		.pipe( gulp.dest(build_folder+'/') );
 });
 /* Unprocessed CSS files */
 gulp.task('css', function() {
 	return gulp.src( css_files )
-		.pipe( newer({dest:"output"}) )
+		.pipe( newer({dest:build_folder}) )
 		.pipe( debug({title:'css:'}) )
-		.pipe( gulp.dest("output/") );
+		.pipe( gulp.dest(build_folder+'/') );
 });
 /* Concatenate all CSS files */
 gulp.task('css-cat', ['less','css'], function() {
 	return gulp.src( css_out_files )
-		.pipe( newer({dest:"output/"+css_output}) )
+		.pipe( newer({dest:build_folder+'/'+css_output}) )
 		.pipe( concat( css_output ) )
 		.pipe( size({title:'css-cat:',showFiles:true}) )
-		.pipe( gulp.dest( "output/" ) );	
+		.pipe( gulp.dest( build_folder+'/' ) );	
 });
 /* Minifiy the concatenated CSS file */
 gulp.task('css-min', ['css-cat'], function() {
@@ -86,21 +96,21 @@ gulp.task('css-min', ['css-cat'], function() {
 	var cleancss	= require('gulp-cleancss');		// Faster, similar results
 //	var cssnano		= require('gulp-cssnano');
 
-	return gulp.src( "output/"+css_output )
-		.pipe( newer({dest:"output/"+css_min_output}) )
+	return gulp.src( build_folder+'/'+css_output )
+		.pipe( newer({dest:release_folder+'/'+css_min_output}) )
 		.pipe( cleancss() )
 //		.pipe( cssnano() )
 		.pipe( concat( css_min_output ) )
 		.pipe( size({title:'css-min:',showFiles:true}) )
-		.pipe( gulp.dest( "output/" ) );	
+		.pipe( gulp.dest( release_folder+'/' ) );	
 });
 /* GZIP minified (for reference) */
 gulp.task('css-min-gz', ['css-min'], function() {
-	return gulp.src( "output/"+css_min_output )
-		.pipe( newer({dest:"output/"+css_min_gz_output}) )
+	return gulp.src( release_folder+'/'+css_min_output )
+		.pipe( newer({dest:build_folder+'/'+css_min_gz_output}) )
 		.pipe( gzip() )
 		.pipe( size({title:'css-min-gz:',showFiles:true}) )
-		.pipe( gulp.dest( "output/" ) );
+		.pipe( gulp.dest( build_folder+'/' ) );
 });
 
 
@@ -109,45 +119,45 @@ gulp.task('babel', function() {
 	var babel = require("gulp-babel");
 	
 	return gulp.src( js_in_files, {base:'src'} )
-		.pipe( newer({dest:"output",ext:".o.js"}) )
+		.pipe( newer({dest:build_folder,ext:".o.js"}) )
 		.pipe( debug({title:'js (babel):'}) )
 		.pipe( babel({presets:['es2015']}) )
 		.pipe( rename({extname: ".o.js"}) )
-		.pipe( gulp.dest( "output/" ) );
+		.pipe( gulp.dest( build_folder+'/' ) );
 });
 /* Unprocessed JS files */
 gulp.task('js', function() {
 	return gulp.src( raw_js_in_files, {base:'src'} )
-		.pipe( newer({dest:"output"}) )
+		.pipe( newer({dest:build_folder}) )
 		.pipe( debug({title:'js (raw):'}) )
-		.pipe( gulp.dest( "output/" ) );
+		.pipe( gulp.dest( build_folder+'/' ) );
 });
 /* Concatenate all JS files */
 gulp.task('js-cat', ['babel','js'], function() {
 	return gulp.src( js_out_files )
-		.pipe( newer({dest:"output/"+js_output}) )
+		.pipe( newer({dest:build_folder+'/'+js_output}) )
 		.pipe( concat( js_output ) )
 		.pipe( size({title:'js-cat:',showFiles:true}) )
-		.pipe( gulp.dest( "output/" ) );
+		.pipe( gulp.dest( build_folder+'/' ) );
 });
 /* Minifiy the concatenated JS file */
 gulp.task('js-min', ['js-cat'], function() {
 	var uglify = require('gulp-uglify');
 	
-	return gulp.src( "output/"+js_output )
-		.pipe( newer({dest:"output/"+js_min_output}) )
+	return gulp.src( build_folder+'/'+js_output )
+		.pipe( newer({dest:release_folder+'/'+js_min_output}) )
 		.pipe( uglify() )
 		.pipe( concat( js_min_output ) )
 		.pipe( size({title:'js-min:',showFiles:true}) )
-		.pipe( gulp.dest( "output/" ) );
+		.pipe( gulp.dest( release_folder+'/' ) );
 });
 /* GZIP minified (for reference) */
 gulp.task('js-min-gz', ['js-min'], function() {
-	return gulp.src( "output/"+js_min_output )
-		.pipe( newer({dest:"output/"+js_min_gz_output}) )
+	return gulp.src( release_folder+'/'+js_min_output )
+		.pipe( newer({dest:build_folder+'/'+js_min_gz_output}) )
 		.pipe( gzip() )
 		.pipe( size({title:'js-min-gz:',showFiles:true}) )
-		.pipe( gulp.dest( "output/" ) );	
+		.pipe( gulp.dest( build_folder+'/' ) );	
 });
 
 
@@ -156,7 +166,7 @@ gulp.task('js-min-gz', ['js-min'], function() {
 gulp.task('clean', function() {
 	var del = require('del');
 	
-	return del( 'output/**/*' );
+	return del( [build_folder+'/**/*',release_folder+'/**/*'] );
 });
 
 
