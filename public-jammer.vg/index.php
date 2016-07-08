561f82902205daf3a356651bb37d3173677a57a3
Mike Kasprzak <mike@sykhronics.com>
2016-07-08 04:41:54 -0400

    added a broken jammer stub

--- public-jammer.vg/index.php
+++ public-jammer.vg/index.php
@@ -1,1 +1,12 @@
-Stub for Jammer.vg
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="//static.jam.dev/output/all.min.css?v=1.0">
+</head>
+
+<body>
+	<script src="//static.jam.dev/output/all.min.js"></script>
+	Hello
+</body>
+</html>
