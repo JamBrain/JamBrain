a90fb5e209d994ba0c2a443fd3d9cf9b17a1bf96
Mike Kasprzak <mike@sykhronics.com>
2016-07-11 22:32:46 -0400

    working onclicks and doc body classLists

--- public-jammer.vg/index.php
+++ public-jammer.vg/index.php
@@ -5,7 +5,8 @@
 	<link rel="stylesheet" href="//static.jam.dev/output/all.min.css?v=1.0">
 </head>
 
-<body class="_static-nav-bar">
+<body>
 	<script src="//static.jam.dev/output/all.js"></script>
+	Hello
 </body>
 </html>
--- src/com/nav-bar/code.js
+++ src/com/nav-bar/code.js
@@ -4,8 +4,8 @@ export default class NavBar extends Component {
 	render(props,state) {
 		return (
 			<div class="nav-bar">
-				<div class="box">Hullo</div>
-				<div class="box">C</div>
+				<div class="box" onClick={ e => navbar_Static() }>Hullo</div>
+				<div class="box" onClick={ e => navbar_Float() }>C</div>
 				<div class="box">NOO</div>
 				<div class="box -right">Well...</div>
 				<div class="box -right">Sure</div>
@@ -13,7 +13,18 @@ export default class NavBar extends Component {
 		);
 	}
 	
-	componentWillMount() {
-		console.log('mount');
+	componentDidMount() {
+		document.body.classList.add('_use-nav-bar');
 	}
+	componentWillUnmount() {
+		document.body.classList.remove('_use-nav-bar');
+		document.body.classList.remove('_static-nav-bar');
+	}
+}
+
+export function navbar_Float() {
+	document.body.classList.remove('_static-nav-bar');
+}
+export function navbar_Static() {
+	document.body.classList.add('_static-nav-bar');
 }
