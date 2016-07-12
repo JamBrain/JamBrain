5d8f25a06f027245c944f604eb7e487ff170a0cb
Mike Kasprzak <mike@sykhronics.com>
2016-07-12 18:11:47 -0400

    Corrected to use WORK URLs

--- public-jammer.vg/index.php
+++ public-jammer.vg/index.php
@@ -1,12 +1,16 @@
-<!doctype html>
+<?php
+// TODO: Figure out if this is the live server or not //
+define('USE_MINIFIED',isset($_GET['debug']) ? '' : '.min');
+define('USE_VERSION','0.1');
+?><!doctype html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="//static.jam.dev/output/all.min.css?v=1.0">
+	<link rel="stylesheet" href="//static.jammer.work/output/all<?=USE_MINIFIED?>.css?v=<?=USE_VERSION?>">
 </head>
 
 <body>
-	<script src="//static.jam.dev/output/all.js"></script>
+	<script src="//static.jammer.work/output/all<?=USE_MINIFIED?>.js?v=<?=USE_VERSION?>"></script>
 	Hello
 </body>
 </html>
