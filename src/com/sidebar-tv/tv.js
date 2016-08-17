9260fd7b798fc987494ecbd0d438b73dfb31e1c5
Mike Kasprzak <mike@sykhronics.com>
2016-08-17 17:14:21 -0400

    TV widget uses live data now

--- src/com/sidebar-tv/tv.js
+++ src/com/sidebar-tv/tv.js
@@ -4,23 +4,83 @@ import SVGIcon 			from 'com/svg-icon/icon';
 
 export default class SidebarTV extends Component {
 	constructor() {
+		this.state = {
+			active: 0,
+			streams: []
+		};
+		
+		this.services = [
+			(<div></div>),					// Null //
+			(<SVGIcon>twitch</SVGIcon>),	// Twitch //
+			(<div></div>),
+			(<div></div>),
+			(<div></div>),
+			(<div></div>),
+			(<div></div>),
+		];
+	}
+	
+	componentDidMount() {
+		fetch('//jammer.tv/v1/live.php/ludum-dare+game-jam+game-dev/')//,{mode:'no-cors'})
+			.then( r => r.json() )
+			.then( data => {
+				let state_copy = this.state;
+				state_copy.streams = state_copy.streams.concat(data.streams);
+				
+				this.setState(state_copy);
+			});
+	}
+	
+	setActive( id, ev ) {
+		let state_copy = this.state;
+		state_copy.active = id;
+		this.setState(state_copy);
+	}
+	
+	showOthers( others, active ) {
+		var that = this;	// Workaround. Not sure if it's a Buble or Preact bug //
+		
+		return others.map( function(other,index) {
+			if (other === active) {
+				return (<div class="selected" onclick={that.setActive.bind(that,index)}><div><img src={other.meta.thumbnail} /></div></div>);
+			}
+			else {
+				return (<div onclick={that.setActive.bind(that,index)}><div><img src={other.meta.thumbnail} /></div></div>);
+			}
+		});
 	}
 	
 	render( props, state ) {
-		return (
-			<div class="sidebar-base sidebar-tv">
-				<div class="-active">
-					<div class="-img"><img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_esl_sc2-320x180.jpg" /></div>
-					<div class="-live"><SVGIcon>circle</SVGIcon> <span>LIVE</span></div>
-					<div class="-play"><SVGIcon>play</SVGIcon></div>
+		if ( state.streams.length == 0 ) {
+			return (
+				<div class="sidebar-base sidebar-tv">
+					Loading...
 				</div>
-				<div class="-detail"><SVGIcon>twitch</SVGIcon> Doing Stuff! #GameDev</div>
-				<div class="-browse">
-					<div class="selected"><div><img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_esl_sc2-320x180.jpg" /></div></div>
-					<div><div><img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_esl_dota2-320x180.jpg" /></div></div>
-					<div><div><img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_esl_csgo-320x180.jpg" /></div></div>
+				);
+		}
+		else {
+			let active = state.streams[state.active];
+			let others = [
+				state.streams[0],
+				state.streams[1],
+				state.streams[2],				
+			];
+			
+			return (
+				<div class="sidebar-base sidebar-tv">
+					<div class="-active">
+						<div class="-img"><img src={active.meta.thumbnail} /></div>
+						<div class="-live"><SVGIcon>circle</SVGIcon> <span class="-text">LIVE</span></div>
+						<div class="-name">{this.services[active.service_id]} <span class="-text">{active.meta.name}</span></div>
+						<div class="-viewers"><span class="-text">{active.viewers}</span></div>
+						<div class="-play"><SVGIcon>play</SVGIcon></div>
+					</div>
+					<div class="-detail" title={active.meta.status}>{active.meta.status}</div>
+					<div class="-browse">
+						{this.showOthers(others,active)}
+					</div>
 				</div>
-			</div>
-		);
+			);
+		}
 	}
 }
