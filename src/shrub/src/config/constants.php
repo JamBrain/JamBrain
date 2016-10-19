3ce78b2bc06ab9e2f6dfc93b4225a70136b2e1d2
Mike Kasprzak <mike@sykhronics.com>
2016-10-19 13:17:49 -0400

    new config pathing working

--- src/shrub/src/config/constants.php
+++ src/shrub/src/config/constants.php
@@ -2,8 +2,25 @@
 /// @defgroup Config
 /// @ingroup Modules
 
+/// @cond INTERNAL
+$SH_TABLES = [];
+/// @endcond
+
+/// Add a table to the table list
+/// @param [in] ... Table name string(s)
+/// @addtogroup Config
+function config_AddTable(...$args) {
+	global $SH_TABLES;
+
+	foreach ( $args as $arg ) {
+		$SH_TABLES[] = $arg;
+	}
+}
+
 /// @name Config Tables
 /// @addtogroup Tables
 /// @{
 const SH_TABLE_CONFIG =					"config";
 /// @}
+	
+config_AddTable( SH_TABLE_CONFIG);
--- src/shrub/src/config/table_destroy.php
+++ src/shrub/src/config/table_destroy.php
@@ -1,2 +1,4 @@
 <?php
-table_Drop(SH_TABLE_CONFIG);
+//table_Drop(SH_TABLE_CONFIG);
+
+echo "hi mom\n";
