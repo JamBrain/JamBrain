d0c2daa359cb88267ce451860b313d3db5ba2894
Mike Kasprzak <mike@sykhronics.com>
2016-10-26 15:39:49 -0400

    new revised indexes, with a message

--- public-jammer.vg/index.php
+++ public-jammer.vg/index.php
@@ -1,2 +1,26 @@
+Coming soon! For now, go to <strong><a href="https://ldjam.com">ldjam.com</a></strong>.<br />
+<br />
+<h2>What is this though?</h2>
+You know how <strong>Ludum Dare</strong> runs <strong>Ludum Dare</strong> game jams? Well <strong>Jammer</strong> is everything else.<br />
+<br />
+So as the network of <strong>Ludum Dare</strong> related web projects grew, it made sense give them a new identity. And because it sounded appropriate (and cool), I chose to name it <strong>Jammer</strong>. So when <strong>Ludum Dare</strong> tells you to make a <strong>Jammer</strong> account, that is us telling you to make an account for <strong>Ludum Dare</strong> and our <strong>future</strong> stuff.<br />
+<br />
+Game Jamming is a thing now. It's a culture, and a way of life. As it matures, to keep it moving forward, we need to do better by preserving and organizing that culture. Being the premier <strong>online</strong> jam, <strong>Ludum Dare</strong> has become the home of a huge community of makers from around the world. I'm extremely humbled by this. Despite our flaws, our ups and downs, you've stuck with us for nearly 15 years. So I feel it's our responsibilty to be spearhead this effort. Be part of the solution, the evolution, rather than just being one more place to jam on the internet. I'm not the best leader, and I've certainly made mistakes, but I've been here this whole time. This is something I deeply care about, and though it may take time, I'm seeing it through (whether we like it or not). 😉<br />
+<br />
+The new <strong>ludumdare.com</strong> (currently alpha-testing on <strong><a href="https://ldjam.com">ldjam.com</a></strong>) is the first step. The focus is on main events. This here, <strong>Jammer</strong>, is the next step.<br />
+<br />
+To avoid confusion, I've disabled this website for now. Effectively though, <strong>ludumdare.com</strong> (<strong><a href="https://ldjam.com">ldjam.com</a></strong> during alpha testing) and <strong>jammer.vg</strong> are the same. They do the same thing, share the same data, and work the same way. But the distinction will be more and more important in time. It lets <strong>ludumdare.com</strong> specfically focus on <strong>Ludum Dare</strong>. It can assume you're there to participate in <strong>Ludum Dare</strong>, brag all about the <strong>Ludum Dare</strong> games on <strong>Steam</strong> and <strong>PlayStation 4</strong>, or push tips and tricks on how to best use your 48 and 72 hours. <strong>Jammer</strong> can then become all-encompassing, collecting and archiving data from other jams (forgotten jams as well) in one place. Like a Mobygames or Wikipedia of jamming. And because they share the same core, <strong>Ludum Dare</strong> data is automatically part of it, and vice versa will benefit (collecting data on tool usage, graphings trends in game jams, etc).<br />
+<br />
+I don't expect the community to up-and-move their activites to <strong>jammer.vg</strong>. But as these two sites continue to evolve, <strong>ludumdare.com</strong> will focus more and more on <strong>Ludum Dare</strong> the event, and <strong>Jammer</strong> will focus on <strong>you</strong>, the culture, and the greater game jamming community.<br />
+<h3>Other things to look forward to</h3>
+<ul>
+	<li>This (<strong>jammer.vg</strong>), of course. Opening our database up to store all your game jam games (and other games) in one place. Eventually letting you run your own jams on our system. Behind the scenes, both <strong>Jammer</strong> and <strong>Ludum Dare</strong> are unified, sharing the same data.</li>
+	<li><strong>Jammer.bio</strong> - You know how some people have lots of games on their <strong>Ludum Dare</strong> user pages? Well along with adding your other games (jammer.vg), wouldn't it be nice to have a version of your user page that focused entirely on you? Something without all the noisy Ludum Dare banners, announcements, etc. Something you could send, or even recite to someone (what's a ludum dare anyway?). jammer.bio/yourname</li>
+	<li><strong>Jammer.tv</strong> - What began as a Twitch livestreaming widget (nicknamed LDtv) has growing in to its own project.</li>
+</ul>
+I hope you tag along for our little adventure. 😃<br />
+<br />
+- Mike Kasprzak (<a href="http://twitter.com/mikekasprzak">@mikekasprzak</a>)<br />
+<br /><br />
 <?php
-include __DIR__."/../src/public-jammer.vg/main-vg.php";
+//include __DIR__."/../src/public-jammer.vg/main-vg.php";
