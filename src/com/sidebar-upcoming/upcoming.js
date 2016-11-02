818328718bde9791a879aabe8399d9bdfe8bf889
Mike Kasprzak <mike@sykhronics.com>
2016-11-02 15:55:23 -0400

    Theme is now darker

--- src/com/sidebar-upcoming/upcoming.js
+++ src/com/sidebar-upcoming/upcoming.js
@@ -9,7 +9,7 @@ export default class SidebarUpcoming extends Component {
 		return (
 			<div class="sidebar-base sidebar-shortlist sidebar-upcoming">
 				<div class="-title _font2"><SVGIcon baseline>calendar-wide</SVGIcon> Coming Up</div>
-				<div class="-item"><strong>Nov. 25th</strong> - Theme Selection Begins</div>
+				<div class="-item"><strong>Nov. 25th</strong> - Theme Selection begins</div>
 				<div class="-item"><strong>Dec. 9th</strong> - Ludum Dare 37 <SVGIcon baseline small>trophy</SVGIcon></div>
 				<div class="-item"><strong>Dec. 30th</strong> - Results</div>
 				<div class="-footer">Full Schedule</div>
