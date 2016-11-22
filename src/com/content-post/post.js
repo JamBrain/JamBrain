26218b3a8a6b6577f6b52cd835819f73e5dff92f
Mike Kasprzak <mike@sykhronics.com>
2016-11-22 12:56:51 -0500

    okay, first part is working again (individual posts)

--- src/com/content-post/post.js
+++ src/com/content-post/post.js
@@ -2,17 +2,9 @@ import { h, Component } from 'preact/preact';
 import SVGIcon 			from 'com/svg-icon/icon';
 import NavLink 			from 'com/nav-link/link';
 
-//import CoreData			from '../../core-data/data';
-
-
 export default class ContentPost extends Component {
 	constructor( props ) {
 		super(props);
-
-//		this.state = {
-//			post: props.node,
-//			user: props.node
-//		};
 	}
 
 	render( { node, user }, state ) {
@@ -22,13 +14,13 @@ export default class ContentPost extends Component {
 			
 			var url = "//";
 			
-			var relative_time = "";//<span class="-time">{node.published}</span> ago
-//			var post_date = <span>on <span class="-title" title={post.date}>{post.short_date}</span></span>;
-			var post_date = <span>on <span class="-title" title={post.published}>{post.published}</span></span>;
-//			var post_by = by <span class="-name"><NavLink href={'/u/'+user.slug+'/'} class="-author" title={'@'+user.slug}><img style="height:0.8em;padding-right:0.1em;" src={avatar} />{user.name}</NavLink></span>
-			var post_by = <span>by {node.author}<span>;
+			// x minutes ago
+			var relative_time = <span></span>;//<span class="-time">{node.published}</span> ago
+			// simple date, full date on hover
+			var post_date = <span>on <span class="-title" title={node.published}>{node.published}</span></span>;
+			var post_by = <span>by {node.author}</span>;
 			
-			var hasTwitter = "";
+			var hasTwitter = <span></span>;
 			
 			return (
 				<div class="content-base content-post">
@@ -49,110 +41,15 @@ export default class ContentPost extends Component {
 				</div>
 			);
 		}
-//		else {
-//			return (
-//				<div class="content-base content-post">
-//					Please Wait...
-//				</div>
-//			);
-//		}
-		
-	}
-	
-	
-/*
-//		if ( !post && !user ) {
-//			return (
-//				<div class="content-base content-post">
-//					Please Wait...
-//				</div>
-//			);
-//		}
-		
-		
-//		var post = CoreData.getNodeById( props.node );
-//		var user = CoreData.getNodeById( post.author );
-		
-//		var hasTwitter = user.meta.twitter ? <span class="-twitter"> (<a href={"https://twitter.com/"+user.meta.twitter} target="_blank" rel="noopener noreferrer" title={"https://twitter.com/"+user.meta.twitter}><SVGIcon baseline small>twitter</SVGIcon>/{user.meta.twitter}</a>)</span> : <span />;
-//		var hasTeam = props.user.team ? <span class="-team"> of <em>{props.user.team}</em> <SVGIcon>users</SVGIcon></span> : <span />;
-		
-		// Build URL //
-		var url = '/'+'timmy';//CoreData.getNodePathSlugsById( props.node ).slice(1).join('/')+'/';
-		
-//		function parseNames( str ) {
-//			// Dummy: Use Global Object //
-//			var users = {
-//				'pov': {
-//					name:'PoV',
-//					slug:'pov',
-//					avatar:'/other/logo/mike/Chicken64.png',
-//					twitter:'mikekasprzak',
-//				}
-//			};
-//			
-//			return str.replace(/@([A-Za-z][A-Za-z0-9-]*)/g,function(original,p1,offset,s) {
-//				//console.log(match,p1,offset);
-//				var name = p1.toLowerCase();
-//				if ( users[name] ) {
-//					return "<span class='inline-name'><img src='//"+STATIC_DOMAIN+users[name].avatar+"'><a href='/u/"+users[name].slug+"'>"+users[name].name+"</a></span>";
-//				}
-//				else {
-//					return original;
-//				}
-//			});
-//			
-//			// TODO: attach the Navigation link code to the <a> tag above //
-//		}
-
-/*		var dangerousParsedBody = { __html:marked.parse(post.body) };
-		var dangerousParsedTitle = { __html:titleParser.parse(post.name) };
-		
-		var avatar = user.meta.avatar ? "//"+STATIC_DOMAIN+user.meta.avatar : "";
-		
-		return (
-			<div class="content-base content-post">
-				<div class="-header">
-					<div class="-avatar"><img src={avatar} /></div>
-					<div class="-title _font2"><NavLink href={url} dangerouslySetInnerHTML={dangerousParsedTitle} /></div>
-					<div class="-subtext">
-						Posted <span class="-time">{post.relative_time}</span> ago
-						on <span class="-title" title={post.date}>{post.short_date}</span>,
-						by <span class="-name"><NavLink href={'/u/'+user.slug+'/'} class="-author" title={'@'+user.slug}><img style="height:0.8em;padding-right:0.1em;" src={avatar} />{user.name}</NavLink></span>
-						{hasTwitter}
-					</div>
+		else {
+			return (
+				<div class="content-base content-post">
+					Please Wait...
 				</div>
-				<div class="-body markup" dangerouslySetInnerHTML={dangerousParsedBody} />
-				<div class="-footer">
-					<div class="-left">
-						<div class="-minmax"><SVGIcon>arrow-up</SVGIcon></div>
-					</div>
-					<div class="-right">
-					</div>
-				</div>
-			</div>
-		);
+			);
+		}
 	}
-*/
-// From the footer class right above
-//						<div class="-love">
-//							<div class="-heart"><SVGIcon>heart-check</SVGIcon></div>
-//							<div class="-text -count">0</div>
-//							<div class="-text -count">0</div>
-//						</div>
-//						<div class="-comment"><SVGIcon>bubble-empty</SVGIcon></div>
 
-
-	// body: unmargin-top, unmargin-bottom. replace with selector
-	
-	// <SVGIcon>wedge-left</SVGIcon>
-	// <SVGIcon>wedge-right</SVGIcon>
-
-//						<div class="-spacer"><SVGIcon>wedge-right</SVGIcon></div>
-//						<div class="-comment"><SVGIcon>bubble-empty</SVGIcon></div>
-//						<div class="-text -comment-count">0</div>
-//						<div class="-spacer2"><SVGIcon>wedge-right</SVGIcon></div>
-//						<div class="-gear"><SVGIcon>cog</SVGIcon></div>
-	
 	componentDidMount() {
 	}
 	componentWillUnmount() {
