1e275be88a042ebd8afd66bf6e9d2858aca0fd0d
Mike Kasprzak <mike@sykhronics.com>
2016-11-23 05:01:13 -0500

    okay, why aren't the buttons clicking?

--- src/com/content-event-idea/event-idea.js
+++ src/com/content-event-idea/event-idea.js
@@ -2,12 +2,18 @@ import { h, Component } 				from 'preact/preact';
 import SVGIcon 							from 'com/svg-icon/icon';
 import NavLink 							from 'com/nav-link/link';
 
+const MAX_IDEAS = 3;
+
 export default class ContentEventIdea extends Component {
 	constructor( props ) {
 		super(props);
 		
 		this.state = {
+			ideas: [ "contra" ]
 		};
+		
+		this.removeIdea = this.removeIdea.bind(this);
+		this.submitIdeaForm = this.submitIdeaForm.bind(this);
 	}
 
 //		<script>
@@ -17,7 +23,24 @@ export default class ContentEventIdea extends Component {
 //			}, false);
 //		</script>
 
-	render( {node, user, path, extra}, {error} ) {
+	removeIdea( idea ) {
+		console.log('remove');
+	}
+	
+	submitIdeaForm( form ) {
+		console.log('submit');
+	}
+
+	renderIdea( idea ) {
+		return (
+			<div class="-item">
+				<div class='-x' onclick={this.removeIdea}>✕</div>
+				<div class='-text' title={idea}>{idea}</div>
+			</div>
+		);
+	}
+
+	render( {node, user, path, extra}, {ideas, error} ) {
 		if ( node.slug ) {
 			if ( user && user['id'] ) {
 				return (
@@ -27,7 +50,11 @@ export default class ContentEventIdea extends Component {
 							<input type="text" class="-single" id="input-idea" placeholder="Your suggestion" maxlength="64" />
 							<button type="button" class="-submit" onclick={this.SubmitIdeaForm}>Submit</button>
 						</div>
-						<div class="foot-note small">You have <strong><span id="sg-count">?</span></strong> suggestion(s) left</div>
+						<div class="foot-note small">You have <strong>{MAX_IDEAS-ideas.length}</strong> suggestion(s) left</div>
+						<h3>My Suggestions</h3>
+						<div class="idea-mylist">
+							{ ideas.map(this.renderIdea) }
+						</div>
 					</div>
 				);
 			}
@@ -35,7 +62,7 @@ export default class ContentEventIdea extends Component {
 				return (
 					<div class="-body">
 						<h3>Theme Suggestion Round</h3>
-						<div>Please log in to make a suggestion</div>
+						<div>Please log in to make suggestion</div>
 					</div>
 				);
 			}
