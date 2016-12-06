6472af20a8255ae584fc12ddf2ab5f3d1dea49f6
Mike Kasprzak <mike@sykhronics.com>
2016-12-06 01:29:42 -0500

    forgot a global

--- src/shrub/src/core/json.php
+++ src/shrub/src/core/json.php
@@ -275,6 +275,7 @@ function json_Emit( $out, $allow_jsonp = true ) {
 
 /// After authenticating and loading globals, confirm that we're not in maintenence mode
 function json_CheckForMaintenence() {
+	global $SH;
 	if ( !userAuth_IsAdmin() && !global_IsActive() ) {
 		json_EmitFatalError_Unavailable( strlen($SH['alert']) ? $SH['alert'] : null );
 	}
