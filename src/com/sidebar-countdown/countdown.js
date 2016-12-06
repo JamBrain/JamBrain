4ab3eafd9c238ade9f8558de082bbd29d06100b6
StormBurpee <stormburpee@gmail.com>
2016-12-06 14:34:59 +1030

    hopefully finished countdown-sidebar

--- src/com/sidebar-countdown/countdown.js
+++ src/com/sidebar-countdown/countdown.js
@@ -4,7 +4,6 @@ import SVGIcon 			from 'com/svg-icon/icon';
 export default class SidebarCountdown extends Component {
 	constructor( props ) {
 		super(props);
-			this.el = document.getElementsByClassName('-countdown')[0];
 
 			this.countdown_interval = null;
 			this.total_seconds = 0;
@@ -27,13 +26,6 @@ export default class SidebarCountdown extends Component {
 			let d = Math.floor(ts / 60 / 60 / 24);
 			//let d = Math.floor(countdownTo / (1000 * 60 * 60 * 24));
 
-			that.$ = {
-				days: document.querySelector('#'+that.class+' .bloc-time.days .figure'),
-				hours: document.querySelector('#'+that.class+' .bloc-time.hours .figure'),
-				minutes: document.querySelector('#'+that.class+' .bloc-time.min .figure'),
-				seconds:  document.querySelector('#'+that.class+' .bloc-time.sec .figure')
-			};
-
 			that.values = {
 				days: d,
 				hours: h,
@@ -41,26 +33,26 @@ export default class SidebarCountdown extends Component {
 				seconds: s
 			}
 			if(d > 1) {
-				that.$.days.parentElement.setAttribute('style', 'display: inline-block');
-				that.$.seconds.parentElement.setAttribute('style', 'display: none');
+				that.setState({"ShowDays": true});
 			}
 
 			//that.total_seconds = (((that.values.days * 24) * 60) * 60) + that.values.hours * 60 * 60 + (that.values.minutes * 60) + that.values.seconds;
 			that.total_seconds = ts;
-
+			that.setState({'values': {'days': d, 'hours': h, 'minutes': m, 'seconds': s}, 'fvalues': {'d1': 0, 'd2': 0, 'h1': 0, 'h2': 0, 'm1': 0, 'm2': 0, 's1': 0, 's2': 0}, 'animate': false});
 			that.count();
+
 		});
 	}
 
 	animateFigure($el, value) {
 		let that = this;
-		let $top = $el.querySelector('.top');
-		let $bottom = $el.querySelector('.bottom');
-		let $back_top = $el.querySelector('.top-back');
-		let $back_bottom = $el.querySelector('.bottom-back');
+		let $top = $el.children[0];
+		let $bottom = $el.children[2];
+		let $back_top = $el.children[1];
+		let $back_bottom = $el.children[3];
 
-		$back_top.querySelector('span').innerHTML = value;
-		$back_bottom.querySelector('span').innerHTML = value;
+		$back_top.children[0].innerHTML = value;
+		$back_bottom.children[0].innerHTML = value;
 
 		$top.setAttribute('style', 'transition: 0.8s all ease-out; transform: rotateX(-180deg) perspective(300px)');
 		setTimeout(function() {
@@ -73,39 +65,33 @@ export default class SidebarCountdown extends Component {
 		setTimeout(function() {
 			$back_top.setAttribute('style', 'transition: 0.0s all ease-out; transform: rotateX(180deg) perspective(0px)');
 		}, 800);
-
 	}
 
 	checkHour(value, e1, elc)
 	{
 		let val_1 = value.toString().charAt(0);
 		let val_2 = value.toString().charAt(1);
-		let fig_1_value = e1.querySelector('.top').innerHTML;
-		let fig_2_value = elc.querySelector('.top').innerHTML;
+		let fig_1_value = arguments[1].children[0].innerHTML;
+		let fig_2_value = arguments[2].children[0].innerHTML;
 		if(value >= 10) {
-        // Animate only if the figure has changed
+				//Animate only if the figure has changed
 				// Something weird is happening here, that's why I'm using arguments[1] rather than el...
         if(fig_1_value !== val_1) this.animateFigure( arguments[1], val_1 );
-        if(fig_2_value !== val_2) this.animateFigure( elc, val_2 );
+        if(fig_2_value !== val_2) this.animateFigure( arguments[2], val_2 );
     }
     else {
         // If we are under 10, replace first figure with 0
         if(fig_1_value !== '0') this.animateFigure(arguments[1], 0);
-        if(fig_2_value !== val_1) this.animateFigure(elc, val_1);
+        if(fig_2_value !== val_1) this.animateFigure(arguments[2], val_1);
     }
 	}
 
 	count() {
 		let that = this;
-		let $day_1 = document.querySelector('#'+that.class+' .bloc-time.days .figure.days-1');
-		let $day_2 = document.querySelector('#'+that.class+' .bloc-time.days .figure.days-2');
-		let $hour_1 = document.querySelector('#'+that.class+' .bloc-time.hours .figure.hours-1');
-		let $hour_2 = document.querySelector('#'+that.class+' .bloc-time.hours .figure.hours-2');
-		let $min_1 = document.querySelector('#'+that.class+' .bloc-time.min .figure.min-1');
-		let $min_2 = document.querySelector('#'+that.class+' .bloc-time.min .figure.min-2');
-		let $sec_1 = document.querySelector('#'+that.class+' .bloc-time.sec .figure.sec-1');
-		let $sec_2 = document.querySelector('#'+that.class+' .bloc-time.sec .figure.sec-2');
-		console.log(that);
+		let d = that.daysblock;
+		let h = that.hoursblock;
+		let m = that.minutesblock;
+		let s = that.secondsblock;
 		this.countdown_interval = setInterval(function() {
 			if(that.total_seconds > 0) {
 				--that.values.seconds;
@@ -127,27 +113,29 @@ export default class SidebarCountdown extends Component {
 					--that.values.days;
 				}
 
-				if(that.values.days < 1) {
-					that.$.seconds.parentElement.setAttribute('style', 'display: inline-block');
-					that.$.days.parentElement.setAttribute('style', 'display: none');
+				if(that.values.days < 1 && that.state.ShowDays == true) {
+					that.setState({"ShowDays": false});
 				}
 
 				if(that.values.days < 1 && that.values.hours <= 6)
 				{
-					if(!document.querySelector('#'+that.class+' .-countdown.urgent')) {
-						document.querySelector('#'+that.class+' .-countdown').setAttribute('class', '-countdown urgent');
+					if(!that.state.Urgent)
+					{
+						that.setState({'Urgent': true});
 					}
 				}
 
+				console.log(that);
+
         // Update DOM values
         // Days
-        that.checkHour(that.values.days, $day_1, $day_2);
+        that.checkHour(that.values.days, d.children[1], d.children[2]);
         // Hours
-        that.checkHour(that.values.hours, $hour_1, $hour_2);
+        that.checkHour(that.values.hours, h.children[1], h.children[2]);
         // Minutes
-        that.checkHour(that.values.minutes, $min_1, $min_2);
+        that.checkHour(that.values.minutes, m.children[1], m.children[2]);
         // Seconds
-        that.checkHour(that.values.seconds, $sec_1, $sec_2);
+        that.checkHour(that.values.seconds, s.children[1], s.children[2]);
 
         --that.total_seconds;
 			} else {
@@ -170,15 +158,25 @@ export default class SidebarCountdown extends Component {
 			utcCode = parseInt(utcCode)+":00";
 		}
 
+		let daysblock = "display: none";
+		let secondsblock = "display: inline-block";
+		if(this.state.ShowDays){
+			daysblock = "display: inline-block";
+			secondsblock = "display: none";
+		}
+
 		utcCode = utcCodep+utcCode;
+		let urgentclass = "-countdown";
+		if(this.state.Urgent && this.state.Urgent == true)
+			urgentclass = "-countdown urgent";
 
 		return (
 			<div class="sidebar-base sidebar-countdown">
 				<div class="-clock font2" id={ this.class }>
 					<h1>{ props.to } <strong>{ props.tt }</strong></h1>
-					<div class="-countdown">
+					<div class={ urgentclass }>
 
-					<div class="bloc-time days" data-init-value="00" style="display: none">
+					<div class="bloc-time days" data-init-value="00"  ref={c => this.daysblock=c} style={ daysblock }>
 						<span class="count-title">Days</span>
 
 						<div class="figure days days-1">
@@ -188,7 +186,7 @@ export default class SidebarCountdown extends Component {
 							</span>
 							<span class="bottom">0</span>
 							<span class="bottom-back">
-								<span>0</span>
+								<span >0</span>
 							</span>
 						</div>
 
@@ -204,7 +202,7 @@ export default class SidebarCountdown extends Component {
 						</div>
 					</div>
 
-						<div class="bloc-time hours" data-init-value="00">
+						<div class="bloc-time hours" data-init-value="00"  ref={c => this.hoursblock=c}>
 							<span class="count-title">Hours</span>
 
 							<div class="figure hours hours-1">
@@ -230,7 +228,7 @@ export default class SidebarCountdown extends Component {
 							</div>
 						</div>
 
-						<div class="bloc-time min" data-init-value="0">
+						<div class="bloc-time min" data-init-value="0"  ref={c => this.minutesblock=c}>
 							<span class="count-title">Minutes</span>
 
 							<div class="figure min min-1">
@@ -256,7 +254,7 @@ export default class SidebarCountdown extends Component {
 							</div>
 						</div>
 
-						<div class="bloc-time sec" data-init-value="0">
+						<div class="bloc-time sec" data-init-value="0" style={ secondsblock }  ref={c => this.secondsblock=c}>
 							<span class="count-title">Seconds</span>
 
 								<div class="figure sec sec-1">
