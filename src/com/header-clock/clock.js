386d7299a4c086f2589b35743945a5d5d11cb02c
Mike Kasprzak <mike@sykhronics.com>
2016-12-07 02:51:03 -0500

    more tweaks, header clock

---
+++ src/com/header-clock/clock.js
@@ -0,0 +1,18 @@
+import { h, Component }				from 'preact/preact';
+//import NavLink 						from 'com/nav-link/link';
+import ClockFlip 						from 'com/clock-flip/flip';
+
+
+export default class HeaderClock extends Component {
+	constructor( props ) {
+		super(props);
+	}
+
+	render( {rows} ) {
+		return (
+			<div class="header-base header-clock outside">
+				<ClockFlip date={new Date()} />
+			</div>
+		);
+	}
+}
