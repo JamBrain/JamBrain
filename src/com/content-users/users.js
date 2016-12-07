07a24dfeb0eeee3b3eb5e303488ef722c614c6fa
Mike Kasprzak <mike@sykhronics.com>
2016-12-07 04:04:15 -0500

    added stubs and 404's for user

---
+++ src/com/content-users/users.js
@@ -0,0 +1,25 @@
+import { h, Component } 				from 'preact/preact';
+import SVGIcon 							from 'com/svg-icon/icon';
+import NavLink 							from 'com/nav-link/link';
+
+export default class ContentUsers extends Component {
+	constructor( props ) {
+		super(props);
+	}
+	
+	render( {extra} ) {
+		
+		if ( extra.length ) {
+			return (
+				<div class="content-base content-post">
+					<strong>404:</strong> User "{extra[0]}" not found.
+				</div>
+			);
+		}
+		return (
+			<div class="content-base content-post">
+				{"Users page... I don't yet know what goes here."}
+			</div>
+		);
+	}
+}
