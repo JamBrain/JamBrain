1e8216dbea747d21e7c30a622bd3b7a8d96750b1
Mike Kasprzak <mike@sykhronics.com>
2016-12-15 09:13:27 -0500

    finished reworking the shrub/tools folder

--- src/shrub/src/gen.sh
+++ src/shrub/src/gen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-php ../tools/gen_includes constants.php . core global node>constants.php
+php ../tools/gen/gen-includes constants.php . core global node>constants.php
 
-php ../tools/gen_includes plugin.php . core>plugin.php
+php ../tools/gen/gen-includes plugin.php . core>plugin.php
 
--- src/shrub/src/plugin.php
+++ src/shrub/src/plugin.php
@@ -1,4 +1,4 @@
 <?php
-/// Generated with "gen_includes plugin.php . core" -- **DO NOT EDIT!**
+/// Generated with "gen-includes plugin.php . core" -- **DO NOT EDIT!**
 include __DIR__."/core/plugin.php";
 
