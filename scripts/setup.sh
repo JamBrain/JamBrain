e1e50940e946eff4b0f8d7ba82cb28a88c738104
Mike Kasprzak <mike@sykhronics.com>
2016-12-18 11:22:28 -0500

    fixed some PHP 7.1 errors, symlinks

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,15 +11,15 @@ cd $SCRIPTPATH
 (cd apache ; sh add-ports.sh)
 
 # Add Symlinks to output files
-ln -s ../../.output/.build/public-ludumdare.com/all.js www/public-ludumdare.com/-/all.js
-ln -s ../../.output/.build/public-ludumdare.com/all.css www/public-ludumdare.com/-/all.css
-ln -s ../../.output/.build/public-ludumdare.com/all.svg www/public-ludumdare.com/-/all.svg
-ln -s ../../.output/.build/public-jammer.vg/all.js www/public-jammer.vg/-/all.js
-ln -s ../../.output/.build/public-jammer.vg/all.css www/public-jammer.vg/-/all.css
-ln -s ../../.output/.build/public-jammer.vg/all.svg www/public-jammer.vg/-/all.svg
-ln -s ../../.output/.build/public-jammer.bio/all.js www/public-jammer.bio/-/all.js
-ln -s ../../.output/.build/public-jammer.bio/all.css www/public-jammer.bio/-/all.css
-ln -s ../../.output/.build/public-jammer.bio/all.svg www/public-jammer.bio/-/all.svg
+ln -s ../../.output/.build/public-ludumdare.com/all.js /vagrant/www/public-ludumdare.com/-/all.js
+ln -s ../../.output/.build/public-ludumdare.com/all.css /vagrant/www/public-ludumdare.com/-/all.css
+ln -s ../../.output/.build/public-ludumdare.com/all.svg /vagrant/www/public-ludumdare.com/-/all.svg
+ln -s ../../.output/.build/public-jammer.vg/all.js /vagrant/www/public-jammer.vg/-/all.js
+ln -s ../../.output/.build/public-jammer.vg/all.css /vagrant/www/public-jammer.vg/-/all.css
+ln -s ../../.output/.build/public-jammer.vg/all.svg /vagrant/www/public-jammer.vg/-/all.svg
+ln -s ../../.output/.build/public-jammer.bio/all.js /vagrant/www/public-jammer.bio/-/all.js
+ln -s ../../.output/.build/public-jammer.bio/all.css /vagrant/www/public-jammer.bio/-/all.css
+ln -s ../../.output/.build/public-jammer.bio/all.svg /vagrant/www/public-jammer.bio/-/all.svg
 
 # Create tables
 (cd /vagrant/www/src/shrub/tools; echo YES | php table-create)
