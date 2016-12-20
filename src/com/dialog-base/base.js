fbd93f1fb02264ea0abd477909ecd5db885f14cb
Mike Kasprzak <mike@sykhronics.com>
2016-12-20 18:04:50 -0500

    fixed tab focus of common dialogs

--- src/com/dialog-base/base.js
+++ src/com/dialog-base/base.js
@@ -4,92 +4,69 @@ export default class DialogBase extends Component {
 	constructor( props ) {
 		super(props);
 		
-//		this.eventWheel = this.eventWheel.bind(this);
-//		this.eventKey = this.eventKey.bind(this);
-//		this.eventFocus = this.eventFocus.bind(this);
+		this.eventKey = this.eventKey.bind(this);
+		this.eventFocus = this.eventFocus.bind(this);
 
 		this.onAbort = this.onAbort.bind(this);
 		this.onDialogClick = this.onDialogClick.bind(this);
 	}
 	
-//	getFocusable() {
-//		return document.getElementsByClassName("focusable");
-//	}
+	componentDidMount() {
+		document.body.addEventListener('keydown', this.eventKey);
+		window.addEventListener('focus', this.eventFocus);
+		window.addEventListener('pageshow', this.eventFocus);		// iOS
+	}
+	
+	componentWillUnmount() {
+		document.body.removeEventListener('keydown', this.eventKey);
+		window.removeEventListener('focus', this.eventFocus);
+		window.removeEventListener('pageshow', this.eventFocus);	// iOS
+	}
 
-//	componentDidMount() {
-////		console.log("DialogBase: componentDidMount");
-//		document.body.addEventListener('mousewheel', this.eventWheel);
-//		document.body.addEventListener('keydown', this.eventKey);
-////		window.addEventListener('focus', this.eventFocus);
-//	}
+	getFocusable() {
+		return document.getElementsByClassName("focusable");
+	}
 	
-//	componentWillUnmount() {
-////		console.log("DialogBase: componentWillUnmount");
-//		document.body.removeEventListener('mousewheel', this.eventWheel);
-//		document.body.removeEventListener('keydown', this.eventKey);
-////		window.removeEventListener('focus', this.eventFocus);
-//	}
+	eventFocus( e ) {
+		var focusable = this.getFocusable();
+		
+		if ( focusable.length ) {
+			if ( !focusable[document.activeElement.id] ) {
+				e.preventDefault();
+				focusable[0].focus();
+			}
+		}
+	}
 	
-//	eventWheel( e ) {
-//		// Disables Mouse Wheel
-//		if ( document.getElementById("dialog-background") ) {
-//			e.preventDefault();
-//		}
-//	}
-	
-//	eventFocus( e ) {
-//		if ( document.getElementById("dialog-background") ) {
-//			var focusable = this.getFocusable();
-//			
-//			if ( !focusable[document.activeElement.id] ) {
-//				e.preventDefault();
-//				focusable[0].focus();					
-//			}
-//		}
-//	}
-	
-//	eventKey( e ) {
-//		var el = document.getElementById("dialog-background");
-//		
-//		if ( el ) {
-//			// TODO: Spacebar needs to be ignored, but only if not in an input box
-//			
-//			var keys_to_disable = [
-//				33, 34, 			// PgUp, PgDown
-//				35, 36,				// End, Home
-//				37, 38, 39, 40,		// Left, Up, Right, Down
-//			];
-//			
-//			// Keys Above
-//			if( keys_to_disable.indexOf(e.keyCode) >= 0) {
-//				e.preventDefault();
-//			}
-//			// Tab
-//			else if ( e.keyCode == 9 ) {
-//				var focusable = this.getFocusable();
-//				var active = document.activeElement.id;
-//				
-//				if ( !focusable[active] ) {
-//					e.preventDefault();
-//					focusable[0].focus();					
-//				}
-//				else if ( focusable[active] == focusable[focusable.length-1] && !e.shiftKey ) {
-//					e.preventDefault();
-//					focusable[0].focus();
-//				}
-//				else if ( focusable[active] == focusable[0] && e.shiftKey ) {
-//					e.preventDefault();
-//					focusable[focusable.length-1].focus();
-//				}			
-//			}
-//			// ESC key
-//			else if ( e.keyCode == 27 ) {
-//				if ( !el.hasAttribute("explicit") ) {
-//					this.onAbort();
-//				}
-//			}
-//		}
-//	}
+	eventKey( e ) {
+		// Tab
+		if ( e.keyCode == 9 ) {
+			var focusable = this.getFocusable();
+			var active = document.activeElement.id;
+
+			if ( focusable.length ) {
+				if ( !focusable[active] ) {
+					e.preventDefault();
+					focusable[0].focus();					
+				}
+				else if ( focusable[active] == focusable[focusable.length-1] && !e.shiftKey ) {
+					e.preventDefault();
+					focusable[0].focus();
+				}
+				else if ( focusable[active] == focusable[0] && e.shiftKey ) {
+					e.preventDefault();
+					focusable[focusable.length-1].focus();
+				}
+			}
+		}
+		// ESC key
+		else if ( e.keyCode == 27 ) {
+			if ( !this.props.explicit ) {
+				e.preventDefault();
+				this.onAbort();
+			}
+		}
+	}
 	
 
 	onAbort() {
@@ -110,12 +87,12 @@ export default class DialogBase extends Component {
 		if ( !props.explicit ) {
 			parent_props.onclick = props.oncancel ? props.oncancel : this.onAbort;
 		}
-		
+
 		var child_props = {
 			'class': 'dialog-base'+(props.class ? ' '+props.class : ''),
 			'onclick': this.onDialogClick
 		};
-		
+
 		return (
 			<div {...parent_props}>
 				<div {...child_props}>
