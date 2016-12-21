a4f6f9be9becf2e4bf2e185317de88da8f15fb7e
Mike Kasprzak <mike@sykhronics.com>
2016-12-20 19:00:34 -0500

    Can now reset passwords again

--- src/com/dialog-password/password.js
+++ src/com/dialog-password/password.js
@@ -1,7 +1,8 @@
 import { h, Component } 				from 'preact/preact';
 import Sanitize							from '../../internal/sanitize/sanitize';
-import DialogCommon						from 'com/dialog-common/common';
 
+import DialogCommon						from 'com/dialog-common/common';
+import NavSpinner						from 'com/nav-spinner/spinner';
 import LabelYesNo						from 'com/label-yesno/yesno';
 
 import $User							from '../shrub/js/user/user';
@@ -11,37 +12,15 @@ export default class DialogPassword extends Component {
 		super(props);
 
 		this.state = {
-			loading: true
+			'loading': true
 		};
 
 		var Vars = Sanitize.getHTTPVars();
-		console.log("v",Vars);
+		//console.log("v",Vars);
 
 		// Get activation ID
 		this.ActID = Vars.id;
 		this.ActHash = Vars.key;
-
-		// Lookup ID, and confirm this is a valid activation
-		$User.Password( this.ActID, this.ActHash.trim(), "" )
-		.then( r => {
-			if ( r.status === 200 ) {
-				this.setState({
-					node: r.node,
-					password: "",
-					password2: "",
-					loading: false
-				});
-			}
-			else {
-				console.log(r);
-				this.setState({ error: r.response, loading: false });
-			}
-		})
-		.catch( err => {
-			console.log(err);
-			this.setState({ error: err, loading: false });
-		});
-
 
 		// Bind functions (avoiding the need to rebind every render)
 		this.onPasswordChange = this.onPasswordChange.bind(this);
@@ -51,14 +30,33 @@ export default class DialogPassword extends Component {
 	}
 
 	componentDidMount() {
-//		this.loginName.focus();
+		// Lookup ID, and confirm this is a valid activation
+		$User.Password(this.ActID, this.ActHash.trim(), "")
+		.then( r => {
+			if ( r.status === 200 ) {
+				this.setState({
+					'node': r.node,
+					'password': "",
+					'password2': "",
+					'loading': false
+				});
+			}
+			else {
+				console.log(r);
+				this.setState({ 'error': r.response, 'loading': false });
+			}
+		})
+		.catch( err => {
+			console.log(err);
+			this.setState({ 'error': err, 'loading': false });
+		});
 	}
 
 	onPasswordChange( e ) {
-		this.setState({ password: e.target.value, error: null });
+		this.setState({ 'password': e.target.value, 'error': null });
 	}
 	onPassword2Change( e ) {
-		this.setState({ password2: e.target.value, error: null });
+		this.setState({ 'password2': e.target.value, 'error': null });
 	}
 
 	isValidPassword() {
@@ -97,21 +95,21 @@ export default class DialogPassword extends Component {
 				if ( r.status === 200 ) {
 					console.log('success',r);
 					//location.href = "#";//user-loggedin";
-					this.setState({ success: true, error: null });
+					this.setState({ 'success': true, 'error': null });
 				}
 				else {
-					console.log(r);
-					this.setState({ error: r.message ? r.message : r.response });
+					//console.log(r);
+					this.setState({ 'error': r.message ? r.message : r.response });
 				}
 				return r;
 			})
 			.catch( err => {
-				console.log(err);
-				this.setState({ error: err });
+				//console.log(err);
+				this.setState({ 'error': err });
 			});
 		}
 		else {
-			this.setState({ error: "Form incomplete or invalid" });
+			this.setState({ 'error': "Form incomplete or invalid" });
 		}
 	}
 
@@ -121,41 +119,44 @@ export default class DialogPassword extends Component {
 	}
 
 	render( props, {password, password2, node, success, loading, error} ) {
-		var ErrorMessage = error ? {'error': error} : {};
-
-		var title = "Reset Password: Step 2";
+		var new_props = {
+			'title': 'Reset Password: Step 2'
+		};
+		if ( error ) {
+			new_props.error = error;
+		}
 
 		if ( loading ) {
 			return (
-				<DialogCommon title={title} explicit {...ErrorMessage}>
-					<div>
-						Please wait...
-					</div>
+				<DialogCommon empty explicit {...new_props}>
+					<NavSpinner />
 				</DialogCommon>
 			);
 		}
 		else if ( !node ) {
 			return (
-				<DialogCommon title={title} ok explicit {...ErrorMessage}>
+				<DialogCommon ok explicit {...new_props}>
 					{"Password can not be reset."}
 				</DialogCommon>
 			);
 		}
 		else if ( success ) {
 			return (
-				<DialogCommon title={title} ok explicit onclick={this.doFinishReset} {...ErrorMessage}>
+				<DialogCommon ok onok={this.doFinishReset} explicit {...new_props}>
 					Password Reset. You can now <strong>Log In</strong>.
 				</DialogCommon>
 			);
 		}
 		else {
 			return (
-				<DialogCommon title={title} ok cancel oktext="Save" explicit onclick={this.doResetPassword} {...ErrorMessage}>
+				<DialogCommon ok oktext="Save" onok={this.doResetPassword} cancel explicit {...new_props}>
 					<div>
-						<input id="dialog-password-password2" oninput={this.onPasswordChange} class="-text focusable" type="password" name="password" maxlength="128" placeholder="Password" value={password} /><LabelYesNo value={this.isValidPassword()} />
+						<input autofocus id="dialog-password-password2" oninput={this.onPasswordChange} class="-text focusable" type="password" name="password" maxlength="128" placeholder="Password" value={password} />
+						<LabelYesNo value={this.isValidPassword()} />
 					</div>
 					<div>
-						<input id="dialog-password-password2" oninput={this.onPassword2Change} class="-text focusable" type="password" name="password2" maxlength="128" placeholder="Password again" value={password2} /><LabelYesNo value={this.isValidPassword2()} />
+						<input id="dialog-password-password2" oninput={this.onPassword2Change} class="-text focusable" type="password" name="password2" maxlength="128" placeholder="Password again" value={password2} />
+						<LabelYesNo value={this.isValidPassword2()} />
 					</div>
 				</DialogCommon>
 			);
