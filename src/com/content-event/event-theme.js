6e6e1e4c28813d5d205022e783469fddb7bf905f
Mike Kasprzak <mike@sykhronics.com>
2017-04-18 09:52:37 -0400

    my changes so far, ack, gotta fix a bug

--- src/com/content-event/event-theme.js
+++ src/com/content-event/event-theme.js
@@ -2,6 +2,16 @@ import { h, Component } 				from 'preact/preact';
 import SVGIcon 							from 'com/svg-icon/icon';
 import NavLink 							from 'com/nav-link/link';
 
+import ContentEventHome					from 'com/content-event/event-home';
+import ContentEventIdea					from 'com/content-event/event-idea';
+import ContentEventSlaughter			from 'com/content-event/event-slaughter';
+import ContentEventFusion				from 'com/content-event/event-fusion';
+import ContentEventList					from 'com/content-event/event-list';
+//import ContentEventFinal				from 'com/content-event/event-final';
+//import ContentEventJudging				from 'com/content-event/event-judging';
+//import ContentEventResults				from 'com/content-event/event-results';
+
+
 import ButtonBase						from 'com/button-base/base';
 
 import ContentCommon					from 'com/content-common/common';
@@ -19,6 +29,25 @@ export default class ContentEventTheme extends Component {
 
 
 	render( props ) {
+//		if ( node.slug ) {
+//			let ThemeMode = (node.meta['theme-mode']) ? parseInt(node.meta['theme-mode']) : 0;
+//
+//			switch (ThemeMode) {
+//				case 1:
+//				EventBody = <ContentEventIdea node={node} user={user} path={_path} extra={_extra} />;
+//				break;
+//			case 2:
+//				EventBody = <ContentEventSlaughter node={node} user={user} path={_path} extra={_extra} />;
+//				break;
+//			case 3:
+//				EventBody = <ContentEventFusion node={node} user={user} path={_path} extra={_extra} />;
+//				break;
+//			case 4:
+//				EventBody = <ContentEventList node={node} user={user} path={_path} extra={_extra} />;
+//				break;
+//
+			
+		
 		return (
 			<ContentCommon {...props}>
 				Wine
--- src/com/content-nav/nav-theme.js
+++ src/com/content-nav/nav-theme.js
@@ -3,6 +3,7 @@ import SVGIcon 							from 'com/svg-icon/icon';
 import NavLink 							from 'com/nav-link/link';
 
 import ContentNavButton					from 'com/content-nav/nav-button';
+import ContentError						from 'com/content-error/error';
 
 
 export default class ContentNavTheme extends Component {
@@ -11,20 +12,37 @@ export default class ContentNavTheme extends Component {
 	}
 	
 	render( {node, user, path, extra}, {} ) {
-		var NewPath = '/'+ (extra ? extra.join('/') : '');
-		
-		if ( NewPath === '/theme' ) {
-			NewPath = '/theme/1';
-		}
-		
-		return (
-			<div class="-body">
-				<div class="content-base content-nav content-nav-root">
-					<ContentNavButton path={path+NewPath} icon='suggestion' href={path+'/theme/idea'}>Suggestions</ContentNavButton>
-					<ContentNavButton path={path+NewPath} icon='fire' href={path+'/theme/slaughter'}>Slaughter</ContentNavButton>
-					<ContentNavButton path={path+NewPath} icon='ticket' href={path+'/theme/1'}>Round 1</ContentNavButton>
+		if ( node.slug ) {
+			let ThemeMode = (node.meta['theme-mode']) ? parseInt(node.meta['theme-mode']) : 0;
+	
+			var NewPath = '/'+ (extra ? extra.join('/') : '');
+			if ( NewPath === '/theme' ) {
+				NewPath = '/theme/1';
+			}
+			
+			var ShowRounds = [];
+			if ( ThemeMode >= 1 )
+				ShowRounds.push(<ContentNavButton path={path+NewPath} icon='suggestion' href={path+'/theme/idea'}>Suggestions</ContentNavButton>);
+			if ( ThemeMode >= 2 )
+				ShowRounds.push(<ContentNavButton path={path+NewPath} icon='fire' href={path+'/theme/slaughter'}>Slaughter</ContentNavButton>);
+			if ( ThemeMode >= 3 )
+				ShowRounds.push(<ContentNavButton path={path+NewPath} icon='heart-broken' href={path+'/theme/fusion'}>Fusion</ContentNavButton>);
+			if ( ThemeMode >= 4 ) {
+				ShowRounds.push(<ContentNavButton path={path+NewPath} icon='ticket' href={path+'/theme/1'}>Round 1</ContentNavButton>);
+			}
+	
+			
+			return (
+				<div class="-body">
+					<div class="content-base content-nav content-nav-root">
+						{ShowRounds}
+					</div>
 				</div>
-			</div>
-		);
+			);
+		}
+		else {
+			return null;
+//			return <ContentError />;
+		}
 	}
 }
