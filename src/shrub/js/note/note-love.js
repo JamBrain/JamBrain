9aab6abb32e048c98a9fbcf0b62b1bb34b30f659
Mike Kasprzak <mike@sykhronics.com>
2017-04-21 10:36:08 -0400

    oops, added missing JS api for note love

--- src/shrub/js/note/note-love.js
+++ src/shrub/js/note/note-love.js
@@ -1,12 +1,35 @@
 import Fetch	 				from '../internal/fetch';
 //import Cache	 				from '../internal/cache';
-import Memory	 				from '../internal/memory';
+//import Memory	 				from '../internal/memory';
 
 export default {
 //	Get,
-//	GetMy,
-//	Add,
-//	Remove
+	GetMy,
+	Add,
+	Remove
 };
 
+
 //var STORAGE = Memory;
+
+//export function SetMy( node, Data ) {
+//	var key = 'NOTE|LOVE|MINE|'+node;
+//	return STORAGE.Store(key, Data);
+//}
+
+// Get my love of comments for a specific node
+function GetMy( node ) {
+	return Fetch.Get(API_ENDPOINT+'/vx/note/love/getmy/'+node, true);
+}
+
+// Add/Remove love from a specific comment * -- node is necessary, for when we start remembering what we've loved locally
+export function Add( node, note ) {
+//	STORAGE.Push('NOTE|LOVE|MINE|'+node, note);
+
+	return Fetch.Get(API_ENDPOINT+'/vx/note/love/add/'+note, true);
+}
+export function Remove( node, note ) {
+//	STORAGE.Pop('NOTE|LOVE|MINE|'+node, note);
+
+	return Fetch.Get(API_ENDPOINT+'/vx/note/love/remove/'+note, true);
+}
