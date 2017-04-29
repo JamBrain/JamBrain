cb5f4689759a9bc4ed9d0c0a62c2509910e03637
Mike Kasprzak <mike@sykhronics.com>
2017-04-28 23:05:34 -0400

    merged in the conversion steps from my old tool

--- public-static/content.php
+++ public-static/content.php
@@ -97,6 +97,7 @@ const IMAGE_TYPE = [
 $out['width'] = null;
 $out['height'] = null;
 $out['format'] = null;
+$out['fit'] = null;
 $out['debug'] = null;
 
 function hasChanges( &$arr ) {
@@ -106,7 +107,6 @@ function hasChanges( &$arr ) {
 	}
 	return false;
 }
-
 
 function redirectToSelfAndExit() {
 	global $out_file;
@@ -123,6 +123,35 @@ function redirectToSelfAndExit() {
 	exit;
 }
 
+function do_proc($cmd,&$data) {
+	$proc = proc_open(
+		$cmd,
+		[
+			['pipe','r'],
+			['pipe','w'],
+			['pipe','w']
+		],
+		$pipes
+	);
+	
+	if ($proc) {
+		fwrite($pipes[0],$data);
+		fclose($pipes[0]);
+		$ret = stream_get_contents($pipes[1]);
+		fclose($pipes[1]);
+		$err = stream_get_contents($pipes[2]);
+		fclose($pipes[2]);
+		$code = proc_close($proc);
+		
+		if ( $code ) {
+			EmitErrorAndExit('PROC ERROR ['.$code.']: '.$err);
+		}
+		
+		return $ret;
+	}
+	return null;
+}
+
 
 // If there are any extensions left left, use them as arguments 
 foreach( $in_ext_part as &$value ) {
@@ -132,7 +161,10 @@ foreach( $in_ext_part as &$value ) {
 	else if ( $value == 'debug' ) {
 		$out['debug'] = true;
 	}
-	else if ( array_search($value, IMAGE_TYPE) ) {
+	else if ( $value == 'fit' ) {
+		$out['fit'] = true;
+	}
+	else if ( array_search($value, IMAGE_TYPE) !== false ) {
 		$out['format'] = $value;
 	}
 	else if ( $value[0] == 'w' && ($v = intval(substr($value, 1))) > 0 ) {
@@ -160,8 +192,35 @@ foreach( $in_ext_part as &$value ) {
 }
 
 // If a change has been requested
-if ( hasChanges( $out ) ) {
-	$image_info = getimagesize($src_fullfile);
+if ( hasChanges($out) ) {
+	// confirm that final extension is an output format
+	if ( ($of = $in_paths[count($in_paths)-1]) && ($of == 'debug' || array_search($of, IMAGE_TYPE) !== false) ) {
+		EmitError(400, "Final extension must be an output format. Invalid format '$of'");
+	}
+	
+	$data = null;
+	$in = $src_fullfile;
+	
+	// Step 0: Read file
+	if ( /*$src_is_video ||*/ $in_ext == 'gif' ) {
+		$dummy = "";
+		$data = do_proc(
+			'ffmpeg -i '.$in.' -loglevel quiet -vframes 1 -f apng pipe:1',
+			$dummy
+		);
+		// Force conversion (even though it should be the case) //
+		$file_out_convert = true;
+	}
+	else if ( $in_ext && array_search($in_ext, IMAGE_TYPE) !== false ) {
+		$data = file_get_contents($in);
+	}
+	else {
+		EmitError(400, "Unsupported input");
+	}
+	
+	// Step 1: Parse file
+	$image_info = getimagesizefromstring($data);
+//	$image_info = getimagesize($src_fullfile);
 	
 	if ( $image_info ) {
 		// It's faster to re-extract the data then ask the database
@@ -169,9 +228,74 @@ if ( hasChanges( $out ) ) {
 			'width' => $image_info[0],
 			'height' => $image_info[1],
 			'mime' => $image_info['mime'],
+			'size' => strlen($data),	// getimagesizefromstring only
 		];
 		
 		$RESPONSE['asset'] = $asset;
+
+		// Step 2: Resize, Crop, and/or Convert the file
+		if ( $out['width'] || $out['height'] || $out['format'] != $in_ext ) {
+			// Strip extra data //
+			$option = '-strip';
+			
+			// http://www.imagemagick.org/Usage/resize/
+			if ( $out['width'] || $out['height'] ) {
+				if ( $out['width'] && $out['height'] ) {
+					$option .= ' -resize '.$out['width'].'x'.$out['height'];
+				}
+				else if ( $out['width'] ) {
+					$option .= ' -resize '.$out['width'];
+				}
+				else if ( $out['height'] ) {
+					$option .= ' -resize x'.$out['width'];
+				}
+
+				// NOTE: modifiers append to resize strings, so this must come next //
+				if ( $out['fit'] ) {
+					// Fit to dimensions //
+					$option .= '^ -gravity center';
+					$option .= ' -extent '.$out['width'].'x'.$out['height'];
+				}
+				else {
+					// Don't allow resizing larger than original //
+					$option .= '\>';
+				}
+			}
+			
+			// Run ImageMagick //
+			// NOTE: This will fail if there exists a file named "png:-", "webp:-", etc.
+			$data = do_proc(
+				'convert - '.$option.' '.$out['format'].':-',
+				$data
+			);
+		}
+
+		// Step 3: Optimize
+		if ( true ) {
+//			if ( ($file_out_ext == 'gif') ) {
+//				// http://www.lcdf.org/gifsicle/
+//				EmitErrorAndExit("ERROR: Unsupported optimizer GIF");
+//			}
+//			else 
+			if ( ($out['format'] == 'png') ) {
+				// https://pngquant.org/
+				// https://pngquant.org/php.html
+				
+				$file_out_min_quality = 60;
+				$file_out_max_quality = 95;
+
+				$data = do_proc(
+					'pngquant --quality='.$file_out_min_quality.'-'.$file_out_max_quality.' -',
+					$data
+				);
+			}
+//				else if ( ($file_out_ext == 'jpg') || ($file_out_ext == 'jpeg') ) {
+//					EmitErrorAndExit("ERROR: Unsupported optimizer JPEG");
+//				}
+//				else if ( ($file_out_ext == 'webp') ) {
+//					EmitErrorAndExit("ERROR: Unsupported optimizer WEBP");
+//				}
+		}	
 
 		// Debug mode, output JSON instead of an image
 		if ( $out['debug'] ) {
@@ -181,9 +305,14 @@ if ( hasChanges( $out ) ) {
 		}
 		// Image output mode
 		else {
-			
-			// hack
-			Emit($RESPONSE);
+			// Step 4: Write File
+			if ( !file_exists($out_fullpath) ) {
+				mkdir($out_fullpath, 0755, true);
+			}
+			file_put_contents($out_file, $data);
+
+			// Step 5: Redirect to self and exit
+			redirectToSelfAndExit();
 		}
 	}
 	else {
