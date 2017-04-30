9b6b8de9fd1d946c832748a8f84478d01de35698
Mike Kasprzak <mike@sykhronics.com>
2017-04-30 18:24:09 -0400

    scoring and grading

---
+++ src/shrub/js/grade/grade.js
@@ -0,0 +1,21 @@
+import Fetch	 				from '../internal/fetch';
+
+export default {
+	GetMy,
+	GetInfo,
+	
+	Add,
+	
+	OptOut
+};
+
+export function GetMy( node ) {
+
+}
+export function GetInfo( node ) {
+
+}
+
+export function Add( node, grade, value ) {
+
+}
