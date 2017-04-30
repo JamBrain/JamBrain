f797ab5756ed53344af69df1f326cc64685839b0
Mike Kasprzak <mike@sykhronics.com>
2017-04-30 19:51:48 -0400

    added JS api

--- src/shrub/js/grade/grade.js
+++ src/shrub/js/grade/grade.js
@@ -5,17 +5,30 @@ export default {
 	GetInfo,
 	
 	Add,
+	Remove,
 	
-	OptOut
+	OptOut,
+	OptIn
 };
 
 export function GetMy( node ) {
-
+	return Fetch.Get(API_ENDPOINT+'/vx/grade/getmy/'+node, true);
 }
 export function GetInfo( node ) {
-
+	return Fetch.Get(API_ENDPOINT+'/vx/grade/getinfo/'+node, true);
 }
 
 export function Add( node, grade, value ) {
+	return Fetch.Get(API_ENDPOINT+'/vx/grade/add/'+node+'/'+grade+'/'+value, true);
+}
+export function Remove( node, grade ) {
+	return Fetch.Get(API_ENDPOINT+'/vx/grade/remove/'+node+'/'+grade, true);
+}
+
+// These use the NODE API
+export function OptOut( node, grade ) {
+
+}
+export function OptIn( node, grade ) {
 
 }
