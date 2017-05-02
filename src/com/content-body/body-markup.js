00e4179bbd78fd400e4775c6f9ae0a416f9f9a2d
Jono Kemball <jonathankemball@gmail.com>
2017-05-03 03:17:08 +1200

    upgraded marked to es6

--- src/com/content-body/body-markup.js
+++ src/com/content-body/body-markup.js
@@ -1,5 +1,6 @@
 import { h, Component } 				from 'preact/preact';
 import { shallowDiff }	 				from 'shallow-compare/index';
+import marked 								from '../../custom/marked/marked';
 
 export default class ContentBodyMarkup extends Component {
 	constructor( props ) {
@@ -17,17 +18,23 @@ export default class ContentBodyMarkup extends Component {
 		// '-body' for backwards compatibility (remove me)
 		var _class = "content-body content-body-markup -body markup" + (props.class ? " "+props.class : "");
 
-		return <div class={_class} dangerouslySetInnerHTML={{__html:_body}} />;
+		var markedOptions = {
+			highlight: function(code, lang) {
+				var language = Prism.languages.clike;
+				if (Prism.languages[lang])
+					language = Prism.languages[lang];
+				return Prism.highlight(code, language);
+			},
+			sanitize: true, // disable HTML
+			smartypants: true, // enable automatic fancy quotes, ellipses, dashes
+			langPrefix: 'language-'
+		};
+
+		// NOTE: only parses the first child
+		//var Text = props.children.length ? marked.parse(props.children[0]) : "";
+		var mrkd = new marked();
+		markdown = mrkd.parse(_body, markedOptions);
+
+		return (<div class={_class}>{markdown}</div>);
 	}
 }
-
-marked.setOptions({
-	highlight: function( code, lang ) {
-		var language = Prism.languages.clike;
-		if ( Prism.languages[lang] )
-			language = Prism.languages[lang];
-		return Prism.highlight( code, language );
-	},
-	sanitize: true,			// disable HTML
-	smartypants: true,		// enable automatic fancy quotes, ellipses, dashes
-});
--- src/com/content-common/common-body-markup.js
+++ src/com/content-common/common-body-markup.js
@@ -1,92 +1,91 @@
-import { h, Component } 				from 'preact/preact';
-import { shallowDiff }	 				from 'shallow-compare/index';
+import {h, Component} 				from 'preact/preact';
+import {shallowDiff} 					from 'shallow-compare/index';
 
-import NavLink							from 'com/nav-link/link';
-import SVGIcon							from 'com/svg-icon/icon';
-import InputTextArea					from 'com/input-textarea/input-textarea';
+import NavLink 								from 'com/nav-link/link';
+import SVGIcon 								from 'com/svg-icon/icon';
+import InputTextArea 					from 'com/input-textarea/input-textarea';
+import marked 								from '../../custom/marked/marked';
 
 export default class ContentCommonBodyMarkup extends Component {
-	constructor( props ) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	shouldComponentUpdate( nextProps ) {
-		return shallowDiff(this.props, nextProps);
-	}
-	
-	render( props ) {
-		var Class = [
-			"content-common-body",
-			"-markup"
-		];
-		if ( typeof props.class == 'string' ) {
-			Class = Class.concat(props.class.split(' '));
-		}
-		
-		var Label = props.label ? props.label : "Description";
-		var Placeholder = props.placeholder ? props.placeholder : "Description";
-		
-		var Text = props.children.join('');
+  shouldComponentUpdate(nextProps) {
+    return shallowDiff(this.props, nextProps);
+  }
 
-		if (props.editing) {
-			//var Height = this.textarea ? this.textarea.scrollHeight : 0;
+  render(props) {
+    var Class = ["content-common-body", "-markup"];
+    if (typeof props.class == 'string') {
+      Class = Class.concat(props.class.split(' '));
+    }
 
-			var Limit = props.limit ? props.limit : 8192;
-			//var Chars = props.children[0] ? props.children[0].length : 0;
-			
-			return (
-				<div class={Class}>
-					<div class="-label">{Label}</div>
-					<InputTextArea
-						user={props.user}
-						value={Text} 
-						onmodify={props.onmodify} 
-						placeholder={Placeholder} 
-						ref={(input) => { this.textarea = input; }} 
-						maxlength={Limit}
-					/>
-				</div>
-			);
+    var Label = props.label
+      ? props.label
+      : "Description";
+    var Placeholder = props.placeholder
+      ? props.placeholder
+      : "Description";
 
-//					<div class="-textarea">
-//						<InputTextArea 
-//							name="paragraph_text" 
-//							value={props.children} 
-//							onmodify={props.onmodify} 
-//							placeholder={props.placeholder} 
-//							ref={(input) => { this.textarea = input; }} 
-//							maxlength={Limit}
-//						/>
-//					</div>
-//					<div class="-footer">
-//						<div class="-right"><span class="-chars">{Chars}</span>/<span class="-limit">{Limit}</span></div>
-//						<div class="-left">Supports <NavLink blank href="/markdown"><SVGIcon>markdown</SVGIcon> <strong>Markdown</strong></NavLink> and <NavLink href="//emoji.codes/"><strong>:emoji_codes:</strong></NavLink></div>
-//					</div>
-		}
-		else {
-			Class.push("markup");
-			
-			if ( !Text.trim().length ) {
-				Text = Placeholder;
-			}
+    var Text = props.children.join('');
 
-			// NOTE: only parses the first child
-			//var Text = props.children.length ? marked.parse(props.children[0]) : "";
-			Text = marked.parse(Text);
+    if (props.editing) {
+      //var Height = this.textarea ? this.textarea.scrollHeight : 0;
 
-			return <div class={Class} dangerouslySetInnerHTML={{__html: Text}} />;
-		}
-	}
+      var Limit = props.limit
+        ? props.limit
+        : 8192;
+      //var Chars = props.children[0] ? props.children[0].length : 0;
+
+      return (
+        <div class={Class}>
+          <div class="-label">{Label}</div>
+          <InputTextArea user={props.user} value={Text} onmodify={props.onmodify} placeholder={Placeholder} ref={(input) => {
+            this.textarea = input;
+          }} maxlength={Limit}/>
+        </div>
+      );
+
+      //					<div class="-textarea">
+      //						<InputTextArea
+      //							name="paragraph_text"
+      //							value={props.children}
+      //							onmodify={props.onmodify}
+      //							placeholder={props.placeholder}
+      //							ref={(input) => { this.textarea = input; }}
+      //							maxlength={Limit}
+      //						/>
+      //					</div>
+      //					<div class="-footer">
+      //						<div class="-right"><span class="-chars">{Chars}</span>/<span class="-limit">{Limit}</span></div>
+      //						<div class="-left">Supports <NavLink blank href="/markdown"><SVGIcon>markdown</SVGIcon> <strong>Markdown</strong></NavLink> and <NavLink href="//emoji.codes/"><strong>:emoji_codes:</strong></NavLink></div>
+      //					</div>
+    } else {
+      Class.push("markup");
+
+      if (!Text.trim().length) {
+        Text = Placeholder;
+      }
+
+      var markedOptions = {
+        highlight: function(code, lang) {
+          var language = Prism.languages.clike;
+          if (Prism.languages[lang])
+            language = Prism.languages[lang];
+          return Prism.highlight(code, language);
+        },
+        sanitize: true, // disable HTML
+        smartypants: true, // enable automatic fancy quotes, ellipses, dashes
+        langPrefix: 'language-'
+      };
+
+      // NOTE: only parses the first child
+      //var Text = props.children.length ? marked.parse(props.children[0]) : "";
+			var mrkd = new marked();
+      markdown = mrkd.parse(Text, markedOptions);
+
+      return (<div class={Class}>{markdown}</div>);
+    }
+  }
 }
-
-marked.setOptions({
-	highlight: function( code, lang ) {
-		var language = Prism.languages.clike;
-		if ( Prism.languages[lang] )
-			language = Prism.languages[lang];
-		return Prism.highlight( code, language );
-	},
-	sanitize: true,			// disable HTML
-	smartypants: true,		// enable automatic fancy quotes, ellipses, dashes
-	langPrefix: 'language-',
-});
