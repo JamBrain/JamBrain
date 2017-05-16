e1df10760847d19028eb0d510e60c3c7134105ef
Mike Kasprzak <mike@sykhronics.com>
2017-05-16 14:43:12 -0400

    damn it

--- src/com/content-item/item.js
+++ src/com/content-item/item.js
@@ -270,18 +270,18 @@ export default class ContentItem extends Component {
 					Title = "Ratings received";
 					Warning = Score < 20.0;
 					if ( !Warning ) {
-						Icon = <SVGIcon baseline small>checked</SVGIcon>;
-						HoverTitle = "This game will be scored";
+						Icon = <SVGIcon baseline small>checkmark</SVGIcon>;
+						HoverTitle = "This will be scored";
 					}
 					else {
 						Icon = <SVGIcon baseline small>warning</SVGIcon>;
-						HoverTitle = "The minimum for a score is about 20";
+						HoverTitle = "The minimum needed to score is about 20";
 					}
 				}
 				else if ( Metric.key == 'given' ) {
 					Title = "Ratings given";
-					if ( Score > 20 ) {
-						Icon = <SVGIcon baseline small>checked</SVGIcon>;
+					if ( Score > 25 ) {
+						Icon = <SVGIcon baseline small>checkmark</SVGIcon>;
 					}
 				}
 				else if ( Metric.key == 'feedback' ) {
