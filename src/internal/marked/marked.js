84d410ef11158932d10da875536f387bd237b462
Jono Kemball <jonathankemball@gmail.com>
2017-05-20 05:33:42 +1200

    Open Graph meta testing

--- src/internal/marked/marked.js
+++ src/internal/marked/marked.js
@@ -110,6 +110,41 @@ export default class marked {
     }
   }
 
+  static cleanMarkdown(markdown) {
+    output = markdown
+    //remove code
+    .replace(/^( {4}[^\n]+\n*)+/, "")
+    //remove tables
+    .replace(/^ *\|(.+)\n *\|( *[-:]+[-| :]*)\n((?: *\|.*(?:\n|$))*)\n*/, "")
+    // Remove HTML tags
+    .replace(/<(.*?)>/g, '$1')
+    // Remove setext-style headers
+    .replace(/^[=\-]{2,}\s*$/g, '')
+    // Remove footnotes?
+    .replace(/\[\^.+?\](\: .*?$)?/g, '')
+    .replace(/\s{0,2}\[.*?\]: .*?$/g, '')
+    // Remove images
+    .replace(/\!\[.*?\][\[\(].*?[\]\)]/g, '')
+    // Remove inline links
+    .replace(/\[(.*?)\][\[\(].*?[\]\)]/g, '$1')
+    // Remove Blockquotes
+    .replace(/>/g, '')
+    // Remove reference-style links?
+    .replace(/^\s{1,2}\[(.*?)\]: (\S+)( ".*?")?\s*$/g, '')
+    // Remove atx-style headers
+    .replace(/^\#{1,6}\s*([^#]*)\s*(\#{1,6})?/gm, '$1')
+    .replace(/([\*_]{1,3})(\S.*?\S)\1/g, '$2')
+    .replace(/(`{3,})(.*?)\1/gm, '$2')
+    .replace(/^-{3,}\s*$/g, '')
+    .replace(/`(.+?)`/g, '$1')
+    .replace(/\n{2,}/g, '\n\n')
+    //emoji
+    .replace(/^:([a-z_]+):/, '')
+    .replace("\n", " ");
+
+    return output;
+  }
+
   setOptions(opt) {
     Util.merge(this.constructor.defaults(), opt);
     this.options = Util.merge(this.constructor.defaults(), opt);
