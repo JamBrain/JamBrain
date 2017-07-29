cc3a52e3b35e2c0351a80a301717fbd97bf7fa81
Mike Kasprzak <mike@sykhronics.com>
2017-07-28 20:01:33 -0400

    Added theme reveal modes

--- src/com/header-whatsup/whatsup.js
+++ src/com/header-whatsup/whatsup.js
@@ -67,7 +67,25 @@ export default class HeaderWhatsup extends Component {
 						</div>
 					);
 				}
-				else if ( featured.meta['event-theme'] ) {
+				else if ( parseInt(featured.meta['theme-mode']) == 5 ) {
+					// Theme is revealed
+					if ( featured.meta['event-theme'] ) {
+						return (
+							<div class="header-base header-whatsup outside">
+								<span class="-title _font2">ON NOW:</span> <NavLink href={featured.path}><SVGIcon baseline small gap>trophy</SVGIcon>{featured.name}</NavLink> <span class="-title _font2">Theme:</span> {featured.meta['event-theme']}
+							</div>
+						);
+					}
+					// Theme is not yet revealed
+					else {
+						return (
+							<div class="header-base header-whatsup outside">
+								<span class="-title _font2">ON NOW:</span> <NavLink href={featured.path}><SVGIcon baseline small gap>trophy</SVGIcon>{featured.name}</NavLink> Theme Announcement Soon! <NavLink href="https://twitter.com/ludumdare"><SVGIcon baseline small gap>twitter</SVGIcon>Check Twitter</NavLink>
+							</div>
+						);
+					}
+				}
+				else if ( parseInt(featured.meta['theme-mode']) == 6 && featured.meta['event-theme'] ) {
 					return (
 						<div class="header-base header-whatsup outside">
 							<span class="-title _font2">RESULTS:</span> <NavLink href={featured.path}><SVGIcon baseline small gap>trophy</SVGIcon>{featured.name}</NavLink>
