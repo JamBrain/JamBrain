ba35f9d1158ee0fb341032288f2a0ae4e66fa606
Mike Kasprzak <mike@sykhronics.com>
2017-08-01 00:29:19 -0400

    judging is a bad word

--- src/com/header-whatsup/whatsup.js
+++ src/com/header-whatsup/whatsup.js
@@ -88,7 +88,7 @@ export default class HeaderWhatsup extends Component {
 				else if ( parseInt(featured.meta['theme-mode']) == 6 && featured.meta['event-theme'] ) {
 					return (
 						<div class="header-base header-whatsup outside">
-							<span class="-title _font2">JUDGING NOW:</span> <NavLink href={featured.path}><SVGIcon baseline small gap>trophy</SVGIcon>{featured.name}</NavLink> <span class="-title _font2">Theme:</span> {featured.meta['event-theme']}
+							<span class="-title _font2">PLAY+RATE NOW:</span> <NavLink href={featured.path}><SVGIcon baseline small gap>trophy</SVGIcon>{featured.name}</NavLink> <span class="-title _font2">Theme:</span> {featured.meta['event-theme']}
 						</div>
 					);
 				}
