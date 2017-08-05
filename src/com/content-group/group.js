ab968034128914c028ebfaa8bc14ecc8fb1ed71b
Mike Kasprzak <mike@sykhronics.com>
2017-08-05 10:34:52 -0400

    Fixing groups so we can browse

--- src/com/content-group/group.js
+++ src/com/content-group/group.js
@@ -2,6 +2,10 @@ import { h, Component } 				from 'preact/preact';
 
 import NavLink 							from 'com/nav-link/link';
 import NavSpinner 						from 'com/nav-spinner/spinner';
+//import SVGIcon							from 'com/svg-icon/icon';
+
+import Common							from 'com/content-common/common';
+import CommonBody						from 'com/content-common/common-body';
 
 import $Node							from '../../shrub/js/node/node';
 
@@ -15,31 +19,31 @@ export default class ContentGroup extends Component {
 	}
 	
 	componentDidMount() {
-//		$Node.
+//		$Node.GetFeed().then( r => {
+//				
+//			});
 	}
 
 	
-	render( {extra}, {items} ) {
+	render( {node, user, extra, featured}, {items} ) {
+		let ShowBody = null;
 		
-		if ( extra.length ) {
-			return (
-				<div class="content-base content-post">
-					<h2><strong>404:</strong> "{extra[0]}" not found.</h2>
-				</div>
-			);
+		if ( items ) {
+			ShowBody = [
+				<div>horse</div>
+			];
 		}
-		else if ( items ) {
-			return (
-				<div class="content-base content-post">
-					<div>Group</div>
-				</div>
-			);
-			
+		else {
+			ShowBody = <NavSpinner />;
 		}
+
 		return (
-			<div class="content-base content-post">
-				<NavSpinner />
-			</div>
+			<Common node={node} user={user}>
+				<h2>Group: {node.name}</h2>
+				<CommonBody>
+					{ShowBody}
+				</CommonBody>
+			</Common>
 		);
 	}
 }
