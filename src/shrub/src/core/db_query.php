5645f63004ed84a6014a895959a47a2477104835
Stephen Stair <sgstair@akkit.org>
2017-08-21 21:36:44 -0700

    DB Query fix (used by magic - was not limiting magic query to only the relevant nodes)

--- src/shrub/src/core/db_query.php
+++ src/shrub/src/core/db_query.php
@@ -146,13 +146,18 @@ function dbQuery_MakeOp( $name, $op, $array, &$QUERY, &$ARGS ) {
 				}
 			}
 			
-			if ( is_numeric($array) ) {
+			if ( is_numeric($array[0]) ) {
 				$QUERY[] = '`'.$name.'` '.$op.' ('.implode(',', $array).')';
 				return true;
 			}
-			else if ( is_string($array) ) {
+			else if ( is_string($array[0]) ) {
 				$QUERY[] = '`'.$name.'` '.$op.' ("'.implode('","', $array).'")';
 				return true;
+			}
+			else
+			{
+				global $RESPONSE;
+				json_EmitFatalError_Server("Array of unknown type passed to ".__FUNCTION__, $RESPONSE);
 			}
 		}
 		else {
