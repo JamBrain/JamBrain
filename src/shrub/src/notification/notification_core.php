85965a6ca7b057117474ee30095cae99994d960a
Stephen Stair <sgstair@akkit.org>
2017-09-17 01:55:11 -0700

    Notification code - WIP

--- src/shrub/src/notification/notification_core.php
+++ src/shrub/src/notification/notification_core.php
@@ -23,16 +23,54 @@ function notification_Add( $user, $node, $note, $type ) {
 }
 
 function notification_CountUnread( $user ) {
-
+	$last_read = user_GetLastReadNotificationByNode( $user );
+	$counts = db_QueryFetchSingle(
+		"SELECT count(*)
+		FROM ".SH_TABLE_PREFIX.SH_TABLE_NOTIFICATION." 
+		WHERE user=? AND id > ?;",
+		$user, $last_read
+	);
+	return $counts[0];
 }
 
 function notification_GetUnread( $user, $limit = 20, $offset = 0 ) {
 	$last_read = user_GetLastReadNotificationByNode( $user )
-	
+	return db_QueryFetch(
+		"SELECT *
+		FROM ".SH_TABLE_PREFIX.SH_TABLE_NOTIFICATION."
+		WHERE user=? AND id > ?
+		ORDER BY id DESC
+		LIMIT ?
+		OFFSET ?
+		;",
+		$user, $last_read,
+		$limit,
+		$offset
+	);
 }
 
+function notification_Count( $user ) {
+	$counts = db_QueryFetchSingle(
+		"SELECT count(*)
+		FROM ".SH_TABLE_PREFIX.SH_TABLE_NOTIFICATION." 
+		WHERE user=?;",
+		$user
+	);
+	return $counts[0];
+}
 
 function notification_Get( $user, $limit = 20, $offset = 0 )
 {
-
+	return db_QueryFetch(
+		"SELECT *
+		FROM ".SH_TABLE_PREFIX.SH_TABLE_NOTIFICATION."
+		WHERE user=?
+		ORDER BY id DESC
+		LIMIT ?
+		OFFSET ?
+		;",
+		$user,
+		$limit,
+		$offset
+	);
 }
