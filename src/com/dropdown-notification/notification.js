e9eaf7b19aa8a7271cc469f2fa7345c76d95fa19
Mike Kasprzak <mike@sykhronics.com>
2017-09-26 22:23:05 -0400

    Added newline

--- src/com/dropdown-notification/notification.js
+++ src/com/dropdown-notification/notification.js
@@ -352,5 +352,4 @@ export default class DropdownNotification extends Component {
 			<DropdownCommon className='-notifications' items={Notifications} startExpanded={true} hideSelectedField={true} onmodify={ (id) => this.onModifyFunction(id) } />			
 		);
 	}
-	
 }
