3c98d63233b2a9f8b3b7ae509b016d386ad83068
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-10-08 01:31:20 +0200

    Removed and restructured content.js

--- src/com/view-content/content.js
+++ src/com/view-content/content.js
@@ -10,6 +10,7 @@ import ContentUserFollowers from 'com/content-user/user-followers';
 
 import ContentTimeline					from 'com/content-timeline/timeline';
 import ContentGames						from 'com/content-games/games';
+import GamesFilter from 'com/content-games/filter';
 import ContentEvent						from 'com/content-event/event';
 //import ContentEvents					from 'com/content-events/events';
 import ContentGroup						from 'com/content-group/group';
@@ -30,11 +31,11 @@ import ContentStatsEvent				from 'com/content-stats/stats-event';
 import ContentPalette					from 'com/content-palette/palette';
 
 import NavLink							from 'com/nav-link/link';
-import SVGIcon							from 'com/svg-icon/icon';
+//import SVGIcon							from 'com/svg-icon/icon';
 import Common							from 'com/content-common/common';
 import CommonBody						from 'com/content-common/common-body';
-import CommonNav						from 'com/content-common/common-nav';
-import CommonNavButton					from 'com/content-common/common-nav-button';
+//import CommonNav						from 'com/content-common/common-nav';
+//import CommonNavButton					from 'com/content-common/common-nav-button';
 
 //import HeadMan 							from '../../internal/headman/headman';
 //import marked 							from '../../internal/marked/marked';
@@ -466,61 +467,30 @@ export default class ViewContent extends Component {
 					Methods = [EvalFilter2(DefaultFilter)];
 				}
 
-				let FilterDesc = {
-					'smart': <div><strong>Smart</strong>: This is the modern balancing filter. It balances the list using a combination of votes and the karma given to feedback. You start seeing diminishing returns after 50 ratings, but you can make up for it by leaving quality feedback.</div>,
-					'unbound': <div><strong>Unbound</strong>: This is a variation of the Smart filter that is unbound. For curiousity.</div>,
-					'classic': <div><strong>Classic</strong>: This is the classic balancing filter. It balances the list based on ratings alone. You start seeing diminishing returns after 100 ratings.</div>,
-					'danger': <div><strong>Danger</strong>: This is the rescue filter. Everything with less than 20 ratings sorted top to bottom. Items on the first page are typically 1-2 rating away, so help them out!</div>, //'
-					'zero': <div><strong>Zero</strong>: This filter shows the most neglected games. These are often new users that didn't understand you should rate games. Leaving them some feedback is greatly appreciated.</div>, //'
-					'feedback': <div><strong>Feedback</strong>: This filter lets you find who is working the hardest, leaving quality feedback for others.</div>,
-					'grade': <div><strong>Grade</strong>: This filter lets you find the games that have the most ratings.</div>,
-				};
-
 				if ( node && node.meta && node.meta['theme-mode'] < 8 ) {
-					let Path = this.props.path+'/'+extra[0]+'/';
-
-					ShowFilters = (
-						<Common node={this.props.node} class="filter-item filter-game">
-							<CommonNav>
-								<CommonNavButton href={Path+Filter+'/all'} class={SubFilter == 'all' ? '-selected' : ''}><SVGIcon>gamepad</SVGIcon><div>All</div></CommonNavButton>
-								<CommonNavButton href={Path+Filter+'/jam'} class={SubFilter == 'jam' ? '-selected' : ''}><SVGIcon>trophy</SVGIcon><div>Jam</div></CommonNavButton>
-								<CommonNavButton href={Path+Filter+'/compo'} class={SubFilter == 'compo' ? '-selected' : ''}><SVGIcon>trophy</SVGIcon><div>Compo</div></CommonNavButton>
-								<CommonNavButton href={Path+Filter+'/unfinished'} class={SubFilter == 'unfinished' ? '-selected' : ''}><SVGIcon>trash</SVGIcon><div>Unfinished</div></CommonNavButton>
-							</CommonNav>
-							<CommonNav>
-								<CommonNavButton href={Path+'smart/'+SubFilter} class={Filter == 'smart' ? '-selected' : ''}><SVGIcon>ticket</SVGIcon><div>Smart</div></CommonNavButton>
-								<CommonNavButton href={Path+'classic/'+SubFilter} class={Filter == 'classic' ? '-selected' : ''}><SVGIcon>ticket</SVGIcon><div>Classic</div></CommonNavButton>
-								<CommonNavButton href={Path+'danger/'+SubFilter} class={Filter == 'danger' ? '-selected' : ''}><SVGIcon>help</SVGIcon><div>Danger</div></CommonNavButton>
-								<CommonNavButton href={Path+'zero/'+SubFilter} class={Filter == 'zero' ? '-selected' : ''}><SVGIcon>gift</SVGIcon><div>Zero</div></CommonNavButton>
-								<CommonNavButton href={Path+'feedback/'+SubFilter} class={Filter == 'feedback' ? '-selected' : ''}><SVGIcon>bubbles</SVGIcon><div>Feedback</div></CommonNavButton>
-								<CommonNavButton href={Path+'grade/'+SubFilter} class={Filter == 'grade' ? '-selected' : ''}><SVGIcon>todo</SVGIcon><div>Grade</div></CommonNavButton>
-							</CommonNav>
-							<CommonBody>{FilterDesc[Filter]}</CommonBody>
-						</Common>
-					);
+					ShowFilters = <GamesFilter
+							Filter={Filter}
+							SubFilter={SubFilter}
+							SubSubFilter={SubSubFilter}
+							Path={`${this.props.path}/${extra[0]}/`}
+							node={node}
+							onchangefilter={(filter)=>this.setState({gamesFilter: filter})}
+							showEvent={true}
+							showRatingSort={true}
+							showRatingSortDesc={true}
+						/>;
 				}
 				else {	// results
-					let Path = this.props.path+'/'+extra[0]+'/';
-
-					ShowFilters = (
-						<Common node={this.props.node} class="filter-item filter-game">
-							<CommonNav>
-								<CommonNavButton href={Path+Filter+'/jam'} class={SubFilter == 'jam' ? '-selected' : ''}><SVGIcon>trophy</SVGIcon><div>Jam</div></CommonNavButton>
-								<CommonNavButton href={Path+Filter+'/compo'} class={SubFilter == 'compo' ? '-selected' : ''}><SVGIcon>trophy</SVGIcon><div>Compo</div></CommonNavButton>
-								<CommonNavButton href={Path+Filter+'/all'} class={SubFilter == 'all' ? '-selected' : ''}><SVGIcon>earth</SVGIcon><div>All</div></CommonNavButton>
-							</CommonNav>
-							<CommonNav>
-								<CommonNavButton href={Path+'overall/'+SubFilter} class={'-no-icon '+(Filter == 'overall' ? '-selected' : '')}><div>Overall</div></CommonNavButton>
-								<CommonNavButton href={Path+'fun/'+SubFilter} class={'-no-icon '+(Filter == 'fun' ? '-selected' : '')}><div>Fun</div></CommonNavButton>
-								<CommonNavButton href={Path+'innovation/'+SubFilter} class={'-no-icon '+(Filter == 'innovation' ? '-selected' : '')}><div>Innovation</div></CommonNavButton>
-								<CommonNavButton href={Path+'theme/'+SubFilter} class={'-no-icon '+(Filter == 'theme' ? '-selected' : '')}><div>Theme</div></CommonNavButton>
-								<CommonNavButton href={Path+'graphics/'+SubFilter} class={'-no-icon '+(Filter == 'graphics' ? '-selected' : '')}><div>Graphics</div></CommonNavButton>
-								<CommonNavButton href={Path+'audio/'+SubFilter} class={'-no-icon '+(Filter == 'audio' ? '-selected' : '')}><div>Audio</div></CommonNavButton>
-								<CommonNavButton href={Path+'humor/'+SubFilter} class={'-no-icon '+(Filter == 'humor' ? '-selected' : '')}><div>Humor</div></CommonNavButton>
-								<CommonNavButton href={Path+'mood/'+SubFilter} class={'-no-icon '+(Filter == 'mood' ? '-selected' : '')}><div>Mood</div></CommonNavButton>
-							</CommonNav>
-						</Common>
-					);
+					ShowFilters = <GamesFilter
+							Filter={Filter}
+							SubFilter={SubFilter}
+							SubSubFilter={SubSubFilter}
+							Path={`${this.props.path}/${extra[0]}/`}
+							node={node}
+							onchangefilter={(filter)=>this.setState({gamesFilter: filter})}
+							showEvent={true}
+							showVotingCategory={true}
+						/>;
 				}
 //							<CommonBody>{FilterDesc[Filter]}</CommonBody>
 
@@ -676,49 +646,20 @@ export default class ViewContent extends Component {
 					Methods = [EvalFilter(DefaultFilter)];
 				}
 
-
-				let FilterDesc = {
-					'smart': <div><strong>Smart</strong>: This is the modern balancing filter. It balances the list using a combination of votes and the karma given to feedback. You start seeing diminishing returns after 50 ratings, but you can make up for it by leaving quality feedback.</div>,
-					'unbound': <div><strong>Unbound</strong>: This is a variation of the Smart filter that is unbound. For curiousity.</div>,
-					'classic': <div><strong>Classic</strong>: This is the classic balancing filter. It balances the list based on ratings alone. You start seeing diminishing returns after 100 ratings.</div>,
-					'danger': <div><strong>Danger</strong>: This is the rescue filter. Everything with less than 20 ratings sorted top to bottom. Items on the first page are typically 1-2 rating away, so help them out!</div>, //'
-					'zero': <div><strong>Zero</strong>: This filter shows the most neglected games. These are often new users that didn't understand you should rate games. Leaving them some feedback is greatly appreciated.</div>, //'
-					'feedback': <div><strong>Feedback</strong>: This filter lets you find who is working the hardest, leaving quality feedback for others.</div>,
-					'grade': <div><strong>Grade</strong>: This filter lets you find the games that have the most ratings.</div>,
-				};
-
 				let ShowFilters = null;
 				if ( true ) {
-					let Path = this.props.path+'/games/';
-
-					let WithSubFilter = SubFilter ? '/'+SubFilter : '';
-					let WithSubSubFilter = SubSubFilter && SubSubFilter != 'featured' ? '/'+SubSubFilter : '';
-
-					ShowFilters = (
-						<Common node={this.props.node} class="filter-item filter-game">
-							<CommonNav>
-								<CommonNavButton href={Path+Filter+WithSubFilter+''} class={SubSubFilter == 'featured' ? '-selected' : ''}><SVGIcon>tag</SVGIcon><div>Featured Event</div></CommonNavButton>
-								<CommonNavButton href={Path+Filter+WithSubFilter+'/everything'} class={SubSubFilter == 'everything' ? '-selected' : ''}><SVGIcon>tag</SVGIcon><div>All Events</div></CommonNavButton>
-							</CommonNav>
-							<CommonNav>
-								<CommonNavButton href={Path+Filter+'/all'+WithSubSubFilter} class={SubFilter == 'all' ? '-selected' : ''}><SVGIcon>gamepad</SVGIcon><div>All</div></CommonNavButton>
-								<CommonNavButton href={Path+Filter+'/jam'+WithSubSubFilter} class={SubFilter == 'jam' ? '-selected' : ''}><SVGIcon>trophy</SVGIcon><div>Jam</div></CommonNavButton>
-								<CommonNavButton href={Path+Filter+'/compo'+WithSubSubFilter} class={SubFilter == 'compo' ? '-selected' : ''}><SVGIcon>trophy</SVGIcon><div>Compo</div></CommonNavButton>
-								<CommonNavButton href={Path+Filter+'/unfinished'+WithSubSubFilter} class={SubFilter == 'unfinished' ? '-selected' : ''}><SVGIcon>trash</SVGIcon><div>Unfinished</div></CommonNavButton>
-							</CommonNav>
-							<CommonNav>
-								<CommonNavButton href={Path+'smart'+WithSubFilter+WithSubSubFilter} class={Filter == 'smart' ? '-selected' : ''}><SVGIcon>ticket</SVGIcon><div>Smart</div></CommonNavButton>
-								<CommonNavButton href={Path+'classic'+WithSubFilter+WithSubSubFilter} class={Filter == 'classic' ? '-selected' : ''}><SVGIcon>ticket</SVGIcon><div>Classic</div></CommonNavButton>
-								<CommonNavButton href={Path+'danger'+WithSubFilter+WithSubSubFilter} class={Filter == 'danger' ? '-selected' : ''}><SVGIcon>help</SVGIcon><div>Danger</div></CommonNavButton>
-								<CommonNavButton href={Path+'zero'+WithSubFilter+WithSubSubFilter} class={Filter == 'zero' ? '-selected' : ''}><SVGIcon>gift</SVGIcon><div>Zero</div></CommonNavButton>
-								<CommonNavButton href={Path+'feedback'+WithSubFilter+WithSubSubFilter} class={Filter == 'feedback' ? '-selected' : ''}><SVGIcon>bubbles</SVGIcon><div>Feedback</div></CommonNavButton>
-								<CommonNavButton href={Path+'grade'+WithSubFilter+WithSubSubFilter} class={Filter == 'grade' ? '-selected' : ''}><SVGIcon>todo</SVGIcon><div>Grade</div></CommonNavButton>
-							</CommonNav>
-							<CommonBody>{FilterDesc[Filter]}</CommonBody>
-						</Common>
-					);
+					ShowFilters = <GamesFilter
+						Filter={Filter}
+						SubFilter={SubFilter}
+						SubSubFilter={SubSubFilter}
+						Path={`${this.props.path}/games/`}
+						node={node}
+						onchangefilter={(filter)=>this.setState({gamesFilter: filter})}
+						showFeatured={true}
+						showEvent={true}
+						showVotingCategory={true}
+					/>;
 				}
-
 
 				let NodeArg = node;
 				if ( SubSubFilter == 'featured' ) {
