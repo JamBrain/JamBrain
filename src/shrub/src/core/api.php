4023e0f5e786f59d7bc1c2fc38c3d218dbd7ebd6
Stephen Stair <sgstair@akkit.org>
2017-10-25 01:26:34 -0700

    Add some documentation.

--- src/shrub/src/core/api.php
+++ src/shrub/src/core/api.php
@@ -8,13 +8,13 @@ require_once __DIR__."/core.php";
 /// @addtogroup API
 /// @{
 
-const API_GET = 		0x0001;
-const API_POST = 		0x0002;
-const API_AUTH = 		0x0004;
+const API_GET = 		0x0001; // Require API call to use GET method
+const API_POST = 		0x0002; // Require API call to use POST method
+const API_AUTH = 		0x0004; // Require calling user to be authenticated.
 
 
-const API_CHARGE_0 = 	0;
-const API_CHARGE_1 = 	1;
+const API_CHARGE_0 = 	0;	// Do not interact with the rate limit system before dispatching this API call
+const API_CHARGE_1 = 	1;	// Charge a single unit to the rate limit system for this call, and bail early if there is not enough credit.
 
 /// Handles an API request by dispatching it to the appropriate handler in the passed-in table.
 /// The table contains the API name, flags declaring usage requirements, the rate limiting charge, and a function that will handle the API request.
