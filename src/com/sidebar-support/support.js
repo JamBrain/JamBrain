915eaa0083e9ac7e817fd83c4b818e30d4cd0560
Mike Kasprzak <mikekasprzak@gmail.com>
2017-11-09 12:48:19 -0500

    shortened 'support us' section, since @help isn't working yet

--- src/com/sidebar-support/support.js
+++ src/com/sidebar-support/support.js
@@ -10,10 +10,10 @@ export default class SidebarSupport extends Component {
 		return (
 			<div class="sidebar-base sidebar-shortlist sidebar-upcoming">
 				<div class="-title _font2"><SVGIcon baseline>help</SVGIcon> <span class="-text">Support Us</span></div>
-				<div class="-summary -gap"><strong>{window.location.host}</strong> is a free, open source service run by Mike Kasprzak (@pov). If you like it, please support Mike on <NavLink href="https://patreon.com/mikekasprzak"><SVGIcon baseline small gap>patreon</SVGIcon>Patreon</NavLink> or <NavLink href="https://www.paypal.me/mikekasprzak/0usd"><SVGIcon baseline small gap>paypal</SVGIcon>PayPal</NavLink>. Other inquiries, contact <NavLink href="/contact/"><SVGIcon baseline small gap>mail</SVGIcon>Mike</NavLink>.</div>
+				<div class="-summary -gap"><strong>{window.location.host}</strong> is a free, open source service run by Mike Kasprzak (<NavLink href="/users/pov">@pov</NavLink>). If you like it, please support Mike on <NavLink href="https://patreon.com/mikekasprzak"><SVGIcon baseline small gap>patreon</SVGIcon>Patreon</NavLink> or <NavLink href="https://www.paypal.me/mikekasprzak/0usd"><SVGIcon baseline small gap>paypal</SVGIcon>PayPal</NavLink>. Other inquiries, contact <NavLink href="/contact/"><SVGIcon baseline small gap>mail</SVGIcon>Mike</NavLink>.</div>
 				<div class="-summary -gap">If you want to help with the code, the Source Code is available on <NavLink href="https://github.com/ludumdare/ludumdare"><SVGIcon baseline small gap>github</SVGIcon>GitHub</NavLink>.</div>
-				<div class="-summary -gap">If you want to help out, follow the @help group and answer questions. Mike may run the site, but it's powered by you!</div>
 			</div>
 		);
+//				<div class="-summary -gap">If you want to help out, follow the @help group and answer questions. Mike may run the site, but it's powered by you!</div>
 	}
 }
