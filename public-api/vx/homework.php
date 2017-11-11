cbfc645b7faefe0f87865a02fc8735dbdba78ebf
Mike Kasprzak <mikekasprzak@gmail.com>
2017-11-10 23:11:49 -0500

    testing grouped homework. got rid of extras

--- public-api/vx/homework.php
+++ public-api/vx/homework.php
@@ -20,23 +20,26 @@ api_Exec([
 	$user_id = userAuth_GetID();
 	
 	$mode = intval($event['meta']['theme-mode']);
+	$making_what = substr(strrchr($event['meta']['can-create'], '/'), 0);
 
 	//$RESPONSE['ev'] = $event;
 	
-	// Homework are tasks that we can detect when you're done
 	$RESPONSE['homework'] = [];
-	
-	// Extras are tasks that a user decides when they are done, or not to do them at all
-	$RESPONSE['extras'] = [];
 
-
+	// task: brief description of task
+	// detail: detailed description of task
+	// link: a URL we should recommend for doing the task
+	// complete: if task is complete
+	// user: if task is something the user decides they've finished
+	// children: tasks that are grouped under this task
+	// unlock: tasks to unlock once this task is complete. Not grouped as children
 
 	// Pre-Event Tasks that you can do any time during the pre-event, that should come before per-round tasks
 	if ( ($mode >= 1) && ($mode <= 5) ) {
 		$RESPONSE['homework'][] = [
 			'task' => "Join event",
-			'complete' => true,
 			'link' => $event['path'],
+			'complete' => true,
 		];
 	}
 	
@@ -49,30 +52,40 @@ api_Exec([
 
 		$RESPONSE['homework'][] = [
 			'task' => "Suggest ".$ideas_required." themes",
-			'complete' => ($idea_count >= $ideas_required),
 			'detail' => $idea_count." of ".$ideas_required,
 			'link' => $event['path'].'/theme',
+			'complete' => ($idea_count >= $ideas_required),
 		];
 	}
 
 	// Pre-Event Tasks that you can do any time during the pre-event, that should come after per-round tasks
 	if ( ($mode >= 1) && ($mode <= 5) ) {
-		$RESPONSE['extras'][] = [
+		// TODO: group these under a parent task "submit a warmup game"
+		$RESPONSE['homework'][] = [
 			'task' => "Decide what tools you are going to use",
+			'user' => true,		// the user decides when they are done
 		];
-		$RESPONSE['extras'][] = [
-			'task' => "Make something with your tools",
-			'detail' => "A quick warmup just to be sure your tools work as expected.",
-		];
-		$RESPONSE['extras'][] = [
-			'task' => "Package up a download that others can try",
-			'detail' => "We don't host downloads, so it's wise to figure out where you're going to do that.",
-			'link' => '/events/ludum-dare/hosting-guide',
-		];
-		$RESPONSE['extras'][] = [
-			'task' => "Submit it as a warmup submission",
-			'detail' => "Submitting a warmup is not required. If this is your first event, it's a good idea to familiarize yourself with how submissions work.",
-			'link' => $event['path'].'/warmup',
+		$RESPONSE['homework'][] = [
+			'task' => "Make a warmup ".$making_what,
+			'children' => [
+				[
+					'task' => "Make something with your tools",
+					'detail' => "A quick warmup just to be sure your tools work as expected.",
+					'user' => true,		// the user decides when they are done
+				],
+				[
+					'task' => "Package up a download that others can try",
+					'detail' => "We don't host downloads, so it's wise to figure out where you're going to do that.",
+					'link' => '/events/ludum-dare/hosting-guide',
+					'user' => true,		// the user decides when they are done
+				],
+				[
+					'task' => "Submit your warmup ".$making_what,
+					'detail' => "Submitting a warmup is not required. If this is your first event, it's a good idea to familiarize yourself with how submissions work.",
+					'link' => $event['path'].'/warmup',
+					'user' => true,		// the user decides when they are done
+				],
+			],
 		];
 	}
 
