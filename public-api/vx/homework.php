ffd3958f73853449b8776a38b8ed032092d95f0f
Mike Kasprzak <mikekasprzak@gmail.com>
2017-11-10 22:43:48 -0500

    added proxy join homework, and a list of optional extras

--- public-api/vx/homework.php
+++ public-api/vx/homework.php
@@ -8,7 +8,7 @@ require_once __DIR__."/".SHRUB_PATH."theme/theme.php";
 
 function GetActiveEvent() {
 	$root = nodeCache_GetById(1);
-	if ( $root && $root['meta'] && $root['meta']['featured'] ) {
+	if ( $root && isset($root['meta']) && isset($root['meta']['featured']) ) {
 		return nodeCache_GetById(intval($root['meta']['featured']));
 	}
 	return null;
@@ -23,8 +23,24 @@ api_Exec([
 
 	//$RESPONSE['ev'] = $event;
 	
+	// Homework are tasks that we can detect when you're done
 	$RESPONSE['homework'] = [];
 	
+	// Extras are tasks that a user decides when they are done, or not to do them at all
+	$RESPONSE['extras'] = [];
+
+
+
+	// Pre-Event Tasks that you can do any time during the pre-event, that should come before per-round tasks
+	if ( ($mode >= 1) && ($mode <= 5) ) {
+		$RESPONSE['homework'][] = [
+			'task' => "Join event",
+			'complete' => true,
+			'link' => $event['path'],
+		];
+	}
+	
+	// Pre-Event Tasks that are per-round
 	if ( $mode == 1 ) {
 		$ideas_required = isset($event['meta']['theme-idea-limit']) ? intval($event['meta']['theme-idea-limit']) : 3;
 
@@ -32,12 +48,36 @@ api_Exec([
 		$idea_count = count($ideas);
 
 		$RESPONSE['homework'][] = [
-			'task' => "Suggest ".$ideas_required." themes", 
+			'task' => "Suggest ".$ideas_required." themes",
 			'complete' => ($idea_count >= $ideas_required),
 			'detail' => $idea_count." of ".$ideas_required,
-			'link' => $event['path'].'/theme'
+			'link' => $event['path'].'/theme',
 		];
 	}
+
+	// Pre-Event Tasks that you can do any time during the pre-event, that should come after per-round tasks
+	if ( ($mode >= 1) && ($mode <= 5) ) {
+		$RESPONSE['extras'][] = [
+			'task' => "Decide what tools you are going to use",
+		];
+		$RESPONSE['extras'][] = [
+			'task' => "Make something with your tools",
+			'detail' => "A quick warmup just to be sure your tools work as expected.",
+		];
+		$RESPONSE['extras'][] = [
+			'task' => "Package up a download that others can try",
+			'detail' => "We don't host downloads, so it's wise to figure out where you're going to do that.",
+			'link' => '/events/ludum-dare/hosting-guide',
+		];
+		$RESPONSE['extras'][] = [
+			'task' => "Submit it as a warmup submission",
+			'detail' => "Submitting a warmup is not required. If this is your first event, it's a good idea to familiarize yourself with how submissions work.",
+			'link' => $event['path'].'/warmup',
+		];
+	}
+
+	// Post-Event Tasks
+
 }],
 ]);
 
