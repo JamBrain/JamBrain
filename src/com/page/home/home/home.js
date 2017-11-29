68d35e6d6400942f2734332955b611bb873cfd5b
Mike Kasprzak <mikekasprzak@gmail.com>
2017-11-29 16:25:59 -0500

    oops, commited a bad commit

--- src/com/page/home/home/home.js
+++ src/com/page/home/home/home.js
@@ -1,17 +1,17 @@
 import {h, Component}					from 'preact/preact';
 
 import ContentTimeline					from 'com/content-timeline/timeline';
-import ContentHeadliner					from 'com/content-headliner/headliner';
+//import ContentHeadliner					from 'com/content-headliner/headliner';
 
 export default class HomeHome extends Component {
-    render( props, state ) {
-        let {node, user, path, extra} = props;
+	render( props, state ) {
+		let {node, user, path, extra} = props;
 
-        return (
-            <ContentTimeline types={['post']} methods={['all']} node={node} user={user} path={path} extra={extra}>
-                <ContentTimeline types={['post']} subtypes={['news']} methods={['all']} minimized nomore noemptymessage limit={1} node={node} user={user} path={path} extra={extra} />
-                <ContentHeadliner node={[node,node]} />
-            </ContentTimeline>
-        );
-    }
+		return (
+			<ContentTimeline types={['post']} methods={['all']} node={node} user={user} path={path} extra={extra}>
+				<ContentTimeline types={['post']} subtypes={['news']} methods={['all']} minimized nomore noemptymessage limit={1} node={node} user={user} path={path} extra={extra} />
+			</ContentTimeline>
+		);
+	}
+//				<ContentHeadliner node={[node,node]} />
 }
