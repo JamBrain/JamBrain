f2cf8910b01ba5e36768c3b56bf18ec103ca6910
Mike Kasprzak <mikekasprzak@gmail.com>
2017-12-02 15:11:24 -0500

    improved link editing allows custom names

--- src/com/input-text/text.js
+++ src/com/input-text/text.js
@@ -1,5 +1,5 @@
-import { h, Component } 				from 'preact/preact';
-import { shallowDiff }	 				from 'shallow-compare/index';
+import {h, Component}	 				from 'preact/preact';
+import {shallowDiff}	 				from 'shallow-compare/index';
 
 import NavLink							from 'com/nav-link/link';
 import SVGIcon							from 'com/svg-icon/icon';
@@ -21,15 +21,18 @@ export default class InputText extends Component {
 	}
 
 	render( props ) {
-		//props = Object.assign({}, props);
 		const {maxlength} = props;
-		var ShowLimit = null;
+
+		let ShowLimit = null;
 		if ( maxlength )
 			ShowLimit = <div class="-right"><span class="-chars">{props.value.length}</span>/<span class="-limit">{maxlength}</span></div>;
-		console.log('[Rend input]', this.textinput);
+
 		return (
-			<div class="input-text">
-				<input {...props}
+			<div class={cN('input-text', props.class)}>
+				<input
+					value={props.value}
+					placeholder={props.placeholder}
+					maxlength={props.maxlength}
 					type="text"
 					oninput={this.onModify}
 					ref={(input) => { this.textinput = input; }}
