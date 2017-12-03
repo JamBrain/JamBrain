3f669ca13771994ee199b3abf84e5a6f1eb43b89
Mike Kasprzak <mikekasprzak@gmail.com>
2017-12-03 13:42:24 -0500

    fixed some tag list bugs

--- public-api/vx/tag.php
+++ public-api/vx/tag.php
@@ -16,10 +16,12 @@ switch ( $action ) {
 		json_ValidateHTTPMethod('GET');
 
 		$filter = json_ArgShift();
-		if ( $filter == 'all' ) {
-			$filter = null;
-		}
-		else if ( $filter ) {
+		// Disabled this because the cached name conflicts with "no filter"
+//		if ( $filter == 'all' ) {
+//			$filter = null;
+//		}
+//		else
+		if ( $filter ) {
 			$filter = coreSlugify_PathName($filter);
 		}
 		else {
@@ -28,16 +30,19 @@ switch ( $action ) {
 
 		$key = "SH_TAG_".($filter ? $filter : "");
 		$ttl = 5*60;	// 5 minutes
-		
+
 		if ( cache_Exists($key) ) {
 			$RESPONSE['tag'] = cache_Fetch($key);
 			$RESPONSE['cached'] = true;
 		}
 		else {
 			$RESPONSE['tag'] = node_GetSimpleByType('tag', $filter);
-			cache_Store($key, $RESPONSE['tag'], $ttl);
+
+			// only store it if there are actually results
+			if ( count($RESPONSE['tag']) )
+				cache_Store($key, $RESPONSE['tag'], $ttl);
 		}
-	
+
 		break; // case 'get': //tag/get/:filter
 	default:
 		json_EmitFatalError_Forbidden(null, $RESPONSE);
