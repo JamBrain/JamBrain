040e0fad61ad8db0297fe4ffe6c8396450abdf57
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-12-23 14:40:29 +0100

    Adding create news buttons for those with rights

--- src/com/page/home/news/news.js
+++ src/com/page/home/news/news.js
@@ -1,12 +1,33 @@
 import {h, Component}					from 'preact/preact';
 import ContentTimeline					from 'com/content-timeline/timeline';
+import UIButton							from 'com/ui/button/button';
 
 export default class PageHomeNews extends Component {
+
+	userCanCreateNews( user ) {
+		return user && user.meta && user.meta.group && user.meta.group.filter(g => g == 'canNews').length > 0;
+	}
+
+	userCanCreateEventNews( user ) {
+		return user && user.meta && user.meta.group && user.meta.group.filter(g => g == 'canNews' || g == 'activeEventHost').length > 0;
+	}
+
     render( props ) {
         let {node, user, path, extra} = props;
-
+		let ShowCreateNews = null;
+		let ShowCreteEventNews = null;
+		if (this.userCanCreateNews(user)) {
+			ShowCreateNews = <UIButton href="/add/0/post/news">Create general news post</UIButton>;
+		}
+		if (this.userCanCreateEventNews(user)) {
+			ShowCreteEventNews = <UIButton href="/add/0/post/news">Create event news post</UIButton>;
+		}
         return (
-            <ContentTimeline class="content-timeline-news" types={['post']} subtypes={['news']} methods={['all']} node={node} user={user} path={path} extra={extra} />
+			<div>
+				{ShowCreateNews}
+				{ShowCreteEventNews}
+				<ContentTimeline class="content-timeline-news" types={['post']} subtypes={['news']} methods={['all']} node={node} user={user} path={path} extra={extra} />
+			</div>
         );
     }
 }
