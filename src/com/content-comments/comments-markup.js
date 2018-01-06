083799631d9fb6c3a8b4288e9aa78c3c7bb4ebf3
Mike Kasprzak <mikekasprzak@gmail.com>
2018-01-06 13:41:40 -0500

    cleaned up coding style

--- src/com/content-comments/comments-markup.js
+++ src/com/content-comments/comments-markup.js
@@ -1,11 +1,11 @@
-import {h, Component} 				from 'preact/preact';
+import {h, Component} 					from 'preact/preact';
 import {shallowDiff}	 				from 'shallow-compare/index';
 
 import NavLink							from 'com/nav-link/link';
 import SVGIcon							from 'com/svg-icon/icon';
 import InputTextArea					from 'com/input-textarea/input-textarea';
 
-import marked 								from '../../internal/marked/marked';
+import marked 							from 'internal/marked/marked';
 
 export default class ContentCommentsMarkup extends Component {
 	constructor( props ) {
@@ -24,7 +24,7 @@ export default class ContentCommentsMarkup extends Component {
 
 		var Text = props.children.join('');
 
-		if (props.editing) {
+		if ( props.editing ) {
 			//var Height = this.textarea ? this.textarea.scrollHeight : 0;
 
 			var Limit = props.limit ? props.limit : 4096;
@@ -61,15 +61,15 @@ export default class ContentCommentsMarkup extends Component {
 			var mrkd = new marked();
 
 			var markedOptions = {
-				highlight: function(code, lang) {
+				'highlight': function(code, lang) {
 					var language = Prism.languages.clike;
 					if (Prism.languages[lang])
 						language = Prism.languages[lang];
 					return Prism.highlight(code, language);
 				},
-				sanitize: true, // disable HTML
-				smartypants: true, // enable automatic fancy quotes, ellipses, dashes
-				langPrefix: 'language-'
+				'sanitize': true, // disable HTML
+				'smartypants': true, // enable automatic fancy quotes, ellipses, dashes
+				'langPrefix': 'language-'
 			};
 
 			markdown = mrkd.parse(Text, markedOptions);
