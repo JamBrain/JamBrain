d4c25a84034b1fb2da0eae5f75d9a184928d21a2
Jono Kemball <jonathankemball@gmail.com>
2018-01-11 12:23:35 +1300

    Itch + mobile height fix

--- public-api/vx/embed.php
+++ public-api/vx/embed.php
@@ -49,6 +49,10 @@
 	           background: transparent;
 	           top: 0;
 	       }
+	       iframe.itch {
+		       max-width: 552px;
+		       width: 100%;
+	       }
 
 	</style>
 
@@ -70,6 +74,57 @@
 	    document.getElementsByTagName('head')[0].appendChild(script);
 	  }
 	};
+
+	/* Taken from https://developer.mozilla.org/en-US/docs/Web/Events/resize */
+	var optimizedResize = (function() {
+
+	    var callbacks = [],
+	        running = false;
+
+	    // fired on resize event
+	    function resize() {
+
+	        if (!running) {
+	            running = true;
+
+	            if (window.requestAnimationFrame) {
+	                window.requestAnimationFrame(runCallbacks);
+	            } else {
+	                setTimeout(runCallbacks, 66);
+	            }
+	        }
+
+	    }
+
+	    // run the actual callbacks
+	    function runCallbacks() {
+
+	        callbacks.forEach(function(callback) {
+	            callback();
+	        });
+
+	        running = false;
+	    }
+
+	    // adds callback to loop
+	    function addCallback(callback) {
+
+	        if (callback) {
+	            callbacks.push(callback);
+	        }
+
+	    }
+
+	    return {
+	        // public method to add additional callback
+	        add: function(callback) {
+	            if (!callbacks.length) {
+	                window.addEventListener('resize', resize);
+	            }
+	            addCallback(callback);
+	        }
+	    }
+	}());
 	</script>
 
 	<script type="text/javascript">
@@ -104,6 +159,8 @@
 				messageClient();
 			}
 		});	
+
+		optimizedResize.add(messageClient);
 
 		function handleEmbed(embedType, regexJson) {
 			var target = document.getElementById('container');
