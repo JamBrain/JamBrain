559b3ea517673a526794693d9219a0d573e8fd47
Mike Kasprzak <mikekasprzak@gmail.com>
2018-01-24 23:12:28 -0500

    Cleaned up coding style

--- src/com/visualization/barchart/bar.js
+++ src/com/visualization/barchart/bar.js
@@ -3,31 +3,29 @@ import {h, Component} 				from 'preact/preact';
 const BAR_WIDTH_GAP_RATIO = 0.8;
 
 export default class Bar extends Component {
-
 	constructor( props ) {
 		super(props);
-
-    }
-
-    render( props ) {
-
+	}
+	
+	// NOTE: This emits SVG, not HTML
+	render( props ) {
 		let {valuePos, zero} = props;
 		const {width, color, left} = props;
-        // it's valid for valuePos/width to be zero so we have to check it against undefined.
-        if ( !((valuePos != null) && (width != null) && (left != null) && color )) {
-            console.warn('Bar was created with invalid props', this.props);
-            return;
-        }
+		// it's valid for valuePos/width to be zero so we have to check it against undefined.
+		if ( !((valuePos != null) && (width != null) && (left != null) && color) ) {
+			console.warn("Bar was created with invalid props", this.props);
+			return;
+		}
 
 		if ( valuePos < zero ) {
 			let tmp = zero;
 			zero = valuePos;
 			valuePos = tmp;
 		}
-        let segmentclass = cN("-bar", "vis_fill_color_"+color, this.props.class);
+		let segmentclass = cN("-bar", "vis_fill_color_"+color, this.props.class);
 
-        return (
-            <rect class={segmentclass} x={left} y={zero} width={width * BAR_WIDTH_GAP_RATIO} height={valuePos - zero}/>
-        );
-    }
+		return (
+			<rect class={segmentclass} x={left} y={zero} width={width * BAR_WIDTH_GAP_RATIO} height={valuePos - zero}/>
+		);
+	}
 }
