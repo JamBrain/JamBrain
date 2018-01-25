e2556cbd70cda76b88ece4ae0f723a84084eace1
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-01-25 08:45:50 +0100

    Fixed comments

--- src/com/content-event/event-idea.js
+++ src/com/content-event/event-idea.js
@@ -6,15 +6,15 @@ import $ThemeIdea						from '../../shrub/js/theme/theme_idea';
 
 
 const MAX_IDEAS = 3;
-const canHaveMoreIdeas = (ideas, submitting) => Object.keys(ideas).length + (submitting ? 1 : 0) < MAX_IDEAS;
-const hasSubmittedIdeas = (ideas) => Object.keys(ideas).length > 0;
+const canHaveMoreIdeas = (ideas, submitting) => ((Object.keys(ideas).length + (submitting ? 1 : 0)) < MAX_IDEAS);
+const hasSubmittedIdeas = (ideas) => (Object.keys(ideas).length > 0);
 
 export default class ContentEventIdea extends Component {
 	constructor( props ) {
 		super(props);
 
 		this.state = {
-			'idea': '',
+			'idea': "",
 			'ideas': null,
 			'enableSubmit': false,
 		};
@@ -38,25 +38,25 @@ export default class ContentEventIdea extends Component {
 			}
 		})
 		.catch(err => {
-			this.setState({'error': 'Error fetching your previous suggestions. Make sure you are logged in.'});
+			this.setState({'error': "Error fetching your previous suggestions. Make sure you are logged in."});
 		});
 	}
 
 	textChange( e, isSubmit ) {
 		let idea = e.target.value;
-		if (isSubmit) {
+		if ( isSubmit ) {
 			idea = idea.trim();
 		}
 		this.setState({'idea': idea, 'error': idea.length > 64 ? 'Suggestion is too long.' : null});
 	}
 
 	onKeyDown( e ) {
-		if (!e) {
+		if ( !e ) {
 			e = window.event;
 		}
 		const isSubmit = e.keyCode === 13;
 		this.textChange(e, isSubmit);
-		if (isSubmit) {
+		if ( isSubmit ) {
 			/*e.preventDefault();*/
 			this.submitIdeaForm();
 		}
@@ -84,7 +84,7 @@ export default class ContentEventIdea extends Component {
 
 	checkDuplicateIdea(idea) {
 		const {processingIdea, ideas} = this.state;
-		if (processingIdea && idea == processingIdea) {
+		if ( processingIdea && (idea == processingIdea) ) {
 			return true;
 		}
 		else {
@@ -95,24 +95,24 @@ export default class ContentEventIdea extends Component {
 	submitIdeaForm( e ) {
 		let idea = this.state.idea.trim();
 		//console.log('submit:', idea);
-		if (this.checkDuplicateIdea(idea)) {
+		if ( this.checkDuplicateIdea(idea) ) {
 			this.setState({
-				'error': 'Duplicated suggestion! There\'s no point in supplying the same suggestion more than once.'
+				'error': "Duplicated suggestion! There\'s no point in supplying the same suggestion more than once."
 			});
 		}
-		else if ( idea.length > 0 && idea.length <= 64 ) {
+		else if ( (idea.length > 0) && (idea.length <= 64) ) {
 			this.setState({'enableSubmit': canHaveMoreIdeas(this.state.ideas, true), 'processingIdea': idea, 'error': null});
 			$ThemeIdea.Add(this.props.node.id, idea)
 			.then(r => {
 				//console.log('r', r);
-				this.setState({'ideas': r.ideas, 'idea': r.status === 201 ? '' : idea, 'enableSubmit': canHaveMoreIdeas(r.ideas), 'processingIdea': null});
+				this.setState({'ideas': r.ideas, 'idea': (r.status === 201) ? '' : idea, 'enableSubmit': canHaveMoreIdeas(r.ideas), 'processingIdea': null});
 			})
 			.catch(err => {
-				this.setState({'error': 'Error processing the request. Make sure you are still logged in.', 'processingIdea': null});
+				this.setState({'error': "Error processing the request. Make sure you are still logged in.", 'processingIdea': null});
 			});
 		}
 		else {
-			this.setState({'error': 'Suggestion is too ' + (idea.length == 0 ? 'short.' : 'long.')});
+			this.setState({'error': "Suggestion is too " + (idea.length == 0 ? "short." : "long.")});
 		}
 	}
 
@@ -122,7 +122,7 @@ export default class ContentEventIdea extends Component {
 		return (
 			<div class="-item">
 				<div class="-x" onclick={this.removeIdea.bind(this, id)}><SVGIcon>cross</SVGIcon></div>
-				<SVGIcon>ticket</SVGIcon>
+				<SVGIcon>lightbuld</SVGIcon>
 				<div class="-text" title={idea}>{idea}</div>
 			</div>
 		);
@@ -131,12 +131,14 @@ export default class ContentEventIdea extends Component {
 		return Object.keys(this.state.ideas).map(this.renderIdea);
 	}
 
-	render( {node, user/*, path, extra*/}, {idea, ideas, error, enableSubmit} ) {
+	render( props, state ) {
+		const {node, user} = props;
+		const {idea, ideas, error, enableSubmit} = state;
 		if ( node.slug && ideas ) {
 			if ( user && user['id'] ) {
 				const ShowError = error ? <div class="content-base content-post idea-error">{error}</div> : null;
 				let ShowSubmit = null;
-				if (enableSubmit) {
+				if ( enableSubmit ) {
 					ShowSubmit = (
 						<div class="idea-form">
 							<input type="text"
@@ -150,7 +152,7 @@ export default class ContentEventIdea extends Component {
 					);
 				}
 				let ShowMySuggestions = null;
-				if (hasSubmittedIdeas(ideas)) {
+				if ( hasSubmittedIdeas(ideas) ) {
 					ShowMySuggestions = (
 						<div class="idea-mylist">
 							{ this.renderIdeas() }
@@ -159,10 +161,10 @@ export default class ContentEventIdea extends Component {
 				}
 				let ShowRemaining = null;
 				const remaining = Math.max(0, MAX_IDEAS - Object.keys(ideas).length);
-				if (remaining > 1) {
+				if ( remaining > 1 ) {
 					ShowRemaining = <div class="foot-note small">You have <strong>{remaining}</strong> suggestions left</div>;
 				}
-				else if (remaining == 1) {
+				else if ( remaining == 1 ) {
 					ShowRemaining = <div class="foot-note small">You have <strong>1</strong> suggestion left</div>;
 				}
 				else {
