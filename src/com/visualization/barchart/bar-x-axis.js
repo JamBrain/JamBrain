97759c5519175e8a221a84948ee7a092f39f15db
Mike Kasprzak <mikekasprzak@gmail.com>
2018-01-24 23:08:24 -0500

    svg documentation question

--- src/com/visualization/barchart/bar-x-axis.js
+++ src/com/visualization/barchart/bar-x-axis.js
@@ -6,14 +6,15 @@ import {h, Component} 				from 'preact/preact';
 // Component returns a group to support extension with ticks and labels
 
 export default class XAxis extends Component {
+	// NOTE: This emits SVG, not HTML
 	render( props ) {
 		const {yZeroPos, padLeft, padRight} = props;
 		
-		// NOTE: This emits SVG, not HTML
+		// QUESTION: does this need to be inside <g>? Can this just be <line>? Please document if <g> is needed.
 		return (
 			<g>
 				<line class={cN('-chart-axis', 'x-axis', this.props.class)} x1={padLeft} y1={yZeroPos} x2={100 - padRight} y2={yZeroPos} />
 			</g>
-        );
+		);
 	}
 }
