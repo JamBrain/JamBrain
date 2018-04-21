66201cf4ebc2c1d62909d7e7b8c7ef72fdd4a9aa
Mike Kasprzak <mikekasprzak@gmail.com>
2018-04-21 15:27:26 -0400

    moved news

--- src/com/page/home/home.js
+++ src/com/page/home/home.js
@@ -11,7 +11,7 @@ import PageNavRoot						from '../nav/root';
 import PageHomeHome						from './home/home';
 import PageHomeMy						from './my/my';
 import PageHomeFeed						from './feed/feed';
-import PageHomeNews						from './news/news';
+import PageHomeFeedNews					from './feed/news';
 import PageHomeExplore					from './explore/explore';
 import PageHomeGames					from './games/games';
 import PageHomeTools					from './tools/tools';
@@ -20,19 +20,17 @@ import PageHomeSearch					from './search/search';
 
 export default class PageHome extends Component {
 	render( props ) {
-		let {node, user, extra} = props;
-
 		return (
 			<ContentList class="page-home">
 				<PageNavRoot {...props} />
-				<Router node={node} props={props}>
+				<Router {...props}>
 					<Route static path="/home" default={true} component={PageHomeHome} />
 					<Route static path="/my" component={PageHomeMy} />
 					<Route static path="/my/settings" component={PageHomeMy} />
-					<Route static path="/news" component={PageHomeNews} />
+					<Route static path="/news" component={PageHomeFeedNews} />
 					<Route static path="/feed" component={PageHomeFeed} />
-					<Route static path="/feed/news" component={PageHomeNews} />
-					<Route static path="/feed/hot" component={PageHomeNews} />
+					<Route static path="/feed/news" component={PageHomeFeedNews} />
+					<Route static path="/feed/hot" component={PageHomeFeedNews} />
 					<Route static path="/games/:filter?/:subfilter?/:target?" component={PageHomeGames} />
 					<Route static path="/explore" component={PageHomeExplore} />
 					<Route static path="/tools" component={PageHomeTools} />
