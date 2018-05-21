718feb03cb9df0cef1c7263e85959ff397e19dc0
Mike Kasprzak <mikekasprzak@gmail.com>
2018-05-21 11:18:11 -0400

    Added API2 with complete REST support

--- public-api/vx/feed.php
+++ public-api/vx/feed.php
@@ -2,11 +2,19 @@
 require_once __DIR__."/../config.php";
 
 include_once __DIR__."/".CONFIG_PATH."config.php";
-require_once __DIR__."/".SHRUB_PATH."api.php";
+require_once __DIR__."/".SHRUB_PATH."api2.php";
 require_once __DIR__."/".SHRUB_PATH."node/node.php";
 
 api_Exec([
-["feed", API_GET, API_CHARGE_1, function(&$RESPONSE) {
+["feed/me", API_GET, function(&$RESPONSE, $HEAD_REQUEST) {
+
+	$RESPONSE['me'] = true;
+}],
+["feed/me", API_POST, function(&$RESPONSE) {
+
+	$RESPONSE['postme'] = true;
+}],
+["feed", API_GET, function(&$RESPONSE, $HEAD_REQUEST) {
 
 	$RESPONSE['test'] = true;
 }],
