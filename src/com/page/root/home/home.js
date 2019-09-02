f976179151bbd0506e01928dbb0ceb800ed30d45
Mike Kasprzak <mike@sykhronics.com>
2019-09-02 14:53:41 -0400

    when not logged in, or not signed up, display event info

--- src/com/page/root/home/home.js
+++ src/com/page/root/home/home.js
@@ -1,5 +1,6 @@
 import {h, Component}					from 'preact/preact';
 
+import ContentEvent						from "com/content-event/event";
 import ContentList						from 'com/content-list/list';
 import ContentTimeline					from 'com/content-timeline/timeline';
 import ContentHeadlinerFeed				from 'com/content-headliner/headliner-feed';
@@ -16,8 +17,14 @@ export default class PageRootHome extends Component {
 			}
 		}
 
+		let ShowEvent = null;
+		if ( featured && (!user || !user.id || !featured.focus) ) {
+			ShowEvent = <ContentEvent node={featured} user={user} path={path} extra={extra} featured={featured} />;
+		}
+
         return (
             <ContentList class="page-home-home">
+				{ShowEvent}
                 <ContentHeadlinerFeed node={node} types={['post']} subtypes={['news']} methods={['all']} limit={1} name="news" icon="news" class="-col-c" published love comments more="/news" />
 				{ShowHomework}
                 <ContentTimeline class="content-timeline-posts" types={['post']} methods={['all']} node={node} user={user} path={path} extra={extra} featured={featured} />
