ace46725f57331df6fac4675ab65e42ff4a0d7c8
Mike Kasprzak <mike@sykhronics.com>
2019-10-29 17:21:18 -0400

    corrected the start date

--- src/com/view/sidebar/sidebar.js
+++ src/com/view/sidebar/sidebar.js
@@ -17,13 +17,13 @@ export default class ViewSidebar extends Component {
 	render( props ) {
 		// TODO: cleanup
 		let ldName = "Ludum Dare 46";
-		let ldStartDate = new Date(Date.UTC(2020, 3, 17, 1, 0, 0));
+		let ldStartDate = new Date(Date.UTC(2020, 3, 18, 1, 0, 0));
 
-		let compoEndDate = new Date(Date.UTC(2020, 3, 19, 1, 0, 0));
-		let compoEndDate2 = new Date(Date.UTC(2020, 3, 19, 2, 0, 0));
+		let compoEndDate = new Date(Date.UTC(2020, 3, 20, 1, 0, 0));
+		let compoEndDate2 = new Date(Date.UTC(2020, 3, 20, 2, 0, 0));
 
-		let jamEndDate = new Date(Date.UTC(2020, 3, 20, 1, 0, 0));
-		let jamEndDate2 = new Date(Date.UTC(2020, 3, 20, 2, 0, 0));
+		let jamEndDate = new Date(Date.UTC(2020, 3, 21, 1, 0, 0));
+		let jamEndDate2 = new Date(Date.UTC(2020, 3, 21, 2, 0, 0));
 
 		let gradeEndDate = new Date(Date.UTC(2020, 4, 12, 20, 0, 0));
 		let resultsDate = new Date(Date.UTC(2020, 4, 12, 24, 0, 0));
