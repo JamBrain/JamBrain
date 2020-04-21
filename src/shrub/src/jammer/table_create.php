5f432f738185f2e8f56dc5de76d5da8f54fa1bd4
Mike Kasprzak <mike@sykhronics.com>
2020-04-21 18:21:54 -0400

    untested, but adds Ludum dare event

--- src/shrub/src/jammer/table_create.php
+++ src/shrub/src/jammer/table_create.php
@@ -9,3 +9,5 @@ MakeKeyNode('SH_NODE_ID_EVENTS', SH_NODE_ID_ROOT, 'group', SH_NODE_TYPE_EVENTS, 
 MakeKeyNode('jammer-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, '', '', 'jammer', 'Jammer' );
 MakeKeyNode('ludumdare-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, '', '', 'ludum-dare', 'Ludum Dare' );
 MakeKeyNode('jammer.bio-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, '', '', 'jammer-bio', 'Jammer.bio' );
+
+MakeKeyNode('ludum-dare-event', SH_NODE_ID_EVENTS, 'group', SH_NODE_TYPE_EVENTS, '', 'ludum-dare', 'Ludum Dare' );
