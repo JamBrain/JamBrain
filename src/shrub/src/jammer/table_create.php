078b85cc5be0e05d62799353cf9527686a9ae3bd
Mike Kasprzak <mike@sykhronics.com>
2020-04-21 16:10:02 -0400

    changed how tags gets created

--- src/shrub/src/jammer/table_create.php
+++ src/shrub/src/jammer/table_create.php
@@ -2,10 +2,10 @@
 
 // Not tables, but nodes
 
-MakeKeyNode('SH_NODE_ID_ITEMS', SH_NODE_ID_ROOT, SH_NODE_TYPE_ITEMS, 'items', 'Items' );
-MakeKeyNode('SH_NODE_ID_TAGS', SH_NODE_ID_ROOT, SH_NODE_TYPE_TAGS, 'tags', 'Tags' );
-MakeKeyNode('SH_NODE_ID_EVENTS', SH_NODE_ID_ROOT, SH_NODE_TYPE_EVENTS, 'events', 'Events' );
+MakeKeyNode('SH_NODE_ID_ITEMS', SH_NODE_ID_ROOT, SH_NODE_TYPE_ITEMS, '', '', 'items', 'Items' );
+MakeKeyNode('SH_NODE_ID_TAGS', SH_NODE_ID_ROOT, 'group', SH_NODE_TYPE_TAGS, '', 'tags', 'Tags' );
+MakeKeyNode('SH_NODE_ID_EVENTS', SH_NODE_ID_ROOT, SH_NODE_TYPE_EVENTS, '', '', 'events', 'Events' );
 
-MakeKeyNode('jammer-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, 'jammer', 'Jammer' );
-MakeKeyNode('ludumdare-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, 'ludum-dare', 'Ludum Dare' );
-MakeKeyNode('jammer.bio-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, 'jammer-bio', 'Jammer.bio' );
+MakeKeyNode('jammer-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, '', '', 'jammer', 'Jammer' );
+MakeKeyNode('ludumdare-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, '', '', 'ludum-dare', 'Ludum Dare' );
+MakeKeyNode('jammer.bio-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, '', '', 'jammer-bio', 'Jammer.bio' );
