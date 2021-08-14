c83df33a4ac8102f413edc58e4ece077906191e1
Mike Kasprzak <mikekasprzak+x270@gmail.com>
2021-08-14 19:14:29 -0400

    Fixed an ID bug during table creation

--- src/shrub/src/jammer/table_create.php
+++ src/shrub/src/jammer/table_create.php
@@ -4,10 +4,10 @@
 
 MakeKeyNode('SH_NODE_ID_ITEMS', SH_NODE_ID_ROOT, SH_NODE_TYPE_ITEMS, '', '', 'items', 'Items' );
 MakeKeyNode('SH_NODE_ID_TAGS', SH_NODE_ID_ROOT, 'group', SH_NODE_TYPE_TAGS, '', 'tags', 'Tags' );
-MakeKeyNode('SH_NODE_ID_EVENTS', SH_NODE_ID_ROOT, 'group', SH_NODE_TYPE_EVENTS, '', 'events', 'Events' );
+$events_id = MakeKeyNode('SH_NODE_ID_EVENTS', SH_NODE_ID_ROOT, 'group', SH_NODE_TYPE_EVENTS, '', 'events', 'Events' );
 
 MakeKeyNode('jammer-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, '', '', 'jammer', 'Jammer' );
 MakeKeyNode('ludumdare-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, '', '', 'ludum-dare', 'Ludum Dare' );
 MakeKeyNode('jammer.bio-root', SH_NODE_ID_ROOT, SH_NODE_TYPE_SITE, '', '', 'jammer-bio', 'Jammer.bio' );
 
-MakeKeyNode('ludum-dare-event', SH_NODE_ID_EVENTS, 'group', SH_NODE_TYPE_EVENTS, '', 'ludum-dare', 'Ludum Dare' );
+MakeKeyNode('ludum-dare-event', $events_id /*SH_NODE_ID_EVENTS*/, 'group', SH_NODE_TYPE_EVENTS, '', 'ludum-dare', 'Ludum Dare' );
