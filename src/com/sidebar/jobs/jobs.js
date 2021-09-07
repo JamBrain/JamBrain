af7be671498d582767d505b0b3471aed16968cba
Mike Kasprzak <mike@sykhronics.com>
2021-09-06 22:50:11 -0400

    hacked in an image

--- src/com/sidebar/jobs/jobs.js
+++ src/com/sidebar/jobs/jobs.js
@@ -11,7 +11,7 @@ export default class SidebarJobs extends Component {
 			<div class="sidebar-base sidebar-shortlist sidebar-upcoming">
 				<div class="-title _font2"><SVGIcon baseline>jobs</SVGIcon> <span class="-text">We're Hiring!</span></div>
 				<div class="-summary -gap">We are looking for a <NavLink href="https://ldjam.com/events/ludum-dare/48/$233336/ludum-dare-is-hiring-a-senior-developer-in-canada">Senior Developer in Canada</NavLink>.</div>
-				<div class="-summary -gap"><NavLink href="https://ldjam.com/events/ludum-dare/48/$233336/ludum-dare-is-hiring-a-senior-developer-in-canada"><img src="https://static.jam.vg/content/b/z/42c46.jpg.w240.jpg" /></NavLink></div>
+				<div class="-summary -gap"><NavLink href="https://ldjam.com/events/ludum-dare/48/$233336/ludum-dare-is-hiring-a-senior-developer-in-canada"><img style="display: block; margin: auto;" src="https://static.jam.vg/content/b/z/42c46.jpg.w240.jpg" /></NavLink></div>
 			</div>
 		);
 	}
