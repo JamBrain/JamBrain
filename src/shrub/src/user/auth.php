cd856ec3dc1d02cccf157764f20190531a0f367e
Mike Kasprzak <mike@sykhronics.com>
2021-09-20 20:42:18 -0400

    updated session code

--- src/shrub/src/user/auth.php
+++ src/shrub/src/user/auth.php
@@ -7,46 +7,65 @@ $USER = null;
 
 function userAuth_Start() {
 	global $USER;
-	
+
 	userSession_Start();
+	// Lookup Id
+	$id = isset($_SESSION['id']) ? intval($_SESSION['id']) : 0;
+	if ( $id == 0 ) {
+		userSession_End();
+		return;
+	}
+
 	// IP Check. If different, expire the session
-	if ( !isset($_SESSION['ip']) || ($_SESSION['ip'] !== $_SERVER['REMOTE_ADDR']) ) {
+	if (isset($_SESSION['ip']) && ($_SESSION['ip'] !== $_SERVER['REMOTE_ADDR'])) {
+		// If it's been less than 24 hours, allow IP changing (i.e. Wifi to Mobile Data switching)
+		if (isset($_SESSION['LAST_ACTIVITY']) && (time() - intval($_SESSION['LAST_ACTIVITY']) > (24*60*60))) {
+			userSession_Expire();
+			return;
+		}
+	}
+
+	// Take note of when you last did something
+	// If it was 3 days hours ago, log you out
+	if (isset($_SESSION['LAST_ACTIVITY']) && (time() - intval($_SESSION['LAST_ACTIVITY']) > (3*24*60*60))) {
 		userSession_Expire();
+		return;
+	}
+
+	// Update IP and timestamp
+	$_SESSION['LAST_ACTIVITY'] = time();
+	$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+	// Done, close session
+	userSession_End();
+
+	// TODO: Refresh Cookie timout
+
+	// Cache the node and permissions
+	$USER = nodeCache_GetById($id);
+	//$USER = nodeComplete_GetById($id);
+	if ( $USER ) {
+		// Extract Permissions
+		//$USER['private'] = nodeMeta_GetPrivateByNode($id);
 	}
 	else {
-		if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > (24*60*60))) {
-			userSession_Expire();
-		}
-		else {
-			$_copy = isset($_SESSION['LAST_ACTIVITY']) ? $_SESSION['LAST_ACTIVITY'] : null;
-			$_SESSION['LAST_ACTIVITY'] = time();
-			userSession_End();
-			$_SESSION['OLD_LAST_ACTIVITY'] = $_copy;
-		}
-	}
-	
-	// TODO: Refresh Cookie/Session timeouts
-
-	// If session is set, lookup the node, and permissions
-	if ( $id = userAuth_GetId() ) {
-		// Lookup user
-		$USER = nodeCache_GetById($id);
-		//$USER = nodeComplete_GetById($id);
-		if ( $USER ) {
-			// Extract Permissions
-			//$USER['private'] = nodeMeta_GetPrivateByNode($id);
-		}
-		else {
-			userSession_Start();
-			userSession_Expire();
-		}
+		// If no user, expire the session
+		userSession_Start();
+		userSession_Expire();
 	}
 }
 
 // Is active user a user (i.e. logged in)
 function userAuth_GetId() {
-	return isset($_SESSION['id']) ? intval($_SESSION['id']) : 0;
+	global $USER;
+	return isset($USER['id']) ? $USER['id'] : 0;
+	//return isset($_SESSION['id']) ? intval($_SESSION['id']) : 0;
 }
+
+// Might be insecure
+//function userAuth_GetUser() {
+//	global $USER;
+//	return $USER;
+//}
 
 /// Is active user an administrator
 function userAuth_IsAdmin() {
@@ -66,14 +85,14 @@ function userAuth_Has(...$args) {
 	if ( !isset($USER['private']) ) {
 		return false;
 	}
-	
+
 	// Check if user has all permissions
 	foreach ( $args as $arg ) {
 		if ( !in_array($arg, $USER['private']) ) {
 			return false;
 		}
 	}
-	
+
 	return true;
 }
 
@@ -84,8 +103,18 @@ function userAuth_Logout() {
 	return $ret;
 }
 
+// Used by login endpoint
+function userAuth_SetId($id) {
+	userSession_Start(true); // regenerate SID
+	$_SESSION['id'] = $id;
+	$_SESSION['LAST_ACTIVITY'] = time();
+	$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+	userSession_End();
+}
 
-function userSession_Start() {
+// ************************************************************************************************************************** //
+
+function userSession_Start($regenerate = false) {
 	$sid_name = 'SID';
 	$is_secure = false;
 
@@ -96,19 +125,9 @@ function userSession_Start() {
 	else if ( !empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on' ) {
 	    $is_secure = true;
 	}
-	
+
 	// Use a different name if we're connected securely
-	$sid_name .= $is_secure ? 'S' : '';
-	
-	// This can be removed, once we've moved entirely to PHP 7
-//	session_name($sid_name);
-//	session_set_cookie_params(
-//		2*24*60*60,			// Two days
-//		'/',
-//		$_SERVER['HTTP_HOST'],
-//		$is_secure,
-//		true
-//	);
+	$sid_name .= $is_secure ? 'S' : '';	// SIDS
 
 	$domain = $_SERVER['HTTP_HOST'];
 	$prefix = "api.";
@@ -116,10 +135,11 @@ function userSession_Start() {
 		$domain = substr($domain, strlen($prefix));
 	}
 
-	// Start Session	
+	// Start Session
 	session_start([
 		'name' => $sid_name,
-		'cookie_lifetime' => 3*24*60*60,			// Three days
+		'use_strict_mode' => true,
+		'cookie_lifetime' => 7*24*60*60,			// Three days
 		'cookie_httponly' => 1,						// Don't pass SID to JavaScript
 		'cookie_secure' => $is_secure ? 1 : 0,
 		//'cookie_path' => '/',
@@ -127,7 +147,10 @@ function userSession_Start() {
 //		'sid_length' => 64,							// As of PHP 7.1
 //		'sid_bits_per_character' => 5,				// As of PHP 7.1
 	]);
-	
+	if ( $regenerate ) {
+		session_regenerate_id(true);
+	}
+
 	// @todo: check how long the SID is. Was about 25 on PHP 5.6
 }
 
@@ -137,8 +160,14 @@ function userSession_End() {
 
 // http://php.net/manual/en/function.session-unset.php#107089
 function userSession_Expire() {
-	session_unset();
-	session_destroy();
+	//session_unset();	// do not use
+
+	// unset session values
+	unset($_SESSION['id']);
+	unset($_SESSION['LAST_ACTIVITY']);
+	unset($_SESSION['ip']);
+
+	session_destroy(); // MAY CAUSE RACE CONDITION! (according to docs)
 	userSession_End();
 	setcookie(session_name(), '', 0, '/');
 //	session_regenerate_id(true);					// error: "Cannot regenerate session id - session is not active"
