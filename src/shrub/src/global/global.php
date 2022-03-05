46320f321ebb48439ceb589539f3a34017d86f5f
Mike Kasprzak <mikekasprzak+x270@gmail.com>
2022-03-05 17:11:14 -0500

    committed a bad version of global

--- src/shrub/src/global/global.php
+++ src/shrub/src/global/global.php
@@ -20,32 +20,6 @@ function _global_Set($key, $value) {
 		"INSERT ".SH_TABLE_PREFIX.SH_TABLE_GLOBAL." (
 			`key`, `value`, `timestamp`
 		)
-		VALUES ( 
-			?, ?, NOW()
-		);",
-		$key,
-		$value
-	);
-}
-/// @endcond
-/// Set a global key to a new value
-function global_Set($key, $value) {
-	global $SH;
-
-	if ( !isset($SH[$key]) || $SH[$key] !== $value ) {
-		if ( !_global_Set($key,$value) )
-			return false;
-		$SH[$key] = $value;
-		cache_Store(_SH_GLOBAL_CACHE_KEY,$SH,_SH_GLOBAL_CACHE_TTL);
-		return true;
-	}
-	return false;
-}
-$value) {
-	return db_Query(
-		"INSERT ".SH_TABLE_PREFIX.SH_TABLE_GLOBAL." (
-			`key`, `value`, `timestamp`
-		)
 		VALUES (
 			?, ?, NOW()
 		);",
@@ -68,10 +42,6 @@ function global_Set($key, $value) {
 	return false;
 }
 
-/// @cond INTERNAL
-function _global_Load() {
-	// Fetch the newest version of every key //
-	$ret = db_QueryFetchPair(
 /// @cond INTERNAL
 function _global_Load() {
 	// Fetch the newest version of every key //
